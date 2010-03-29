be528cb3f91463c12bae33156e5c590cea9ab72d
Liam Girdwood <lrg@slimlogic.co.uk>
2010-03-29 13:56:24 +0100

    ASoC: multi-component - Wolfson WM87xx CODECs
    Moved Wolfson WM87xx CODECs to multi-component model.
    
    This patch changes the probe() and remove() of the CODEC drivers as follows:-
    
     o Make CODEC driver a platform driver
     o Moved all struct snd_soc_codec list, mutex, etc initialiasation to core.
     o Removed all static codec pointers (drivers now support > 1 codec dev)
     o snd_soc_register_pcms() now done by core.
     o snd_soc_register_dai() folded into snd_soc_register_codec().
     o codec cache can now be malloc()ed by core.
    
    Other required changes due to multi-component model :-
    
     o wm8753: dai mode configuration now modifies private data to store mode.
    
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/codecs/wm8727.h
+++ sound/soc/codecs/wm8727.h
@@ -15,7 +15,7 @@
 #ifndef WM8727_H_
 #define WM8727_H_
 
-extern struct snd_soc_dai wm8727_dai;
-extern struct snd_soc_codec_device soc_codec_dev_wm8727;
+extern struct snd_soc_dai_driver wm8727_dai;
+extern struct snd_soc_codec_driver soc_codec_dev_wm8727;
 
 #endif /* WM8727_H_ */
