dc13cc9b3cbc93f68feed5b528670d2f8a49a4ec
Benoit Cousson <b-cousson@ti.com>
2010-05-12 17:54:36 +0200

    OMAP4: hwmod: Add initial data for OMAP4430 ES1 & ES2
    The current version contains only the interconnects and the
    mpu hwmods.
    The remaining hwmods will be introduced by further patches on
    top of this one.
    
    - enable as well omap_hwmod.c build for OMAP4 Soc
    
    Please not that this file uses the new naming convention for
    naming HW IPs. This convention will be backported soon for previous
    OMAP2 & 3 data files.
    
    new name        trm name
    -------------   -------------------
    counter_32k     synctimer_32k
    l3_main         l3
    timerX          gptimerX / dmtimerX
    mmcX            mmchsX / sdmmcX
    dma_system      sdma
    smartreflex_X   sr_X / sr?
    usb_host_fs     usbfshost
    usb_otg_hs      hsusbotg
    usb_tll_hs      usbtllhs_config
    wd_timerX       wdtimerX
    ipu             cortexm3 / ducati
    dsp             c6x / tesla
    iva             ivahd / iva2.2
    kbd             kbdocp / keyboard
    mailbox         system_mailbox
    mpu             cortexa9 / chiron
    
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -324,6 +324,9 @@ void __init omap2_init_common_hw(struct omap_sdrc_params *sdrc_cs0,
 		omap2430_hwmod_init();
 	else if (cpu_is_omap34xx())
 		omap3xxx_hwmod_init();
+	else if (cpu_is_omap44xx())
+		omap44xx_hwmod_init();
+
 	omap2_mux_init();
 	/* The OPP tables have to be registered before a clk init */
 	omap_pm_if_early_init(mpu_opps, dsp_opps, l3_opps);
