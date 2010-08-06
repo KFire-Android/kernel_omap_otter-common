dd89e83f419c17f39572e32ea0277fa8e6394b14
Kevin Hilman <khilman@deeprootsystems.com>
2010-08-06 21:26:59 +0530

    OMAP: PM: create omap_devices for MPU, DSP, L3
    Create simple omap_devices for the main processors and busses.
    
    This is required to support the forth-coming device-based OPP
    approach, where OPPs are managed and tracked at the device level.
    
    Also, move these common PM init functions into a common_pm_init call
    that is called as a device_initcall().  The PM init is done at this level
    to ensure that the driver core is initialized before initialized.
    
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    [paul@pwsan.com: sparse warnings cleaned up; newly-created functions moved
     from mach-omap2/io.c to mach-omap2/pm.c; newly-created functions renamed
     to start with "omap2" rather than "omap"]
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -45,6 +45,7 @@
 
 #include <plat/clockdomain.h>
 #include "clockdomains.h"
+
 #include <plat/omap_hwmod.h>
 #include "dmtimer.h"
 
@@ -353,7 +354,6 @@ void __init omap2_init_common_hw(struct omap_sdrc_params *sdrc_cs0,
     skip_setup_idle = 1;
 #endif
     omap_hwmod_late_init(skip_setup_idle);
-	omap_pm_if_init();
 	if (cpu_is_omap24xx() || cpu_is_omap34xx()) {
 		omap2_sdrc_init(sdrc_cs0, sdrc_cs1);
 		_omap2_init_reprogram_sdrc();
