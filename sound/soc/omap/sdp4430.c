36b73f27e7f7639dca302ab616fdbec6bf869e0f
Liam Girdwood <lrg@slimlogic.co.uk>
2010-08-12 21:44:19 +0100

    ASoC: SDP4430 - sync stream names with BE widgets.
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Margarita Olaya Cabrera <magi.olaya@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -504,7 +504,7 @@ static struct snd_soc_dai_link sdp4430_dai[] = {
 	},
 	{
 		.name = OMAP_ABE_BE_DMIC0,
-		.stream_name = "D Mic 0",
+		.stream_name = "DMIC0",
 
 		/* ABE components - DMIC UL 1 */
 		.cpu_dai_name = "omap-dmic-dai.0",
@@ -519,7 +519,7 @@ static struct snd_soc_dai_link sdp4430_dai[] = {
 	},
 	{
 		.name = OMAP_ABE_BE_DMIC1,
-		.stream_name = "D Mic 1",
+		.stream_name = "DMIC1",
 
 		/* ABE components - DMIC UL 1 */
 		.cpu_dai_name = "omap-dmic-dai.1",
@@ -534,7 +534,7 @@ static struct snd_soc_dai_link sdp4430_dai[] = {
 	},
 	{
 		.name = OMAP_ABE_BE_DMIC2,
-		.stream_name = "D Mic 2",
+		.stream_name = "DMIC2",
 
 		/* ABE components - DMIC UL 2 */
 		.cpu_dai_name = "omap-dmic-dai.2",
