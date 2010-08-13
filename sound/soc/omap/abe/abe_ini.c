89d33710c3d3eac77012f87a16072ba607105b31
David Anders <x0132446@ti.com>
2010-08-13 10:00:12 -0500

    Fix array size error for aUplinkMuxing for ABE
    Fix an error in calculating the aUplinkMuxing array size in ABE
    
    Signed-off-by: David Anders <x0132446@ti.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Margarita Olaya Cabrera <magi.olaya@ti.com>

--- sound/soc/omap/abe/abe_ini.c
+++ sound/soc/omap/abe/abe_ini.c
@@ -51,7 +51,7 @@ void abe_hw_configuration()
  *  Return value :
  *
  */
-char aUplinkMuxing[16];
+char aUplinkMuxing[D_aUplinkRouting_sizeof];
 
 void abe_build_scheduler_table()
 {
@@ -314,7 +314,7 @@ void abe_build_scheduler_table()
 	// abe_block_copy (COPY_FROM_HOST_TO_ABE, ABE_DMEM, D_pFastLoopBack_ADDR, (abe_uint32*)&dFastLoopback, sizeof (dFastLoopback));
 
 	/* reset the uplink router */
-	n = D_aUplinkRouting_ADDR_END - D_aUplinkRouting_ADDR + 1;
+	n = sizeof(aUplinkMuxing);
 	for(i = 0; i < n; i++)
 		aUplinkMuxing[i] = ZERO_labelID;
 
