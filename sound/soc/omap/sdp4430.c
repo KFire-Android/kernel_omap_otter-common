2cccfdf1036cfb18102b6971e68e4be3769ec67f
Liam Girdwood <lrg@slimlogic.co.uk>
2010-08-18 18:30:59 +0100

    ASoC: SDP4430 - Add DMIC specific hw_params()
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Margarita Olaya Cabrera <magi.olaya@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -113,7 +113,10 @@ static int sdp4430_mcbsp_hw_params(struct snd_pcm_substream *substream,
 		return ret;
 	}
 
-	// TODO: where does this clock come from (external source??) - do we need to enable it
+	/*
+	 * TODO: where does this clock come from (external source??) -
+	 * do we need to enable it.
+	 */
 	/* Set McBSP clock to external */
 	ret = snd_soc_dai_set_sysclk(cpu_dai, OMAP_MCBSP_SYSCLK_CLKS_FCLK,
 				     64 * params_rate(params),
@@ -127,6 +130,40 @@ static int sdp4430_mcbsp_hw_params(struct snd_pcm_substream *substream,
 
 static struct snd_soc_ops sdp4430_mcbsp_ops = {
 	.hw_params = sdp4430_mcbsp_hw_params,
+};
+
+static int sdp4430_dmic_hw_params(struct snd_pcm_substream *substream,
+	struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	int ret;
+
+	/* Set DMIC DAI configuration */
+	ret = snd_soc_dai_set_fmt(cpu_dai,
+				  SND_SOC_DAIFMT_DSP_A |
+				  SND_SOC_DAIFMT_NB_NF |
+				  SND_SOC_DAIFMT_CBS_CFS);
+	if (ret < 0) {
+		printk(KERN_ERR "can't set DMIC DAI configuration\n");
+		return ret;
+	}
+
+	/* TODO: where does the DMIC clock come from (external source??) -
+	 * do we need to enable it. Set McBSP clock to external */
+	/* ret = snd_soc_dai_set_sysclk(cpu_dai, OMAP_MCBSP_SYSCLK_CLKS_FCLK,
+					64 * params_rate(params),
+				     SND_SOC_CLOCK_IN);
+	 */
+	if (ret < 0) {
+		printk(KERN_ERR "can't set DMIC cpu system clock\n");
+		return ret;
+	}
+	return 0;
+}
+
+static struct snd_soc_ops sdp4430_dmic_ops = {
+	.hw_params = sdp4430_dmic_hw_params,
 };
 
 /* Headset jack */
@@ -488,7 +525,7 @@ static struct snd_soc_dai_link sdp4430_dai[] = {
 		.codec_dai_name = "dmic-hifi",
 		.codec_name = "dmic-codec.0",
 
-		.ops = &sdp4430_mcpdm_ops,
+		.ops = &sdp4430_dmic_ops,
 	},
 
 /*
