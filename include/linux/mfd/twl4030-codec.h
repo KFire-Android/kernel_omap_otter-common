4c00d7f2a702a6f4b508926493964394b875e1ca
Manjunatha GK <manjugk@ti.com>
2010-08-27 01:36:27 +0530

    TWL4030: Codec: Fix fucntion declaration error
    Fixes sparse warning for the below error
    
    drivers/mfd/twl4030-codec.c:118:5: error: symbol 'twl4030_codec_disable_resource'
    redeclared with different type (originally declared at include/linux/mfd/twl4030-
    codec.h:268) - incompatible argument 1 (different signedness)
    
    Signed-off-by: Manjunatha GK <manjugk@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Nishanth Menon <nm@ti.com>

--- include/linux/mfd/twl4030-codec.h
+++ include/linux/mfd/twl4030-codec.h
@@ -265,7 +265,7 @@ enum twl4030_codec_res {
 	TWL4030_CODEC_RES_MAX,
 };
 
-int twl4030_codec_disable_resource(enum twl4030_codec_res id);
+int twl4030_codec_disable_resource(unsigned id);
 int twl4030_codec_enable_resource(enum twl4030_codec_res id);
 unsigned int twl4030_codec_get_mclk(void);
 
