skitlab/kernel-source-dm8168-3.2.y

omap3: introduce cpufreq
OMAP OPP layer functions now have dependencies of CONFIG_CPU_FREQ only.

With this patch, omap opp layer now has its compilation flags
bound to CONFIG_CPU_FREQ. Also its code has been removed from pm34xx.c.

A new file has been created to contain cpu freq code related to
OMAP3: cpufreq34xx.c

OMAP34xx and OMAP36xx family OPPs are made available

Signed-off-by: Eduardo Valentin <eduardo.valentin@nokia.com>
Signed-off-by: Paul Walmsley <paul@pwsan.com>
Signed-off-by: Nishanth Menon <nm@ti.com>
Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>
Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
Signed-off-by: Romit Dasgupta <romit@ti.com>
Signed-off-by: Rajendra Nayak <rnayak@ti.com>

arch/arm/mach-omap2/omap3-opp.h
@@ -1,0 +1,20 @@
+#ifndef __OMAP3_OPP_H_
+#define __OMAP3_OPP_H_
+
+#include <plat/omap-pm.h>
+
+/**
+ * omap3_pm_init_opp_table - OMAP opp table lookup called after cpu is detected.
+ * Initialize the basic opp table here, board files could choose to modify opp
+ * table after the basic initialization
+ */
+#ifdef CONFIG_CPU_FREQ
+extern int omap3_pm_init_opp_table(void);
+#else
+static inline int omap3_pm_init_opp_table(void)
+{
+	return -EINVAL;
+}
+#endif
+
+#endif

KFire-Android/kernel_omap_otter-common

arch/arm/mach-omap2/omap3-opp.h
@@ -1,0 +1,20 @@
+#ifndef __OMAP3_OPP_H_
+#define __OMAP3_OPP_H_
+
+#include <plat/omap-pm.h>
+
+/**
+ * omap3_pm_init_opp_table - OMAP opp table lookup called after cpu is detected.
+ * Initialize the basic opp table here, board files could choose to modify opp
+ * table after the basic initialization
+ */
+#ifdef CONFIG_PM
+extern int omap3_pm_init_opp_table(void);
+#else
+static inline int omap3_pm_init_opp_table(void)
+{
+	return -EINVAL;
+}
+#endif
+
+#endif