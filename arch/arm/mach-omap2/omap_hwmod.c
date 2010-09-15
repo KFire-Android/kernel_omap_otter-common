f2b4f8a6491ea1e407aad0b49d4d0d8e5ffa3159
Basak, Partha <p-basak2@ti.com>
2010-09-15 16:10:14 +0530

    OMAP: hwmod: Do not reset IP's with no reset status
    Some IP's like mcbsp and timer which do not have reset status
     when reset at boot are gating L3 init domain from transtioning
     and hence blocking CORE retention.
     For now do not reset these IP's.
    
    Signed-off-by: Basak, Partha <p-basak2@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -893,6 +893,7 @@ static int _reset(struct omap_hwmod *oh)
 	int ret = 0;
 
 	if (!oh->class->sysc ||
+	    !(oh->class->sysc->sysc_flags & SYSC_HAS_RESET_STATUS) ||
 	    !(oh->class->sysc->sysc_flags & SYSC_HAS_SOFTRESET))
 		return -EINVAL;
 
