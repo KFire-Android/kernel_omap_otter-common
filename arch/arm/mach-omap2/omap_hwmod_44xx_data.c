a338b354a6f7249b99ec7d2daf1ed2476c16f2fb
Mayuresh Janorkar <mayur@ti.com>
2010-09-15 16:54:23 -0500

    Merge Fix: Correcting DSS HWMOD
    This patch would correct fields in DSS HWMOD in OMAP4 HWMOD database
    
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>
    Signed-off-by: Ricardo Perez Olivares <x0081762@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -1293,15 +1293,9 @@ static struct omap_hwmod omap44xx_dsp_hwmod = {
  */
  static struct omap_hwmod_class_sysconfig omap44xx_dss_sysc = {
 	.rev_offs	= 0x0000,
-	.sysc_offs	= 0x0010,
 	.syss_offs	= 0x0014,
-	.sysc_flags	= (SYSC_HAS_SIDLEMODE | SYSC_HAS_CLOCKACTIVITY |
-			   SYSC_HAS_MIDLEMODE | SYSC_HAS_ENAWAKEUP |
-			   SYSC_HAS_SOFTRESET | SYSC_HAS_AUTOIDLE |
-			   SYSS_HAS_RESET_STATUS),
-	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |
-			   MSTANDBY_FORCE | MSTANDBY_NO | MSTANDBY_SMART),
-	.sysc_fields	= &omap_hwmod_sysc_type1,
+	.sysc_flags	= SYSS_HAS_RESET_STATUS,
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART),
 };
 
 static struct omap_hwmod_class omap44xx_dss_hwmod_class = {
