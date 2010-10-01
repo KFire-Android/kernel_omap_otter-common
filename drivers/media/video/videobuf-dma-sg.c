903e7dce0381c0ea5fc19b788341193f42fc46c8
Deepak K <deepak.k@ti.com>
2010-10-01 12:40:06 -0500

    OMAP3 : CAMERA : Fix the highmem related issues
    The video buffer library was not handling highmem pages properly.
    The function calls "videobuf_vmalloc_to_sg" and
    "videobuf_pages_to_sg" used to skip highmem pages. These buffer
    could be allocated else where in application for processing,
    which can fall in highmem region.
    
    Cherry Picked from .32 android kernel.
    
    Signed-off-by: Deepak K <deepak.k@ti.com>
    Acked-by: Christina Warren <cawarren@ti.com>
    Acked-by: Akwasi Boateng <akwasi.boateng@ti.com>

--- drivers/media/video/videobuf-dma-sg.c
+++ drivers/media/video/videobuf-dma-sg.c
@@ -72,7 +72,9 @@ struct scatterlist *videobuf_vmalloc_to_sg(unsigned char *virt, int nr_pages)
 		pg = vmalloc_to_page(virt);
 		if (NULL == pg)
 			goto err;
+#if !defined(CONFIG_HIGHMEM)
 		BUG_ON(PageHighMem(pg));
+#endif
 		sg_set_page(&sglist[i], pg, PAGE_SIZE, 0);
 	}
 	return sglist;
@@ -95,16 +97,19 @@ struct scatterlist *videobuf_pages_to_sg(struct page **pages, int nr_pages,
 	if (NULL == sglist)
 		return NULL;
 	sg_init_table(sglist, nr_pages);
-
+#if !defined(CONFIG_HIGHMEM)
 	if (PageHighMem(pages[0]))
 		/* DMA to highmem pages might not work */
 		goto highmem;
+#endif
 	sg_set_page(&sglist[0], pages[0], PAGE_SIZE - offset, offset);
 	for (i = 1; i < nr_pages; i++) {
 		if (NULL == pages[i])
 			goto nopage;
+#if !defined(CONFIG_HIGHMEM)
 		if (PageHighMem(pages[i]))
 			goto highmem;
+#endif
 		sg_set_page(&sglist[i], pages[i], PAGE_SIZE, 0);
 	}
 	return sglist;
