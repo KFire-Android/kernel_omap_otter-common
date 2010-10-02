23635066c5e6399ae886ef1c169b2c1cfd8030cc
Ohad Ben-Cohen <ohad@wizery.com>
2010-10-02 13:54:11 +0200

    mmc: sdio: enable runtime PM for SDIO functions
    Enable runtime PM for SDIO functions.
    
    SDIO functions are initialized with a disabled runtime PM state,
    and are set active (and their usage count is incremented)
    only before potential drivers are probed.
    
    SDIO function drivers that support runtime PM should call
    pm_runtime_put_noidle() in their probe routine, and
    pm_runtime_get_noresume() in their remove routine (very
    similarly to PCI drivers).
    
    In case a matching driver does not support runtime PM, power will
    always be kept high (since the usage count is positive).
    
    Change-Id: Ifbac93b3ff2ecb5329883b1a01d872437ad40fe4
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Tested-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -659,6 +659,11 @@ int mmc_attach_sdio(struct mmc_host *host, u32 ocr)
 		err = sdio_init_func(host->card, i + 1);
 		if (err)
 			goto remove;
+
+		/*
+		 * Enable Runtime PM for this func
+		 */
+		pm_runtime_enable(&card->sdio_func[i]->dev);
 #ifdef CONFIG_TIWLAN_SDIO
 	}
 #endif
