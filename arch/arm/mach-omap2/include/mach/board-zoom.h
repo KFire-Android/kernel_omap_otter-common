5b019f215c4c8a92bf38c68596263a66691ae83f
Christina Warren <cawarren@ti.com>
2010-10-04 17:43:52 -0700

    OMAP3: Fix compilation error in board-zoom2
    This patch fixes this compilation error:
    arch/arm/mach-omap2/board-zoom2.c:84: error:
    implicit declaration of function 'zoom_display_init'
    
    The error was introduced due to commit:
    f36c5988203884097b902eda8fb5c2e603743273
    
    Change-Id: I65ef65b7e3b84e44f284a2a1c69fdc0a2344b38f
    Signed-off-by: Christina Warren <cawarren@ti.com>

--- arch/arm/mach-omap2/include/mach/board-zoom.h
+++ arch/arm/mach-omap2/include/mach/board-zoom.h
@@ -5,5 +5,6 @@
 
 extern int __init zoom_debugboard_init(void);
 extern void __init zoom_peripherals_init(void);
+extern void __init zoom_display_init(enum omap_dss_venc_type venc_type);
 
 #define ZOOM2_HEADSET_EXTMUTE_GPIO	153
