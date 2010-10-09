665c7e4e09167629d45dfa0609e95d1e969dd4bc
Lajos Molnar <molnar@ti.com>
2010-10-09 15:40:54 -0500

    OMAP/DSS2: Disable devices that failed to resume.
    We only call the resume handler on suspended devices, so any
    failure should disable the device.
    
    Signed-off-by: Lajos Molnar <molnar@ti.com>

--- drivers/video/omap2/dss/display.c
+++ drivers/video/omap2/dss/display.c
@@ -651,8 +651,12 @@ static int dss_resume_device(struct device *dev, void *data)
 
 	if (dssdev->activate_after_resume && dssdev->driver->resume) {
 		r = dssdev->driver->resume(dssdev);
-		if (r)
+		if (r) {
+			DSSERR("Failed to resume %s device (%d), disabling\n",
+				dssdev->name, r);
+			dssdev->driver->disable(dssdev);
 			return r;
+		}
 	} else {
 		/* disabled may not be the same as suspended so call handler */
 		dssdev->driver->disable(dssdev);
