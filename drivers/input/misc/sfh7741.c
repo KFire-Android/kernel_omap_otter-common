72548c7cb9491ef9026b337115a6f05faa56e2e7
Dan Murphy <dmurphy@ti.com>
2010-10-13 09:24:58 -0500

    OMAP4: Update the SFH7741 prox sensor for Android
    Updated the SFH7741 prox driver for Android
    
    1. Enabled IRQ wake for proximity
    2. Ported probe changes from 32 kernel to 35 retaining
    open source enhancements.
    3. Removed suspend/resume handlers for Android build
    
    Change-Id: If957e676c9bca89a2433980cb9d83584204a7f31
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- drivers/input/misc/sfh7741.c
+++ drivers/input/misc/sfh7741.c
@@ -26,12 +26,19 @@
 #include <linux/input/sfh7741.h>
 #include <linux/slab.h>
 
+#define SFH7741_PROX_ON	1
+#define SFH7741_PROX_OFF	0
+
+#undef SFH7741_SUSPEND_RESUME
+
 struct sfh7741_drvdata {
 	struct input_dev *input;
 	int irq;
 	int prox_enable;
+	int on_before_suspend;
 	/* mutex for sysfs operations */
 	struct mutex lock;
+	struct sfh7741_platform_data *pdata;
 	void (*activate_func)(int state);
 	int (*read_prox)(void);
 };
@@ -52,26 +59,37 @@ static ssize_t set_prox_state(struct device *dev,
 				struct device_attribute *attr,
 				const char *buf, size_t count)
 {
-	int state;
+	int state = SFH7741_PROX_OFF;
+	int proximity = 0;
 	struct platform_device *pdev = to_platform_device(dev);
 	struct sfh7741_drvdata *ddata = platform_get_drvdata(pdev);
 
 	if (sscanf(buf, "%u", &state) != 1)
 		return -EINVAL;
 
-	if ((state != 1) && (state != 0))
-		return -EINVAL;
-
-	ddata->activate_func(state);
+	if (state != SFH7741_PROX_OFF)
+		state = SFH7741_PROX_ON;
 
 	mutex_lock(&ddata->lock);
 	if (state != ddata->prox_enable) {
-		if (state)
+		if (state) {
 			enable_irq(ddata->irq);
-		else
-			disable_irq(ddata->irq);
+			if (ddata->pdata->flags & SFH7741_WAKEABLE_INT)
+				enable_irq_wake(ddata->irq);
+
+			proximity = ddata->read_prox();
+			input_report_abs(ddata->input, ABS_DISTANCE, !proximity);
+			input_sync(ddata->input);
+		} else {
+			disable_irq_nosync(ddata->irq);
+			if (ddata->pdata->flags & SFH7741_WAKEABLE_INT)
+				disable_irq_wake(ddata->irq);
+		}
+
+		ddata->activate_func(state);
 		ddata->prox_enable = state;
 	}
+
 	mutex_unlock(&ddata->lock);
 	return strnlen(buf, count);
 }
@@ -98,29 +116,39 @@ static const struct attribute_group sfh7741_attr_group = {
 
 static int __devinit sfh7741_probe(struct platform_device *pdev)
 {
-	struct sfh7741_platform_data *pdata = pdev->dev.platform_data;
 	struct sfh7741_drvdata *ddata;
-	struct device *dev = &pdev->dev;
 	struct input_dev *input;
+	struct device *dev = &pdev->dev;
 	int  error;
-	char *desc = "sfh7741";
 
-	pr_info("SFH7741: Proximity sensor\n");
+	pr_info("%s: Proximity sensor\n", __func__);
 
-	if (!pdata->activate_func || !pdata->read_prox) {
-		dev_err(dev, "The activate and read func not allocated\n");
+	if (pdev->dev.platform_data == NULL) {
+		pr_err("%s: Platform data is NULL\n", __func__);
 		return -EINVAL;
 	}
 
 	ddata = kzalloc(sizeof(struct sfh7741_drvdata),
 			GFP_KERNEL);
-	if (!ddata)
+	if (!ddata) {
+		pr_err("%s:Could not allocate memory for Proximity\n",
+			__func__);
 		return -ENOMEM;
+	}
+
+	ddata->pdata = pdev->dev.platform_data;
+	if (!ddata->pdata->activate_func ||
+	    !ddata->pdata->read_prox) {
+		pr_err("%s:Read Prox or Activate is NULL\n", __func__);
+		error = -EINVAL;
+		goto fail0;
+	}
+
 	input = input_allocate_device();
 	if (!input) {
-		dev_err(dev, "failed to allocate input device\n");
-		kfree(ddata);
-		return -ENOMEM;
+		pr_err("%s: Failed to allocate input device\n", __func__);
+		error = -ENOMEM;
+		goto fail0;
 	}
 
 	input->name = pdev->name;
@@ -128,11 +156,11 @@ static int __devinit sfh7741_probe(struct platform_device *pdev)
 	input->dev.parent = &pdev->dev;
 
 	input->id.bustype = BUS_HOST;
-	ddata->irq = pdata->irq;
-	ddata->prox_enable = pdata->prox_enable;
+	ddata->irq = ddata->pdata->irq;
+	ddata->prox_enable = ddata->pdata->prox_enable;
 
-	ddata->activate_func = pdata->activate_func;
-	ddata->read_prox =  pdata->read_prox;
+	ddata->activate_func = ddata->pdata->activate_func;
+	ddata->read_prox =  ddata->pdata->read_prox;
 
 	ddata->input = input;
 	__set_bit(EV_ABS, input->evbit);
@@ -141,29 +169,31 @@ static int __devinit sfh7741_probe(struct platform_device *pdev)
 
 	error = input_register_device(input);
 	if (error) {
-		dev_err(dev, "Unable to register input device,error: %d\n",
-				error);
+		pr_err("%s: Unable to register input device,error: %d\n",
+				__func__, error);
 		goto fail1;
 	}
 
 	platform_set_drvdata(pdev, ddata);
 
-	error = request_threaded_irq(pdata->irq , NULL ,
+	error = request_threaded_irq(ddata->irq , NULL ,
 				sfh7741_isr,
 				IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
-				desc, ddata);
+				"sfh7741_irq", ddata);
 	if (error) {
-		dev_err(dev, "Unable to claim irq %d; error %d\n",
-			pdata->irq, error);
+		pr_err("%s: Unable to claim irq %d; error %d\n",
+			__func__, ddata->pdata->irq, error);
 		goto fail2;
 	}
 
 	mutex_init(&ddata->lock);
 	error = sysfs_create_group(&dev->kobj, &sfh7741_attr_group);
 	if (error) {
-		dev_err(dev, "failed to create sysfs entries\n");
+		pr_err("%s: Failed to create sysfs entries\n", __func__);
 		mutex_destroy(&ddata->lock);
 	}
+
+	disable_irq_nosync(ddata->irq);
 
 	return 0;
 
@@ -172,6 +202,7 @@ fail2:
 	platform_set_drvdata(pdev, NULL);
 fail1:
 	input_free_device(input);
+fail0:
 	kfree(ddata);
 	return error;
 
@@ -190,11 +221,15 @@ static int __devexit sfh7741_remove(struct platform_device *pdev)
 }
 
 #ifdef CONFIG_PM
+#ifdef SFH7741_SUSPEND_RESUME
 static int sfh7741_suspend(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct sfh7741_drvdata *ddata = platform_get_drvdata(pdev);
-	ddata->activate_func(0);
+	/* Save the prox state for the resume */
+	ddata->on_before_suspend = ddata->prox_enable
+	ddata->pdata->activate_func(SFH7741_PROX_OFF);
+
 	return 0;
 }
 
@@ -202,7 +237,10 @@ static int sfh7741_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct sfh7741_drvdata *ddata = platform_get_drvdata(pdev);
-	ddata->activate_func(1);
+
+	if (ddata->on_before_suspend)
+		ddata->pdata->activate_func(SFH7741_PROX_ON);
+
 	return 0;
 }
 
@@ -211,15 +249,18 @@ static const struct dev_pm_ops sfh7741_pm_ops = {
 	.resume		= sfh7741_resume,
 };
 #endif
+#endif
 
 static struct platform_driver sfh7741_device_driver = {
 	.probe		= sfh7741_probe,
 	.remove		= __devexit_p(sfh7741_remove),
 	.driver		= {
-		.name	= "sfh7741",
+		.name	= SFH7741_NAME,
 		.owner	= THIS_MODULE,
 #ifdef CONFIG_PM
+#ifdef SFH7741_SUSPEND_RESUME
 		.pm	= &sfh7741_pm_ops,
+#endif
 #endif
 	}
 };
--- include/linux/input/sfh7741.h
+++ include/linux/input/sfh7741.h
@@ -21,6 +21,11 @@
 #ifndef __SFH7741_H
 #define __SFH7741_H
 
+#define SFH7741_NAME	"sfh7741"
+
+#define SFH7741_WAKEABLE_INT  (1 << 0)
+#define SFH7741_DEFAULT_ON    (1 << 1)
+
 /*
  * struct sfh7741_platform_data - SFH7741 Platform data
  * @irq: IRQ assigned
@@ -29,6 +34,7 @@
  * @read_prox: function to read the sensor output
  */
 struct sfh7741_platform_data {
+	int flags;
 	int irq;
 	int prox_enable;
 	void (*activate_func)(int state);
