ff5ff5d4e906e5221f20f3df6f7f5815dfb1ba81
Ohad Ben-Cohen <ohad@wizery.com>
2010-11-27 03:50:16 +0200

    mmc: skip detection of nonremovable cards on rescan
    mmc_rescan checks whether registered cards are still present before
    skipping them, by calling the bus-specific ->detect() handler.
    
    With buses that support runtime PM, the card may be powered off at
    this point, so they need to be powered on and fully reinitialized before
    ->detect() executes.
    
    This whole process is redundant with nonremovable cards; in those cases,
    we can safely skip calling ->detect() and implicitly assume its success.
    
    Change-Id: I2e9728f25177d092b1db218c7dd0cdd070136e6c
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1128,8 +1128,12 @@ void mmc_rescan(struct work_struct *work)
 
 	mmc_bus_get(host);
 
-	/* if there is a card registered, check whether it is still present */
-	if ((host->bus_ops != NULL) && host->bus_ops->detect && !host->bus_dead)
+	/*
+	 * if there is a _removable_ card registered, check whether it is
+	 * still present
+	 */
+	if ((host->bus_ops != NULL) && host->bus_ops->detect && !host->bus_dead
+					&& mmc_card_is_removable(host))
 		host->bus_ops->detect(host);
 
 	/* If the card was removed the bus will be marked
