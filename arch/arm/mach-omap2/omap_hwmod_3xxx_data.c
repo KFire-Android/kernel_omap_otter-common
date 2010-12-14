f8bee7afbb7536a984a6d0fb7e142af4f0d4d190
keun-o.park <sahara@ti.com>
2010-12-15 00:22:50 +0900

    OMAP3: camera: add ispmmu hwmod
    This patchset adds a missing hwmod for ispmmu.
    This depends on commit I4210f601569d9292b9c7d806302bd5654424fa82
    for referencing chip id (CHIP_IS_OMAP3630ES1_2).
    
    Change-Id: I4859505af0bd6d3b54c47355eb195d9b877349da
    Signed-off-by: keun-o.park <sahara@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
@@ -48,6 +48,7 @@ static struct omap_hwmod omap3xxx_dss_dsi1_hwmod;
 static struct omap_hwmod omap3xxx_dss_rfbi_hwmod;
 static struct omap_hwmod omap3xxx_dss_venc_hwmod;
 static struct omap_hwmod omap3xxx_mpu_hwmod;
+static struct omap_hwmod omap3xxx_ispmmu_hwmod;
 static struct omap_hwmod omap3xxx_iva_hwmod;
 static struct omap_hwmod omap3xxx_l3_main_hwmod;
 static struct omap_hwmod omap3xxx_l4_core_hwmod;
@@ -1283,6 +1284,67 @@ static struct omap_hwmod omap3xxx_mpu_hwmod = {
 	.masters	= omap3xxx_mpu_masters,
 	.masters_cnt	= ARRAY_SIZE(omap3xxx_mpu_masters),
 	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_OMAP3430),
+};
+
+/*
+ * ISP MMU interface data
+ */
+
+static struct omap_hwmod_class_sysconfig omap3xxx_ispmmu_sysc = {
+	.rev_offs	= 0x00,
+	.sysc_offs	= 0x10,
+	.syss_offs	= 0x14,
+	.sysc_flags	= (SYSC_HAS_CLOCKACTIVITY | SYSC_HAS_SIDLEMODE |
+			   SYSC_HAS_SOFTRESET | SYSC_HAS_AUTOIDLE |
+			   SYSS_HAS_RESET_STATUS),
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART),
+	.sysc_fields	= &omap_hwmod_sysc_type1,
+};
+
+static struct omap_hwmod_class omap3xxx_ispmmu_hwmod_class = {
+	.name = "isp",
+	.sysc = &omap3xxx_ispmmu_sysc,
+};
+
+static struct omap_hwmod_irq_info omap3xxx_ispmmu_irqs[] = {
+	{ .irq = INT_34XX_CAM_IRQ, },
+};
+
+static struct omap_hwmod_addr_space omap3xxx_ispmmu_addrs[] = {
+	{
+		.pa_start	= 0x480bd400,
+		.pa_end		= 0x480bd4ff,
+		.flags		= ADDR_TYPE_RT
+	},
+};
+
+/* ISP <- L3 interface */
+static struct omap_hwmod_ocp_if omap3xxx_l3__ispmmu = {
+	.master		= &omap3xxx_l3_main_hwmod,
+	.slave		= &omap3xxx_ispmmu_hwmod,
+	.clk		= "cam_ick",
+	.addr		= omap3xxx_ispmmu_addrs,
+	.addr_cnt	= ARRAY_SIZE(omap3xxx_ispmmu_addrs),
+	.user		= OCP_USER_MPU | OCP_USER_SDMA,
+};
+static struct omap_hwmod_ocp_if *omap3xxx_ispmmu_slaves[] = {
+	&omap3xxx_l3__ispmmu,
+};
+
+/*
+ * ISP
+ */
+
+static struct omap_hwmod omap3xxx_ispmmu_hwmod = {
+	.name		= "isp",
+	.class		= &omap3xxx_ispmmu_hwmod_class,
+	.vdd_name	= "mpu",
+	.mpu_irqs	= omap3xxx_ispmmu_irqs,
+	.mpu_irqs_cnt	= ARRAY_SIZE(omap3xxx_ispmmu_irqs),
+	.flags		= (HWMOD_NO_IDLEST | HWMOD_INIT_NO_RESET),
+	.slaves		= omap3xxx_ispmmu_slaves,
+	.slaves_cnt	= ARRAY_SIZE(omap3xxx_ispmmu_slaves),
+	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_OMAP3630ES1_2)
 };
 
 /*
@@ -3466,6 +3528,7 @@ static __initdata struct omap_hwmod *omap3xxx_hwmods[] = {
 	&omap3xxx_mmc2_hwmod,
 	&omap3xxx_mmc3_hwmod,
 	&omap3xxx_mpu_hwmod,
+	&omap3xxx_ispmmu_hwmod,
 	&omap3xxx_iva_hwmod,
 	&omap3xxx_gpu_hwmod,
 	&omap3xxx_i2c1_hwmod,
