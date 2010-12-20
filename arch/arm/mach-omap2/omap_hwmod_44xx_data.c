46fdadc7327df29d2d19606d6a2e9af5c7d92420
Djamil Elaidi <d-elaidi@ti.com>
2010-12-20 10:40:21 +0100

    OMAP4: HSI: Added Sysconfig class to HSI HWMOD class
    The SYSCONFIG registers definition class (omap_hwmod_class_sysconfig) was missing in the the HSI HWMOD class (omap_hwmod_class).
    
    Change-Id: Idc9b2385c615e0a5e96482b79f2fdf42bed0b83c
    Signed-off-by: Djamil Elaidi <d-elaidi@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -2204,8 +2204,21 @@ static struct omap_hwmod omap44xx_dss_hdmi_hwmod = {
  * serial if)
  */
 
+static struct omap_hwmod_class_sysconfig omap44xx_hsi_sysc = {
+	.rev_offs	= 0x0000,
+	.sysc_offs	= 0x0010,
+	.syss_offs	= 0x0014,
+	.sysc_flags	= (SYSC_HAS_AUTOIDLE | SYSC_HAS_EMUFREE |
+			   SYSC_HAS_SIDLEMODE | SYSC_HAS_MIDLEMODE |
+			   SYSC_HAS_SOFTRESET),
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |
+			   MSTANDBY_FORCE | MSTANDBY_NO | MSTANDBY_SMART),
+	.sysc_fields	= &omap_hwmod_sysc_type1,
+};
+
 static struct omap_hwmod_class omap44xx_hsi_hwmod_class = {
 	.name = "hsi",
+	.sysc = &omap44xx_hsi_sysc,
 };
 
 /* hsi */
