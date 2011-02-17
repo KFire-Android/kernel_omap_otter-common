19fcc5b3754baba188439aed3694fc3d1b2d6413
Pavan Savoy <pavan_savoy@ti.com>
2011-02-17 12:14:58 +0530

    include:tty: add N_TI_WL line discipline
    Change-Id: I204325bcc546786f5ef0f698f9012565e720d752
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -49,6 +49,7 @@
 #define N_V253		19	/* Codec control over voice modem */
 #define N_CAIF		20      /* CAIF protocol for talking to modems */
 #define N_GSM0710	21	/* GSM 0710 Mux */
+#define N_TI_WL		22	/* Texas Instruments WiLink shared transport */
 
 /*
  * This character is the same as _POSIX_VDISABLE: it cannot be used as
