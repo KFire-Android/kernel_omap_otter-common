deef2421b82154fd80c00abd3c14acd82e82aa2d
Mohan <mohanv@ti.com>
2011-02-22 18:15:29 +0530

    usb: musb: Do not cut clock on suspend interrupt
    The USB CV Mass Storage Test cases fail with an error "Device not
    found" when the PHY clocks are cut in SUSPEND interrupt.
    
    When the USB cable is connected, a wake lock is held; hence there
    is no need to cut the clock in suspend interrupt.
    This patch removes cutting and enabling of clock in SUSPEND and
    RESUME interrupt respectively.
    
    [Moiz] This patch also fixes the issue of gadget enumeration
    in full-speed.
    
    Change-Id: Id72ded67af1e16d12ecea6c2edc7cecac424ab08
    Signed-off-by: Mohan V <mohanv@ti.com>
    Acked-by: Moiz Sonasath <m-sonasath@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -519,7 +519,6 @@ static irqreturn_t musb_stage0_irq(struct musb *musb, u8 int_usb,
 			}
 #endif
 		} else {
-			otg_set_clk(musb->xceiv, 1);
 
 			switch (musb->xceiv->state) {
 #ifdef CONFIG_USB_MUSB_HDRC_HCD
@@ -696,7 +695,6 @@ static irqreturn_t musb_stage0_irq(struct musb *musb, u8 int_usb,
 							OTG_TIME_B_ASE0_BRST));
 #endif
 			}
-			otg_set_clk(musb->xceiv, 0);
 			break;
 		case OTG_STATE_A_WAIT_BCON:
 			if (musb->a_wait_bcon != 0)
