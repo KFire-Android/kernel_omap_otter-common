b4bc3825cc1282903d2839a241417c440efb9a29
Misael Lopez Cruz <misael.lopez@ti.com>
2011-03-01 20:19:33 -0600

    misc: cdc tcxo: Create misc driver and remove board specific code
    Remove board specific code from CDC TCXO driver and create a set
    of APIs to allow external drivers to configure CDC CLKs settings,
    for example: request source (internal, external) and internal
    request values.
    
    CDC TCXO driver is converted to a misc driver to easily allow
    other drivers to access common APIs.
    
    Change-Id: If14288fa6b04da024bf1fd7a71b1b7238c248958
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

---
+++ drivers/misc/cdc_tcxo.c
@@ -0,0 +1,173 @@
+/*
+ * Clock Divider Chip (TCXO) support
+ *
+ * Copyright (C) 2010 Texas Instruments, Inc.
+ * Written by Rajendra Nayak <rnayak@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/platform_device.h>
+#include <linux/cdc_tcxo.h>
+#include <linux/i2c.h>
+
+#define DRIVER_DESC       "CDC TCXO driver"
+#define DRIVER_NAME       "cdc_tcxo_driver"
+
+struct cdc_tcxo_info {
+	struct i2c_client *client;
+	struct device *dev;
+	char buf[4];
+	int active;
+};
+
+static struct cdc_tcxo_info cdc_info;
+
+int cdc_tcxo_set_req_int(int clk_id, int enable)
+{
+	char mask;
+	int ret;
+
+	if (!cdc_info.active) {
+		dev_err(cdc_info.dev, "device is not active yet\n");
+		return -ENODEV;
+	}
+
+	switch (clk_id) {
+	case CDC_TCXO_CLK1:
+		mask = CDC_TCXO_REQ1INT;
+		break;
+	case CDC_TCXO_CLK2:
+		mask = CDC_TCXO_REQ2INT;
+		break;
+	case CDC_TCXO_CLK3:
+		mask = CDC_TCXO_REQ3INT;
+		break;
+	case CDC_TCXO_CLK4:
+		mask = CDC_TCXO_REQ4INT;
+		break;
+	default:
+		dev_err(cdc_info.dev, "invalid clk_id: %d\n", clk_id);
+		return -EINVAL;
+	}
+
+	if (enable)
+		cdc_info.buf[CDC_TCXO_REG1] |= mask;
+	else
+		cdc_info.buf[CDC_TCXO_REG1] &= ~mask;
+
+	ret = i2c_master_send(cdc_info.client, cdc_info.buf, CDC_TCXO_REGNUM);
+	if (ret < 0) {
+		dev_err(cdc_info.dev, "failed to write data (%d)\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(cdc_tcxo_set_req_int);
+
+int cdc_tcxo_set_req_prio(int clk_id, int req_prio)
+{
+	char mask;
+	int ret;
+
+	if (!cdc_info.active) {
+		dev_err(cdc_info.dev, "device is not active yet\n");
+		return -ENODEV;
+	}
+
+	switch (clk_id) {
+	case CDC_TCXO_CLK1:
+		mask = CDC_TCXO_REQ1PRIO;
+		break;
+	case CDC_TCXO_CLK2:
+		mask = CDC_TCXO_REQ2PRIO;
+		break;
+	case CDC_TCXO_CLK3:
+		mask = CDC_TCXO_REQ3PRIO;
+		break;
+	case CDC_TCXO_CLK4:
+		mask = CDC_TCXO_REQ4PRIO;
+		break;
+	default:
+		dev_err(cdc_info.dev, "invalid clk_id: %d\n", clk_id);
+		return -EINVAL;
+	}
+
+	if (req_prio == CDC_TCXO_PRIO_REQINT)
+		cdc_info.buf[CDC_TCXO_REG3] |= mask;
+	else
+		cdc_info.buf[CDC_TCXO_REG3] &= ~mask;
+
+	ret = i2c_master_send(cdc_info.client, cdc_info.buf, CDC_TCXO_REGNUM);
+	if (ret < 0) {
+		dev_err(cdc_info.dev, "failed to write data (%d)\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL(cdc_tcxo_set_req_prio);
+
+static int cdc_tcxo_probe(struct i2c_client *client, \
+				const struct i2c_device_id *id)
+{
+	struct cdc_tcxo_platform_data *pdata = dev_get_platdata(&client->dev);
+	int i, ret;
+
+	cdc_info.client = client;
+	cdc_info.dev = &client->dev;
+
+	for (i = 0; i < CDC_TCXO_REGNUM; i++)
+		cdc_info.buf[i] = pdata->buf[i];
+
+	ret = i2c_master_send(client, cdc_info.buf, CDC_TCXO_REGNUM);
+	if (ret < 0) {
+		dev_err(cdc_info.dev, "failed to initialize registers (%d)\n",
+			ret);
+		return ret;
+	}
+
+	cdc_info.active = 1;
+
+	return 0;
+}
+
+static const struct i2c_device_id cdc_tcxo_id[] = {
+	{"cdc_tcxo_driver", 0},
+	{ }
+};
+
+static struct i2c_driver cdc_tcxo_i2c_driver = {
+	.driver = {
+		.name		= DRIVER_NAME,
+	 },
+	.probe		= cdc_tcxo_probe,
+	.id_table	= cdc_tcxo_id,
+};
+
+static int __init cdc_tcxo_init(void)
+{
+	int r;
+
+	r = i2c_add_driver(&cdc_tcxo_i2c_driver);
+	if (r < 0) {
+		printk(KERN_WARNING DRIVER_NAME
+		       " driver registration failed\n");
+		return r;
+	}
+
+	return 0;
+}
+
+static void __exit cdc_tcxo_exit(void)
+{
+	i2c_del_driver(&cdc_tcxo_i2c_driver);
+}
+
+
+module_init(cdc_tcxo_init);
+module_exit(cdc_tcxo_exit);
+
---
+++ include/linux/cdc_tcxo.h
@@ -0,0 +1,60 @@
+/*
+ * Clock Divider Chip (TCXO) support
+ *
+ * Copyright (C) 2011 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _LINUX_CDC_TCXO_H_
+#define _LINUX_CDC_TCXO_H_
+
+#define CDC_TCXO_REG1		0
+#define CDC_TCXO_REG2		1
+#define CDC_TCXO_REG3		2
+#define CDC_TCXO_REG4		3
+#define CDC_TCXO_REGNUM		4
+
+/* REGISTER1 bit fields */
+
+#define CDC_TCXO_REQ1POL	0x01
+#define CDC_TCXO_REQ2POL	0x02
+#define CDC_TCXO_REQ3POL	0x04
+#define CDC_TCXO_REQ4POL	0x08
+#define CDC_TCXO_REQ1INT	0x10
+#define CDC_TCXO_REQ2INT	0x20
+#define CDC_TCXO_REQ3INT	0x40
+#define CDC_TCXO_REQ4INT	0x80
+
+/* REGISTER2 bit fields */
+
+#define CDC_TCXO_MREQ1		0x10
+#define CDC_TCXO_MREQ2		0x20
+#define CDC_TCXO_MREQ3		0x40
+#define CDC_TCXO_MREQ4		0x80
+
+/* REGISTER3 bit fields */
+
+#define CDC_TCXO_REQ1PRIO	0x01
+#define CDC_TCXO_REQ2PRIO	0x02
+#define CDC_TCXO_REQ3PRIO	0x04
+#define CDC_TCXO_REQ4PRIO	0x08
+
+#define CDC_TCXO_CLK1		1
+#define CDC_TCXO_CLK2		2
+#define CDC_TCXO_CLK3		3
+#define CDC_TCXO_CLK4		4
+
+#define CDC_TCXO_PRIO_REQ	0
+#define CDC_TCXO_PRIO_REQINT	1
+
+struct cdc_tcxo_platform_data {
+	char buf[4];
+};
+
+int cdc_tcxo_set_req_int(int clk_id, int enable);
+int cdc_tcxo_set_req_prio(int clk_id, int req_prio);
+
+#endif /* _LINUX_CDC_TCXO_H_ */
