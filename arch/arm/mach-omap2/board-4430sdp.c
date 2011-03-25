a4e3b3439e4878c1b2c0597fabf02ad284582d26
Rajeev Kulkarni <rajeevk@ti.com>
2011-03-25 07:47:03 -0500

    OMAP4: Update Missing Voltage Rail Values for MPU, IVA and CORE
    This patch  updates the voltage init table which is present
    in board file.
    
    Change-Id: Ic8f810c6a1945084a2e4d318935af9e61272e9fd
    Signed-off-by: Rajeev Kulkarni <rajeevk@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -1564,16 +1564,16 @@ static struct omap_volt_pmic_info omap_pmic_iva = {
 };
 
 static struct omap_volt_vc_data vc_config = {
-	.vdd0_on = 1350000,        /* 1.35v */
-	.vdd0_onlp = 1350000,      /* 1.35v */
+	.vdd0_on = 1375000,        /* 1.375v */
+	.vdd0_onlp = 1375000,      /* 1.375v */
 	.vdd0_ret = 837500,       /* 0.8375v */
 	.vdd0_off = 0,		/* 0 v */
-	.vdd1_on = 1100000,        /* 1.1v */
-	.vdd1_onlp = 1100000,      /* 1.1v */
+	.vdd1_on = 1300000,        /* 1.3v */
+	.vdd1_onlp = 1300000,      /* 1.3v */
 	.vdd1_ret = 837500,       /* 0.8375v */
 	.vdd1_off = 0,		/* 0 v */
-	.vdd2_on = 1100000,        /* 1.1v */
-	.vdd2_onlp = 1100000,      /* 1.1v */
+	.vdd2_on = 1200000,        /* 1.2v */
+	.vdd2_onlp = 1200000,      /* 1.2v */
 	.vdd2_ret = 837500,       /* .8375v */
 	.vdd2_off = 0,		/* 0 v */
 };
