2cf3c35decc02cc198d835596e0393490ef3414e
seonkon.choi <seonkon.choi@ti.com>
2011-04-13 10:20:20 +0900

    OMAP3: HWMOD: Fixed bugs of GPU hwmod.
    The GPU(SGX) has a type2 SYSCONFIG at 0x50000000 + 0xfe10.
    
    Needed for SGX-KM 1.7.17.4403.
    
    Change-Id: Id07801673d2a57858cb22a0fbb9e109cd7d2f2d7
    Signed-off-by: seonkon.choi <seonkon.choi@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
@@ -1887,8 +1887,18 @@ static struct omap_hwmod omap3xxx_gpio6_hwmod = {
  * 2d/3d graphics accelerator
  */
 
+static struct omap_hwmod_class_sysconfig omap3xxx_gpu_sysc = {
+	.rev_offs	= 0xfe00,
+	.sysc_offs	= 0xfe10,
+	.sysc_flags	= (SYSC_HAS_MIDLEMODE | SYSC_HAS_SIDLEMODE),
+	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |
+			   MSTANDBY_FORCE | MSTANDBY_NO | MSTANDBY_SMART),
+	.sysc_fields	= &omap_hwmod_sysc_type2,
+};
+
 static struct omap_hwmod_class omap3xxx_gpu_hwmod_class = {
 	.name = "gpu",
+	.sysc = &omap3xxx_gpu_sysc,
 };
 
 /* gpu */
@@ -1904,7 +1914,7 @@ static struct omap_hwmod_ocp_if *omap3xxx_gpu_masters[] = {
 static struct omap_hwmod_addr_space omap3xxx_gpu_addrs[] = {
 	{
 		.pa_start       = 0x50000000,
-		.pa_end         = 0x50003fff,
+		.pa_end         = 0x5000ffff,
 		.flags          = ADDR_TYPE_RT
 	},
 };
@@ -1913,7 +1923,7 @@ static struct omap_hwmod_addr_space omap3xxx_gpu_addrs[] = {
 static struct omap_hwmod_ocp_if omap3xxx_l3_main__gpu = {
 	.master         = &omap3xxx_l3_main_hwmod,
 	.slave          = &omap3xxx_gpu_hwmod,
-	.clk		= "core_l3_ick",
+	.clk		= "sgx_ick",
 	.addr           = omap3xxx_gpu_addrs,
 	.addr_cnt       = ARRAY_SIZE(omap3xxx_gpu_addrs),
 	.user           = OCP_USER_MPU | OCP_USER_SDMA,
@@ -1929,7 +1939,8 @@ static struct omap_hwmod omap3xxx_gpu_hwmod = {
 	.class          = &omap3xxx_gpu_hwmod_class,
 	.mpu_irqs       = omap3xxx_gpu_irqs,
 	.mpu_irqs_cnt   = ARRAY_SIZE(omap3xxx_gpu_irqs),
-	.flags		= HWMOD_NO_IDLEST,
+	.flags		= HWMOD_NO_IDLEST | HWMOD_NO_OCP_AUTOIDLE,
+	.main_clk	= "sgx_fck",
 	.vdd_name       = "core",
 	.slaves         = omap3xxx_gpu_slaves,
 	.slaves_cnt     = ARRAY_SIZE(omap3xxx_gpu_slaves),
