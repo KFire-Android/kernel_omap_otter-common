38357f5e173ae05b13a5af2357c7217dfa9e2ea8
Nishanth Menon <nm@ti.com>
2011-04-13 00:14:01 -0500

    omap3630: sdp3630 - add support for init of opps
    Change-Id: Ib1256c9259770de7cedb46cd2dd41c9d8be22023
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: jack.miller <jack.miller@ti.com>

--- arch/arm/mach-omap2/board-3630sdp.c
+++ arch/arm/mach-omap2/board-3630sdp.c
@@ -20,6 +20,8 @@
 #include <plat/gpmc-smc91x.h>
 #include <plat/mux.h>
 #include <plat/usb.h>
+#include <plat/opp_twl_tps.h>
+#include <plat/opp.h>
 
 #include <mach/board-zoom.h>
 
@@ -47,6 +49,88 @@ static inline void board_smc91x_init(void)
 }
 
 #endif /* defined(CONFIG_SMC91X) || defined(CONFIG_SMC91X_MODULE) */
+
+#ifdef CONFIG_PM
+static struct omap_volt_vc_data vc_config = {
+	/* MPU */
+	.vdd0_on	= 1200000, /* 1.2v */
+	.vdd0_onlp	= 1000000, /* 1.0v */
+	.vdd0_ret	=  975000, /* 0.975v */
+	.vdd0_off	=  600000, /* 0.6v */
+	/* CORE */
+	.vdd1_on	= 1150000, /* 1.15v */
+	.vdd1_onlp	= 1000000, /* 1.0v */
+	.vdd1_ret	=  975000, /* 0.975v */
+	.vdd1_off	=  600000, /* 0.6v */
+
+	.clksetup	= 0xff,
+	.voltoffset	= 0xff,
+	.voltsetup2	= 0xff,
+	.voltsetup_time1 = 0xfff,
+	.voltsetup_time2 = 0xfff,
+};
+
+#ifdef CONFIG_TWL4030_CORE
+static struct omap_volt_pmic_info omap_pmic_mpu = { /* and iva */
+	.name = "twl",
+	.slew_rate = 4000,
+	.step_size = 12500,
+	.i2c_addr = 0x12,
+	.i2c_vreg = 0x0, /* (vdd0) VDD1 -> VDD1_CORE -> VDD_MPU */
+	.vsel_to_uv = omap_twl_vsel_to_uv,
+	.uv_to_vsel = omap_twl_uv_to_vsel,
+	.onforce_cmd = omap_twl_onforce_cmd,
+	.on_cmd = omap_twl_on_cmd,
+	.sleepforce_cmd = omap_twl_sleepforce_cmd,
+	.sleep_cmd = omap_twl_sleep_cmd,
+	.vp_config_erroroffset = 0,
+	.vp_vstepmin_vstepmin = 0x01,
+	.vp_vstepmax_vstepmax = 0x04,
+	.vp_vlimitto_timeout_us = 0x200,
+	.vp_vlimitto_vddmin = 0x14,
+	.vp_vlimitto_vddmax = 0x44,
+};
+
+static struct omap_volt_pmic_info omap_pmic_core = {
+	.name = "twl",
+	.slew_rate = 4000,
+	.step_size = 12500,
+	.i2c_addr = 0x12,
+	.i2c_vreg = 0x1, /* (vdd1) VDD2 -> VDD2_CORE -> VDD_CORE */
+	.vsel_to_uv = omap_twl_vsel_to_uv,
+	.uv_to_vsel = omap_twl_uv_to_vsel,
+	.onforce_cmd = omap_twl_onforce_cmd,
+	.on_cmd = omap_twl_on_cmd,
+	.sleepforce_cmd = omap_twl_sleepforce_cmd,
+	.sleep_cmd = omap_twl_sleep_cmd,
+	.vp_config_erroroffset = 0,
+	.vp_vstepmin_vstepmin = 0x01,
+	.vp_vstepmax_vstepmax = 0x04,
+	.vp_vlimitto_timeout_us = 0x200,
+	.vp_vlimitto_vddmin = 0x18,
+	.vp_vlimitto_vddmax = 0x42,
+};
+#endif /* CONFIG_TWL4030_CORE */
+#endif /* CONFIG_PM */
+
+/* OPP MPU/IVA Clock Frequency */
+struct opp_frequencies {
+	unsigned long mpu;
+	unsigned long iva;
+};
+
+static struct opp_frequencies opp_freq_add_table[] __initdata = {
+  {
+	.mpu = 1000000000,
+	.iva =  800000000,
+  },
+  {
+	.mpu = 1200000000,
+	.iva =   65000000,
+  },
+
+  { 0, 0 },
+};
 
 static void enable_board_wakeup_source(void)
 {
@@ -102,7 +186,90 @@ static void __init omap_sdp_init(void)
 	enable_board_wakeup_source();
 	usb_uhhtll_init(&usbhs_pdata);
 	sr_class3_init();
+#ifdef CONFIG_PM
+#ifdef CONFIG_TWL4030_CORE
+	omap_voltage_register_pmic(&omap_pmic_core, "core");
+	omap_voltage_register_pmic(&omap_pmic_mpu, "mpu");
+#endif
+	omap_voltage_init_vc(&vc_config);
+#endif
 }
+/* must be called after omap2_common_pm_init() */
+static int __init sdp3630_opp_init(void)
+{
+	struct omap_hwmod *mh, *dh;
+	struct omap_opp *mopp, *dopp;
+	struct device *mdev, *ddev;
+	struct opp_frequencies *opp_freq;
+
+
+	if (!cpu_is_omap3630())
+		return 0;
+
+	mh = omap_hwmod_lookup("mpu");
+	if (!mh || !mh->od) {
+		pr_err("%s: no MPU hwmod device.\n", __func__);
+		return 0;
+	}
+
+	dh = omap_hwmod_lookup("iva");
+	if (!dh || !dh->od) {
+		pr_err("%s: no DSP hwmod device.\n", __func__);
+		return 0;
+	}
+
+	mdev = &mh->od->pdev.dev;
+	ddev = &dh->od->pdev.dev;
+
+	/* add MPU and IVA clock frequencies */
+	for (opp_freq = opp_freq_add_table; opp_freq->mpu; opp_freq++) {
+		/* check enable/disable status of MPU frequecy setting */
+		mopp = opp_find_freq_exact(mdev, opp_freq->mpu, false);
+		if (IS_ERR(mopp))
+			mopp = opp_find_freq_exact(mdev, opp_freq->mpu, true);
+		if (IS_ERR(mopp)) {
+			pr_err("%s: MPU does not support %lu MHz\n", __func__, opp_freq->mpu / 1000000);
+			continue;
+		}
+
+		/* check enable/disable status of IVA frequency setting */
+		dopp = opp_find_freq_exact(ddev, opp_freq->iva, false);
+		if (IS_ERR(dopp))
+			dopp = opp_find_freq_exact(ddev, opp_freq->iva, true);
+		if (IS_ERR(dopp)) {
+			pr_err("%s: DSP does not support %lu MHz\n", __func__, opp_freq->iva / 1000000);
+			continue;
+		}
+
+		/* try to enable MPU frequency setting */
+		if (opp_enable(mopp)) {
+			pr_err("%s: OPP cannot enable MPU:%lu MHz\n", __func__, opp_freq->mpu / 1000000);
+			continue;
+		}
+
+		/* try to enable IVA frequency setting */
+		if (opp_enable(dopp)) {
+			pr_err("%s: OPP cannot enable DSP:%lu MHz\n", __func__, opp_freq->iva / 1000000);
+			opp_disable(mopp);
+			continue;
+		}
+
+		/* verify that MPU and IVA frequency settings are available */
+		mopp = opp_find_freq_exact(mdev, opp_freq->mpu, true);
+		dopp = opp_find_freq_exact(ddev, opp_freq->iva, true);
+		if (!mopp || !dopp) {
+			pr_err("%s: OPP requested MPU: %lu MHz and DSP: %lu MHz not found\n",
+				__func__, opp_freq->mpu / 1000000, opp_freq->iva / 1000000);
+			continue;
+		}
+
+		dev_info(mdev, "OPP enabled %lu MHz\n", opp_freq->mpu / 1000000);
+		dev_info(ddev, "OPP enabled %lu MHz\n", opp_freq->iva / 1000000);
+	}
+
+	return 0;
+}
+device_initcall(sdp3630_opp_init);
 
 MACHINE_START(OMAP_3630SDP, "OMAP 3630SDP board")
 	.phys_io	= 0x48000000,
