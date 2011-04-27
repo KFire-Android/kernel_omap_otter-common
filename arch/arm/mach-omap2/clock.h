bb93990f631f28d35f053f106395cee9b447cb9c
Mike Turquette <mturquette@ti.com>
2011-04-26 19:06:51 -0500

    OMAP3+: clock: bypassed DPLL clkout recalc
    The output clocks for bypassed DPLLs follow the bypass rate without MN
    dividers applied.  Simply using omap2_clksel_recalc for the recalc function
    of these clocks will cause the bypassed rates to have the divider programmed
    in HW to be applied to that rate, which is wrong.  Patch introduces a new
    recalc function meant to handle these clocks specifically, and replaces the
    .recalc function pointer of each of the affected clocks.
    
    Change-Id: I84f852cce6e10af2a8242d1982363ac091377fe9
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/clock.h
+++ arch/arm/mach-omap2/clock.h
@@ -61,6 +61,7 @@ int omap2_dpll_set_rate_tolerance(struct clk *clk, unsigned int tolerance);
 long omap2_dpll_round_rate(struct clk *clk, unsigned long target_rate);
 unsigned long omap3_dpll_recalc(struct clk *clk);
 unsigned long omap3_clkoutx2_recalc(struct clk *clk);
+unsigned long omap3_clkout_mn_recalc(struct clk *clk);
 void omap3_dpll_allow_idle(struct clk *clk);
 void omap3_dpll_deny_idle(struct clk *clk);
 u32 omap3_dpll_autoidle_read(struct clk *clk);
