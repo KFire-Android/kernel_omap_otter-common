af30de36db7791c044d67f19fa9d263b2e748a13
Colin Cross <ccross@android.com>
2011-07-12 19:53:24 -0700

    cgroup: Add generic cgroup subsystem permission checks
    Rather than using explicit euid == 0 checks when trying to move
    tasks into a cgroup via CFS, move permission checks into each
    specific cgroup subsystem. If a subsystem does not specify a
    'allow_attach' handler, then we fall back to doing our checks
    the old way.
    
    Use the 'allow_attach' handler for the 'cpu' cgroup to allow
    non-root processes to add arbitrary processes to a 'cpu' cgroup
    if it has the CAP_SYS_NICE capability set.
    
    This version of the patch adds a 'allow_attach' handler instead
    of reusing the 'can_attach' handler.  If the 'can_attach' handler
    is reused, a new cgroup that implements 'can_attach' but not
    the permission checks could end up with no permission checks
    at all.
    
    Change-Id: Icfa950aa9321d1ceba362061d32dc7dfa2c64f0c
    Original-Author: San Mehat <san@google.com>
    Signed-off-by: Colin Cross <ccross@android.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7582,6 +7582,23 @@ static void cpu_cgroup_css_free(struct cgroup *cgrp)
 	sched_destroy_group(tg);
 }
 
+static int
+cpu_cgroup_allow_attach(struct cgroup *cgrp, struct cgroup_taskset *tset)
+{
+	const struct cred *cred = current_cred(), *tcred;
+	struct task_struct *task;
+
+	cgroup_taskset_for_each(task, cgrp, tset) {
+		tcred = __task_cred(task);
+
+		if ((current != task) && !capable(CAP_SYS_NICE) &&
+		    cred->euid != tcred->uid && cred->euid != tcred->suid)
+			return -EACCES;
+	}
+
+	return 0;
+}
+
 static int cpu_cgroup_can_attach(struct cgroup *cgrp,
 				 struct cgroup_taskset *tset)
 {
@@ -7939,6 +7956,7 @@ struct cgroup_subsys cpu_cgroup_subsys = {
 	.css_free	= cpu_cgroup_css_free,
 	.can_attach	= cpu_cgroup_can_attach,
 	.attach		= cpu_cgroup_attach,
+	.allow_attach	= cpu_cgroup_allow_attach,
 	.exit		= cpu_cgroup_exit,
 	.subsys_id	= cpu_cgroup_subsys_id,
 	.base_cftypes	= cpu_files,
