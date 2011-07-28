87c677d3bb48433e91f437b9747dabdea8a74038
Lajos Molnar <lajos@ti.com>
2011-07-27 20:57:10 -0500

    OMAPDSS: DISPC: Fix buffer underflow err on downscaling
    When downscaling heavily, FIFO buffers may underflow at the first
    line.  Set DMA preload value to the high threshold to avoid this.
    
    Change-Id: Ic791f48a252d82b2ba514f16c9e824d25e96fa44
    Signed-off-by: Lajos Molnar <lajos@ti.com>
    Signed-off-by: Varadarajan, Charulatha <charu@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -1031,6 +1031,9 @@ void dispc_ovl_set_fifo_threshold(enum omap_plane plane, u32 low, u32 high)
 				hi_start, hi_end) * unit,
 			low * unit, high * unit);
 
+	/* preload to high threshold to avoid FIFO underflow */
+	dispc_write_reg(DISPC_OVL_PRELOAD(plane), min(high, 0xfffu));
+
 	dispc_write_reg(DISPC_OVL_FIFO_THRESHOLD(plane),
 			FLD_VAL(high, hi_start, hi_end) |
 			FLD_VAL(low, lo_start, lo_end));
