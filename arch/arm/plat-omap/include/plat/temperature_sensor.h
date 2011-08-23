d2ef387b0e3bf0127f8cd07c577b242b9062b0b8
Dan Murphy <dmurphy@ti.com>
2011-08-23 13:32:49 -0500

    Add new ifdef for temp sensor idle
    Added new ifdef for temp sensor idle.
    Since the driver in drivers/misc and the staging
    temp driver share the same API we need to differeniate
    the two calls
    
    Change-Id: I69470bcb4327a36980be831dc1a7776c8bf7426c
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/plat-omap/include/plat/temperature_sensor.h
+++ arch/arm/plat-omap/include/plat/temperature_sensor.h
@@ -56,7 +56,8 @@ static inline void omap_temp_sensor_resume_idle(void) { }
 static inline void omap_temp_sensor_prepare_idle(void) { }
 #endif
 
-#ifdef CONFIG_OMAP_DIE_TEMP_SENSOR
+#if defined (CONFIG_OMAP_DIE_TEMP_SENSOR) || \
+	defined(CONFIG_OMAP4_DIE_TEMP_SENSOR)
 void omap_temp_sensor_idle(int idle_state);
 #else
 static inline void omap_temp_sensor_idle(int idle_state) { }
