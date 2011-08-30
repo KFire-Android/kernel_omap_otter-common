d06d3c0187b5c6d71c2f8ccaed5d76fd9047224a
Abraham Varghese <av@ti.com>
2011-08-30 17:00:00 +0530

    OMAP5 : DSS : HDMI : hack patch for HDMI in OMAP5 hwmod
    The class name has to be prefixed with dss unlike OMAP4 in the OMAP5hwmod this
    is a hack patch for now.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_54xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_54xx_data.c
@@ -1617,7 +1617,7 @@ static struct omap_hwmod_class_sysconfig omap54xx_hdmi_sysc = {
 };
 
 static struct omap_hwmod_class omap54xx_hdmi_hwmod_class = {
-	.name	= "hdmi",
+	.name	= "dss_hdmi",
 	.sysc	= &omap54xx_hdmi_sysc,
 };
 
