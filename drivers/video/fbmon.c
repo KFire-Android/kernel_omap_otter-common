d614218181095958bfd100978d7cb32a15eef01b
Erik Gilling <konkers@android.com>
2011-09-13 13:44:44 -0700

    video: fbmon: add remaining modes to CEA mode database
    These mode are directly from the CEA-861-E spec
    Table 3 Video Format Timings Detailed Sync Information.
    
    Change-Id: If93dcc037f522473cc22fcc9b534ca4c3c28cc5a
    Signed-off-by: Erik Gilling <konkers@android.com>
    Signed-off-by: Muralidhar Dixit <murali.dixit@ti.com>

--- drivers/video/fbmon.c
+++ drivers/video/fbmon.c
@@ -1050,9 +1050,9 @@ void fb_edid_add_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 
 	for (i = specs->modedb_len + num; i < specs->modedb_len + num + svd_n; i++) {
 		int idx = svd[i - specs->modedb_len - num];
-		if (!idx || idx > 63) {
+		if (!idx || idx > (CEA_MODEDB_SIZE - 1)) {
 			pr_warning("Reserved SVD code %d\n", idx);
-		} else if (idx > ARRAY_SIZE(cea_modes) || !cea_modes[idx].xres) {
+		} else if (!cea_modes[idx].xres) {
 			pr_warning("Unimplemented SVD code %d\n", idx);
 		} else {
 			memcpy(&m[i], cea_modes + idx, sizeof(m[i]));
