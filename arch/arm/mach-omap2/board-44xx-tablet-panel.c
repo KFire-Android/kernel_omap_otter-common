9c7668d3f72845356ea3ec3712431e250b121fa4
Murali Dixit <murali.dixit@ti.com>
2011-09-22 19:38:30 +0530

    OMAP4 : HDMI : Configure GPIO for HDMI HPD detection for blaze tablet
    Adding configuration of GPIO for HDMI HPD detection for balze tablet.
    GPIO 63 is used to detect the HPD in blaze.
    
    Change-Id: I7289f66a79c00a4d285689eb5ab47930d185ab5e
    Signed-off-by: Muralidhar Dixit <murali.dixit@ti.com>
    Signed-off-by: Sujeet Baranwal <s-baranwal@ti.com>

--- arch/arm/mach-omap2/board-44xx-tablet-panel.c
+++ arch/arm/mach-omap2/board-44xx-tablet-panel.c
@@ -40,13 +40,18 @@
 #define LED_DISP_EN		102
 #define DSI2_GPIO_59		59
 
-#define HDMI_GPIO_HPD 60 /* Hot plug pin for HDMI */
+#define HDMI_GPIO_CT_CP_HPD             60
+#define HDMI_GPIO_HPD                   63  /* Hot plug pin for HDMI */
+
 #define HDMI_GPIO_LS_OE 41 /* Level shifter for HDMI */
 #define LCD_BL_GPIO		59 /*27*/	/* LCD Backlight GPIO */
 /* PWM2 and TOGGLE3 register offsets */
 #define LED_PWM2ON		0x03
 #define LED_PWM2OFF		0x04
 #define TWL6030_TOGGLE3		0x92
+
+#define OMAP_HDMI_HPD_ADDR      0x4A100098
+#define OMAP_HDMI_PULLTYPE_MASK 0x00000010
 
 
 static void __init omap4_tablet_init_display_led(void)
@@ -97,42 +102,43 @@ static struct platform_device omap4_tablet_disp_led = {
 	},
 };
 
-static void tablet_hdmi_mux_init(void)
-{
-	/* PAD0_HDMI_HPD_PAD1_HDMI_CEC */
-	omap_mux_init_signal("hdmi_hpd",
-			OMAP_PIN_INPUT_PULLUP);
-	omap_mux_init_signal("hdmi_cec",
-			OMAP_PIN_INPUT_PULLUP);
-	/* PAD0_HDMI_DDC_SCL_PAD1_HDMI_DDC_SDA */
-	omap_mux_init_signal("hdmi_ddc_scl",
-			OMAP_PIN_INPUT_PULLUP);
-	omap_mux_init_signal("hdmi_ddc_sda",
-			OMAP_PIN_INPUT_PULLUP);
-}
-
 static struct gpio tablet_hdmi_gpios[] = {
-	{ HDMI_GPIO_HPD,	GPIOF_OUT_INIT_HIGH,	"hdmi_gpio_hpd"   },
-	{ HDMI_GPIO_LS_OE,	GPIOF_OUT_INIT_HIGH,	"hdmi_gpio_ls_oe" },
+	{HDMI_GPIO_CT_CP_HPD,  GPIOF_OUT_INIT_HIGH,    "hdmi_gpio_hpd"   },
+	{HDMI_GPIO_LS_OE,      GPIOF_OUT_INIT_HIGH,    "hdmi_gpio_ls_oe" },
 };
 
-static int tablet_panel_enable_hdmi(struct omap_dss_device *dssdev)
+static void tablet_hdmi_mux_init(void)
 {
+	u32 r;
 	int status;
+	/* PAD0_HDMI_HPD_PAD1_HDMI_CEC */
+	omap_mux_init_signal("hdmi_hpd.hdmi_hpd",
+				OMAP_PIN_INPUT_PULLDOWN);
+	omap_mux_init_signal("gpmc_wait2.gpio_100",
+			OMAP_PIN_INPUT_PULLDOWN);
+	omap_mux_init_signal("hdmi_cec.hdmi_cec",
+			OMAP_PIN_INPUT_PULLUP);
+	/* PAD0_HDMI_DDC_SCL_PAD1_HDMI_DDC_SDA */
+	omap_mux_init_signal("hdmi_ddc_scl.hdmi_ddc_scl",
+			OMAP_PIN_INPUT_PULLUP);
+	omap_mux_init_signal("hdmi_ddc_sda.hdmi_ddc_sda",
+			OMAP_PIN_INPUT_PULLUP);
+
+	/* strong pullup on DDC lines using unpublished register */
+	r = ((1 << 24) | (1 << 28)) ;
+	omap4_ctrl_pad_writel(r, OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_I2C_1);
+
+	gpio_request(HDMI_GPIO_HPD, NULL);
+	omap_mux_init_gpio(HDMI_GPIO_HPD, OMAP_PIN_INPUT | OMAP_PULL_ENA);
+	gpio_direction_input(HDMI_GPIO_HPD);
 
 	status = gpio_request_array(tablet_hdmi_gpios,
-				    ARRAY_SIZE(tablet_hdmi_gpios));
+			ARRAY_SIZE(tablet_hdmi_gpios));
 	if (status)
-		pr_err("%s: Cannot request HDMI GPIOs\n", __func__);
-
-	return status;
+		pr_err("%s: Cannot request HDMI GPIOs %x \n", __func__, status);
 }
 
-static void tablet_panel_disable_hdmi(struct omap_dss_device *dssdev)
-{
-	gpio_free(HDMI_GPIO_LS_OE);
-	gpio_free(HDMI_GPIO_HPD);
-}
+
 
 static struct tc358765_board_data tablet_dsi_panel = {
 	.x_res		= 1280,
@@ -200,8 +206,7 @@ static struct omap_dss_device tablet_hdmi_device = {
 			.regm2	= 1,
 		},
 	},
-	.platform_enable = tablet_panel_enable_hdmi,
-	.platform_disable = tablet_panel_disable_hdmi,
+	.hpd_gpio = HDMI_GPIO_HPD,
 	.channel = OMAP_DSS_CHANNEL_DIGIT,
 };
 
