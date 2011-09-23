95d6aff36b02cd77f859e4fe27a9c549ab27330b
G, Manjunath Kondaiah <manjugk@ti.com>
2011-09-23 18:51:34 +0530

    omap5: evm: Add omap5 onchip keypad board data
        Enable qwerty keypad interface for omap5.
        Update omap5 board board with key mapping specific data.
    
    Signed-off-by: G, Manjunath Kondaiah <manjugk@ti.com>
    Signed-off-by: Sourav Poddar <sourav.poddar@ti.com>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -16,6 +16,9 @@
 #include <linux/platform_device.h>
 #include <linux/io.h>
 #include <linux/gpio.h>
+#include <linux/input.h>
+#include <linux/input/matrix_keypad.h>
+#include <linux/platform_data/omap4-keypad.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
@@ -25,8 +28,98 @@
 #include "common.h"
 #include <asm/hardware/gic.h>
 #include <plat/common.h>
+#include <plat/omap4-keypad.h>
 #include "common-board-devices.h"
 #include "mux.h"
+
+static const int evm5430_keymap[] = {
+	KEY(0, 0, KEY_RESERVED),
+	KEY(0, 1, KEY_RESERVED),
+	KEY(0, 2, KEY_RESERVED),
+	KEY(0, 3, KEY_RESERVED),
+	KEY(0, 4, KEY_RESERVED),
+	KEY(0, 5, KEY_RESERVED),
+	KEY(0, 6, KEY_RESERVED),
+	KEY(0, 7, KEY_RESERVED),
+
+	KEY(1, 0, KEY_RESERVED),
+	KEY(1, 1, KEY_RESERVED),
+	KEY(1, 2, KEY_RESERVED),
+	KEY(1, 3, KEY_RESERVED),
+	KEY(1, 4, KEY_RESERVED),
+	KEY(1, 5, KEY_RESERVED),
+	KEY(1, 6, KEY_RESERVED),
+	KEY(1, 7, KEY_RESERVED),
+
+	KEY(2, 0, KEY_RESERVED),
+	KEY(2, 1, KEY_RESERVED),
+	KEY(2, 2, KEY_VOLUMEUP),
+	KEY(2, 3, KEY_VOLUMEDOWN),
+	KEY(2, 4, KEY_SEND),
+	KEY(2, 5, KEY_HOME),
+	KEY(2, 6, KEY_END),
+	KEY(2, 7, KEY_SEARCH),
+
+	KEY(3, 0, KEY_RESERVED),
+	KEY(3, 1, KEY_RESERVED),
+	KEY(3, 2, KEY_RESERVED),
+	KEY(3, 3, KEY_RESERVED),
+	KEY(3, 4, KEY_RESERVED),
+	KEY(3, 5, KEY_RESERVED),
+	KEY(3, 6, KEY_RESERVED),
+	KEY(3, 7, KEY_RESERVED),
+
+	KEY(4, 0, KEY_RESERVED),
+	KEY(4, 1, KEY_RESERVED),
+	KEY(4, 2, KEY_RESERVED),
+	KEY(4, 3, KEY_RESERVED),
+	KEY(4, 4, KEY_RESERVED),
+	KEY(4, 5, KEY_RESERVED),
+	KEY(4, 6, KEY_RESERVED),
+	KEY(4, 7, KEY_RESERVED),
+
+	KEY(5, 0, KEY_RESERVED),
+	KEY(5, 1, KEY_RESERVED),
+	KEY(5, 2, KEY_RESERVED),
+	KEY(5, 3, KEY_RESERVED),
+	KEY(5, 4, KEY_RESERVED),
+	KEY(5, 5, KEY_RESERVED),
+	KEY(5, 6, KEY_RESERVED),
+	KEY(5, 7, KEY_RESERVED),
+
+	KEY(6, 0, KEY_RESERVED),
+	KEY(6, 1, KEY_RESERVED),
+	KEY(6, 2, KEY_RESERVED),
+	KEY(6, 3, KEY_RESERVED),
+	KEY(6, 4, KEY_RESERVED),
+	KEY(6, 5, KEY_RESERVED),
+	KEY(6, 6, KEY_RESERVED),
+	KEY(6, 7, KEY_RESERVED),
+
+	KEY(7, 0, KEY_RESERVED),
+	KEY(7, 1, KEY_RESERVED),
+	KEY(7, 2, KEY_RESERVED),
+	KEY(7, 3, KEY_RESERVED),
+	KEY(7, 4, KEY_RESERVED),
+	KEY(7, 5, KEY_RESERVED),
+	KEY(7, 6, KEY_RESERVED),
+	KEY(7, 7, KEY_RESERVED),
+};
+
+static struct matrix_keymap_data evm5430_keymap_data = {
+	.keymap                 = evm5430_keymap,
+	.keymap_size            = ARRAY_SIZE(evm5430_keymap),
+};
+
+static struct omap4_keypad_platform_data evm5430_keypad_data = {
+	.keymap_data            = &evm5430_keymap_data,
+	.rows                   = 8,
+	.cols                   = 8,
+};
+
+static struct omap_board_data keypad_data = {
+	.id                     = 1,
+};
 
 #ifdef CONFIG_OMAP_MUX
 static struct omap_board_mux board_mux[] __initdata = {
@@ -47,7 +140,7 @@ static struct __devinitdata emif_custom_configs custom_configs = {
 
 static void __init omap_5430evm_init(void)
 {
-
+	int status;
 #if defined(CONFIG_TI_EMIF) || defined(CONFIG_TI_EMIF_MODULE)
 ifndef CONFIG_MACH_OMAP_5430ZEBU
 	omap_emif_set_device_details(1, &lpddr2_elpida_4G_S4_x2_info,
@@ -65,6 +158,9 @@ ifndef CONFIG_MACH_OMAP_5430ZEBU
 	omap5_mux_init(board_mux, NULL, OMAP_PACKAGE_CBL);
 	omap_sdrc_init(NULL, NULL);
 	omap_serial_init();
+	status = omap4_keyboard_init(&evm5430_keypad_data, &keypad_data);
+	if (status)
+		pr_err("Keypad initialization failed: %d\n", status);
 }
 
 MACHINE_START(OMAP5_SEVM, "OMAP5430 evm board")
