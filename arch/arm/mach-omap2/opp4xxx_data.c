22216bb6cd46716b1356324f6080ccf825b3816e
Rajeev Kulkarni <rajeevk@ti.com>
2011-10-06 13:46:47 -0500

    Revert " HACK: omap4: opp: do not use MPU 1.5 GHz OPP"
    This reverts commit b030e29d6fef6ea3fffde1c4836f8d6931b99ed9.
    
    By Reverting we will enable 1.5Ghz Opp for 4460 Silicon.
    
    Change-Id: Id2ce115965cdb0e485da762d5126b4b7d636fbca
    Signed-off-by: Rajeev Kulkarni <rajeevk@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -324,8 +324,8 @@ int __init omap4_opp_init(void)
 	if (!r) {
 		if (omap4_has_mpu_1_2ghz())
 			omap4_mpu_opp_enable(1200000000);
-/*		if (omap4_has_mpu_1_5ghz())
-			omap4_mpu_opp_enable(1500000000);*/
+		if (omap4_has_mpu_1_5ghz())
+			omap4_mpu_opp_enable(1500000000);
 	}
 
 	return r;
