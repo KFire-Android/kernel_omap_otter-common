9aa3c421295428ac7f51dc64a7da9df3fa4e0a86
Dan Murphy <dmurphy@ti.com>
2011-10-11 14:27:55 -0500

    ASoC: add support for panda machine in sdp4430 driver
    Panda device introduced and need to add checks for
    tablet so HF playback and HS detection will work.
    
    Change-Id: I813d07f841994a7271464616fe472e47fdb54d03
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -510,7 +510,8 @@ static int sdp4430_twl6040_init(struct snd_soc_pcm_runtime *rtd)
 	ret = snd_soc_jack_add_pins(&hs_jack, ARRAY_SIZE(hs_jack_pins),
 				hs_jack_pins);
 
-	if (machine_is_omap_4430sdp() || machine_is_omap_tabletblaze())
+	if (machine_is_omap_4430sdp() || machine_is_omap_tabletblaze()
+		|| machine_is_omap4_panda())
 		twl6040_hs_jack_detect(codec, &hs_jack, SND_JACK_HEADSET);
 	else
 		snd_soc_jack_report(&hs_jack, SND_JACK_HEADSET, SND_JACK_HEADSET);
