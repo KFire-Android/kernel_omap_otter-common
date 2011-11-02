53f33c7f933250151402f5ad1b5dcb16b7f7bc45
Axel Castaneda Gonzalez <x0055901@ti.com>
2011-11-02 11:40:43 -0500

    ASoC: sdp4430: Use default McBSP configuration for legacy DAIs
    McBSP was only configured for ABE backend DAIs (OMAP_ABE_DAI_BT_VX and
    OMAP_ABE_DAI_MM_FM), but for legacy DAI links, DAI hardware and clocking
    was not configured. Use default settings for McBSP legacy DAIs.
    
    Change-Id: I87082af46e88678b830cdf9d1ae5f62e08d4c947
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>
    Signed-off-by: Axel Castaneda Gonzalez <x0055901@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -192,20 +192,19 @@ static int sdp4430_mcbsp_hw_params(struct snd_pcm_substream *substream,
 	int ret = 0;
 	unsigned int be_id, channels;
 
+	be_id = rtd->dai_link->be_id;
 
-        be_id = rtd->dai_link->be_id;
-
-	if (be_id == OMAP_ABE_DAI_MM_FM) {
+	 if (be_id == OMAP_ABE_DAI_BT_VX) {
+		ret = snd_soc_dai_set_fmt(cpu_dai,
+			SND_SOC_DAIFMT_DSP_B |
+			SND_SOC_DAIFMT_NB_IF |
+			SND_SOC_DAIFMT_CBM_CFM);
+	} else {
 		/* Set cpu DAI configuration */
 		ret = snd_soc_dai_set_fmt(cpu_dai,
 				  SND_SOC_DAIFMT_I2S |
 				  SND_SOC_DAIFMT_NB_NF |
 				  SND_SOC_DAIFMT_CBM_CFM);
-	} else if (be_id == OMAP_ABE_DAI_BT_VX) {
-	        ret = snd_soc_dai_set_fmt(cpu_dai,
-                                  SND_SOC_DAIFMT_DSP_B |
-                                  SND_SOC_DAIFMT_NB_IF |
-                                  SND_SOC_DAIFMT_CBM_CFM);
 	}
 
 	if (ret < 0) {
@@ -275,10 +274,11 @@ static int mcbsp_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
                                        SNDRV_PCM_HW_PARAM_CHANNELS);
 	unsigned int be_id = rtd->dai_link->be_id;
 
-	if (be_id == OMAP_ABE_DAI_MM_FM)
-		channels->min = 2;
-	else if (be_id == OMAP_ABE_DAI_BT_VX)
+	if (be_id == OMAP_ABE_DAI_BT_VX)
 		channels->min = 1;
+	else
+		channels->min = 2;
+
 	snd_mask_set(&params->masks[SNDRV_PCM_HW_PARAM_FORMAT -
 	                            SNDRV_PCM_HW_PARAM_FIRST_MASK],
 	                            SNDRV_PCM_FORMAT_S16_LE);
