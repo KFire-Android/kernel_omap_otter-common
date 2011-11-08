2cb4b7938111db336aa8a79182ac07be8927d959
Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>
2011-11-07 18:49:58 -0600

    OMAP4: HACK: disable OPP 1.5 GHz on untrimmed units.
    Noticed device reboots with untrimmed 1.5 GHz units.
    Temporary workaround to restrict the max OPP to 1.2 GHz
    to make it functional.
    
    Change-Id: Ia254e34893e008b4a966e5987bdb7b0099793ebc
    Signed-off-by: Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -333,7 +333,9 @@ int __init omap4_opp_init(void)
 	if (!r) {
 		if (omap4_has_mpu_1_2ghz())
 			omap4_mpu_opp_enable(1200000000);
-		if (omap4_has_mpu_1_5ghz())
+		if (!trimmed)
+			pr_info("This is DPLL un-trimmed SOM. OPP is limited at 1.2 GHz\n");
+		if (omap4_has_mpu_1_5ghz() && trimmed)
 			omap4_mpu_opp_enable(1500000000);
 	}
 
