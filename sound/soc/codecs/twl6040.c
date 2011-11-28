6b03b321487f52edd563dbb706f49dd69709d5a2
Sebastien Guiriec <s-guiriec@ti.com>
2011-11-28 21:32:26 +0100

    ASoC: TWL6040: HACK for ABE support of 44.1 kHz
    Temporary hack to set McPDM CLK at 96 kHz for all the mode. With
    updated ABE the FW is doing the SRC from 44.1 to 48 internally.
    Previous custom mode needs to be removed.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- sound/soc/codecs/twl6040.c
+++ sound/soc/codecs/twl6040.c
@@ -191,8 +191,10 @@ static unsigned int lp_rates[] = {
 
 static unsigned int hp_rates[] = {
 	8000,
+	11250,
 	16000,
-	32000,
+	22500,
+	44100,
 	48000,
 	96000,
 };
@@ -1366,9 +1368,6 @@ static int twl6040_hw_params(struct snd_pcm_substream *substream,
 
 	rate = params_rate(params);
 	switch (rate) {
-	case 11250:
-	case 22500:
-	case 44100:
 	case 88200:
 		/* These rates are not supported when HPPLL is in use */
 		if (unlikely(priv->pll == TWL6040_SYSCLK_SEL_HPPLL)) {
@@ -1379,8 +1378,11 @@ static int twl6040_hw_params(struct snd_pcm_substream *substream,
 		priv->sysclk = 17640000;
 		break;
 	case 8000:
+	case 11250:
 	case 16000:
+	case 22500:
 	case 32000:
+	case 44100:
 	case 48000:
 	case 96000:
 		priv->sysclk = 19200000;
