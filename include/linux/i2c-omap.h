38a874b5e526dd965029227aba4176b2d66a6288
Shubhrajyoti D <shubhrajyoti@ti.com>
2011-12-02 14:51:12 +0530

    OMAP: I2C: Reset support
    Under some error conditions the i2c driver may do a reset.
    Adding a reset field and support in the device-specific code.
    
    Change-Id: I1093774aa6ba419da92047f28a087b2d29ff6750
    Signed-off-by: Shubhrajyoti D <shubhrajyoti@ti.com>
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>

--- include/linux/i2c-omap.h
+++ include/linux/i2c-omap.h
@@ -21,6 +21,7 @@ struct omap_i2c_bus_platform_data {
 	int		(*device_enable) (struct platform_device *pdev);
 	int		(*device_shutdown) (struct platform_device *pdev);
 	int		(*device_idle) (struct platform_device *pdev);
+	int		(*device_reset) (struct device *dev);
 	struct		hwspinlock *handle;
 	int		(*hwspin_lock_timeout)(struct hwspinlock *hwlock,
 							unsigned int to);
