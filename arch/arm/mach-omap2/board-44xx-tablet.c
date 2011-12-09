fa69444a0688eeb6e6ed9b8118d95aa3dc883a3f
Ambresh K <ambresh@ti.com>
2011-12-09 16:25:48 +0530

    OMAP4: Tablet: Regulator: Fix warning from suspend prepare.
    [   38.568176] suspend_set_state: VANA: No configuration
    [   38.574432] suspend_set_state: VAUX3_6030: No configuration
    [   38.580657] suspend_set_state: VAUX2_6030: No configuration
    [   38.586791] suspend_set_state: VAUX1_6030: No configuration
    [   38.594024] suspend_set_state: VUSIM: No configuration
    [   38.599853] suspend_set_state: VMMC: No configuration
    
    VMMC, VAUX1, VAUX2 to be configured to appropriate state after
    consulting domain owners.
    
    Change-Id: I1b1f2bed201a3ca8c0613566946b6583d41e668a
    Signed-off-by: Ambresh K <ambresh@ti.com>

--- arch/arm/mach-omap2/board-44xx-tablet.c
+++ arch/arm/mach-omap2/board-44xx-tablet.c
@@ -373,6 +373,10 @@ static struct regulator_init_data tablet_vaux3 = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_VOLTAGE
 					| REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
+		.state_mem = {
+			.disabled       = true,
+		},
+		.initial_state          = PM_SUSPEND_MEM,
 	},
 };
 
@@ -419,6 +423,10 @@ static struct regulator_init_data tablet_vusim = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_VOLTAGE
 					| REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
+		.state_mem = {
+			.disabled       = true,
+		},
+		.initial_state          = PM_SUSPEND_MEM,
 	},
 };
 
@@ -431,6 +439,10 @@ static struct regulator_init_data tablet_vana = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 		.always_on		= true,
+		.state_mem = {
+			.enabled	= true,
+		},
+		.initial_state          = PM_SUSPEND_MEM,
 	},
 };
 
