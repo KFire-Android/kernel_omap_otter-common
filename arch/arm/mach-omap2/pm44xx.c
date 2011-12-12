1fdcb33c9a869395923b2cefcee58da3dbdd6b39
Volodymyr Riazantsev <v.riazantsev@ti.com>
2011-12-12 02:20:06 +0200

    OMAP4: PM: Correct powerstates initialization sequence
      During initialization of new powerstate item it
    should be added into list before it's configuration.
    
      Initialization of powerstates skiped for CORE, MPU,
    CPU0, CPU1 powerdomains. Current implementation caused
    incorrect initialization of powerstate at least for
    GFX PWRDM because it masked by CORE PWRDM.
    
    We should add powerstate node to list before it will
    been initialized.
    
    Change-Id: I2f9274a9ec951eb9f6b54e104a0e14921b61264e
    Signed-off-by: Volodymyr Riazantsev <v.riazantsev@ti.com>

--- arch/arm/mach-omap2/pm44xx.c
+++ arch/arm/mach-omap2/pm44xx.c
@@ -879,6 +879,9 @@ static int __init pwrdms_setup(struct powerdomain *pwrdm, void *unused)
 #endif
 
 	pwrst->pwrdm = pwrdm;
+
+	list_add(&pwrst->node, &pwrst_list);
+
 	if ((!strcmp(pwrdm->name, "mpu_pwrdm")) ||
 			(!strcmp(pwrdm->name, "core_pwrdm")) ||
 			(!strcmp(pwrdm->name, "cpu0_pwrdm")) ||
