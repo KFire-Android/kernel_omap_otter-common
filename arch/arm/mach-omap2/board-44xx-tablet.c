0387eac777ce98dcddf97faf74cf5e2685ec350d
Volodymyr Riazantsev <v.riazantsev@ti.com>
2011-12-16 04:20:29 +0200

    TABLET: Add init data for CLK32KAUDIO TWL6030 resource
    Added initialization data for CLK32KAUDIO regulator in
    Tablet and Blaze board files.  On OMAP4 reference platform
    this regulator needed for supply TWL6040 audio supplicant.
    Also make this regulator "always on" until it not correctly
    handled in kernel.
    
    Change-Id: If137a523991afc7c967beb21c9055bf20f927287
    Signed-off-by: Volodymyr Riazantsev <v.riazantsev@ti.com>

--- arch/arm/mach-omap2/board-44xx-tablet.c
+++ arch/arm/mach-omap2/board-44xx-tablet.c
@@ -563,6 +563,13 @@ static struct regulator_init_data tablet_clk32kg = {
 	},
 };
 
+static struct regulator_init_data tablet_clk32kaudio = {
+	.constraints = {
+		.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
+		.always_on		= true,
+	},
+};
+
 static void omap4_audio_conf(void)
 {
 	/* twl6040 naudint */
@@ -707,7 +714,6 @@ static struct twl4030_platform_data tablet_twldata = {
 	.vaux1		= &tablet_vaux1,
 	.vaux2		= &tablet_vaux2,
 	.vaux3		= &tablet_vaux3,
-	.clk32kg	= &tablet_clk32kg,
 
 	/* TWL6032 regulators at OMAP447X based SOMs */
 	.ldo1		= &tablet_vpp,
@@ -723,6 +729,10 @@ static struct twl4030_platform_data tablet_twldata = {
 	/* SMPS */
 	.vdd1		= &tablet_vcore1,
 	.vdd2		= &tablet_vcore2,
+
+	/* TWL6030/6032 common resources */
+	.clk32kg	= &tablet_clk32kg,
+	.clk32kaudio	= &tablet_clk32kaudio,
 
 	/* children */
 	.bci		= &sdp4430_bci_data,
