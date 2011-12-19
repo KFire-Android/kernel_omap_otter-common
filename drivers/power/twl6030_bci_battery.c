822fcd34dfeecc4e8c5d7cdf5d11264636954657
Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>
2011-12-19 15:23:32 +0200

    OMAP4: battery: Update capacity while battery is being charged.
    Estimate capacity not only when battery is discharged, but
    also when charger is plugged.
    
    Change-Id: I776f65929d5adbc8db06aea214ed502105880682
    Signed-off-by: Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>

--- drivers/power/twl6030_bci_battery.c
+++ drivers/power/twl6030_bci_battery.c
@@ -1018,24 +1018,18 @@ static int capacity_changed(struct twl6030_bci_device_info *di)
 
 	}
 
-	/* Setting the capacity level only makes sense when on
-	 * the battery is powering the board.
-	 */
-	if (di->charge_status == POWER_SUPPLY_STATUS_DISCHARGING) {
-		if (di->voltage_uV < 3500)
-			curr_capacity = 5;
-		else if (di->voltage_uV < 3600 && di->voltage_uV >= 3500)
-			curr_capacity = 20;
-		else if (di->voltage_uV < 3700 && di->voltage_uV >= 3600)
-			curr_capacity = 50;
-		else if (di->voltage_uV < 3800 && di->voltage_uV >= 3700)
-			curr_capacity = 75;
-		else if (di->voltage_uV < 3900 && di->voltage_uV >= 3800)
-			curr_capacity = 90;
-		else if (di->voltage_uV >= 3900) {
-				curr_capacity = 100;
-		}
-	}
+	if (di->voltage_uV < 3500)
+		curr_capacity = 5;
+	else if (di->voltage_uV < 3600 && di->voltage_uV >= 3500)
+		curr_capacity = 20;
+	else if (di->voltage_uV < 3700 && di->voltage_uV >= 3600)
+		curr_capacity = 50;
+	else if (di->voltage_uV < 3800 && di->voltage_uV >= 3700)
+		curr_capacity = 75;
+	else if (di->voltage_uV < 3900 && di->voltage_uV >= 3800)
+		curr_capacity = 90;
+	else if (di->voltage_uV >= 3900)
+		curr_capacity = 100;
 
 	/* if we disabled charging to check capacity,
 	 * enable it again after we read the
