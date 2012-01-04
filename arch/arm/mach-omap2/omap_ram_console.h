d7b9a8f9a6faa6265bcdc7d8da87d29d32d5e1aa
Huzefa Kankroliwala <huzefank@ti.com>
2012-01-04 15:51:57 -0600

    OMAP: Support for Android RAM console
    Android's RAMCONSOLE framework is handy framework which allows
    saving the kernel printk messages to a buffer in RAM, so that
    after a kernel panic they can be viewed in the next kernel
    invocation, by accessing /proc/last_kmsg.
    
    This obviously needs DDR to be in selfrefresh mode. Board files
    which have relevant support need to enable CONFIG_OMAP_RAM_CONSOLE
    and call omap_ram_console_init with relevant parameters for this
    to function.
    
    Change-Id: I2dd5ad55a0a3af5a5dcb9a9b8a797e1371af39bc
    Signed-off-by: Huzefa Kankroliwala <huzefank@ti.com>
    Signed-off-by: Akash Choudhari <akashc@ti.com>
    Signed-off-by: Ameya Palande <ameya.palande@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ arch/arm/mach-omap2/omap_ram_console.h
@@ -0,0 +1,33 @@
+/*
+ * OMAP ram console support
+ *
+ * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
+ * Author: Huzefa Kankroliwala <huzefank@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __OMAP_RAM_CONSOLE_H
+#define __OMAP_RAM_CONSOLE_H
+
+/* Board files use the following if they are ok with 512M size defaults */
+#define OMAP_RAM_CONSOLE_START_DEFAULT	(PLAT_PHYS_OFFSET + SZ_512M)
+#define OMAP_RAM_CONSOLE_SIZE_DEFAULT	SZ_2M
+
+#ifdef CONFIG_OMAP_RAM_CONSOLE
+extern int omap_ram_console_init(phys_addr_t phy_addr, size_t size);
+#else
+static inline int omap_ram_console_init(phys_addr_t phy_addr, size_t size)
+{
+	return 0;
+}
+#endif /* CONFIG_OMAP_RAM_CONSOLE */
+
+#endif
