075d19bbe29184d91065be85e443136bcb6d4fd7
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-01-06 06:26:09 +0100

    OMAP5: board: omap5evm: Add regulators for twl6040
    Add regulator support for twl6040 VIO, and V2V1 power
    supply.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -344,6 +344,10 @@ static struct regulator_init_data omap5_smps6 = {
 	},
 };
 
+static struct regulator_consumer_supply omap5_vdds1v8_main_supply[] = {
+	REGULATOR_SUPPLY("vio", "1-004b"),
+};
+
 static struct regulator_init_data omap5_smps7 = {
 	.constraints = {
 		.min_uV			= 1800000,
@@ -353,6 +357,8 @@ static struct regulator_init_data omap5_smps7 = {
 		.valid_ops_mask		= REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 	},
+	.num_consumer_supplies	= ARRAY_SIZE(omap5_vdds1v8_main_supply),
+	.consumer_supplies	= omap5_vdds1v8_main_supply,
 };
 
 static struct regulator_init_data omap5_smps8 = {
@@ -368,7 +374,7 @@ static struct regulator_init_data omap5_smps8 = {
 };
 
 static struct regulator_consumer_supply omap5_adac_supply[] = {
-	REGULATOR_SUPPLY("vcc", "soc-audio"),
+	REGULATOR_SUPPLY("v2v1", "1-004b"),
 };
 
 static struct regulator_init_data omap5_smps9 = {
@@ -382,7 +388,6 @@ static struct regulator_init_data omap5_smps9 = {
 	},
 	.num_consumer_supplies	= ARRAY_SIZE(omap5_adac_supply),
 	.consumer_supplies	= omap5_adac_supply,
-
 };
 
 static struct regulator_consumer_supply omap5_vbus_supply[] = {
