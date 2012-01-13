c82cc0556a73a820a397023da4533c4184f2f772
Manjunatha Halli <x0130808@ti.com>
2012-01-13 11:58:08 -0600

    Bluetooth: btwilink: avoid double freeing of recv skb
    Upon error in hci_recv_frame, the HCI Core is freed before the error
    is reported to btwilink. When the error does get to btwilink and forwarded
    to ST core, ST core also attempts to free the skb.
    
    Although the 2 frees of skb shouldn't theoritically cause any harm, this
    is redundant and hence this patch returns 0 to ST core even upon error,
    because there is no action to be taken.
    
    Change-Id: Ia3ea728625b01eb32509389c75894818f135bc86
    Signed-off-by: Manjunatha Halli <x0130808@ti.com>
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>

--- drivers/bluetooth/btwilink.c
+++ drivers/bluetooth/btwilink.c
@@ -114,7 +114,7 @@ static long st_receive(void *priv_data, struct sk_buff *skb)
 	err = hci_recv_frame(skb);
 	if (err < 0) {
 		BT_ERR("Unable to push skb to HCI core(%d)", err);
-		return err;
+		return 0;
 	}
 
 	lhst->hdev->stat.byte_rx += skb->len;
