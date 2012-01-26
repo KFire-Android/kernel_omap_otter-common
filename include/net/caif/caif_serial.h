3b01468e6879dde8f5149d4f3e9b3b1695ec7b7b
Sjur Brandeland <sjur.brandeland@stericsson.com>
2012-01-26 17:53:14 +0100

    caif: Add serial power management
    Add Power management based on GPIO hand-shake (AWR/CWR).
    This is used in order to negotiate to go to low power mode
    for the UART. This work was done by:
    Daniel Martensson <daniel.martensson@stericsson.com>
    
    Signed-off-by: Sjur Brandeland <sjur.brandeland@stericsson.com>
    Change-Id: Iea878415e623defc8b7de40d1749d9b9fa138efc

--- include/net/caif/caif_serial.h
+++ include/net/caif/caif_serial.h
@@ -8,4 +8,11 @@
 #define CAIF_SERIAL_H_
 void power_notify(bool is_on);
 void cwr_notify(struct tty_struct *tty, bool asserted);
+#ifdef CONFIG_CAIF_TTY_PM
+extern int pm_ext_init(struct tty_struct *tty);
+extern void pm_ext_deinit(struct tty_struct *tty);
+extern void pm_ext_prep_down(struct tty_struct *tty);
+extern void pm_ext_down(struct tty_struct *tty);
+extern void pm_ext_up(struct tty_struct *tty);
+#endif
 #endif /* CAIF_SERIAL_H_ */
