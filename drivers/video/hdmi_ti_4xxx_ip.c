2a2af75471c29356dc9d21cae0df46b8a1e9ca92
Muralidhar Dixit <murali.dixit@ti.com>
2012-02-06 11:31:11 +0530

    OMAP: HDMI: Correction in HDMI PLL program sequence
    Current code sets PLL_GO to 1 and the waits for the PLL_GO to
    become 1. This is not required and instead we should wait for
    PLL_GO to become zero. As per TRM once PLL operation is over
    PLL_GO bit will be reset to zero. After waiting for PLL_GO to
    become zero PLL_STATUS register is read to check the PLL LOCK
    status.
    
    Change-Id: If077695ee95d07df354d97f2b88cf64db94bc27e
    Signed-off-by: Muralidhar Dixit <murali.dixit@ti.com>

--- drivers/video/hdmi_ti_4xxx_ip.c
+++ drivers/video/hdmi_ti_4xxx_ip.c
@@ -124,9 +124,9 @@ static int hdmi_pll_init(struct hdmi_ip_data *ip_data,
 	/* go now */
 	REG_FLD_MOD(hdmi_pll_base(ip_data), PLLCTRL_PLL_GO, 0x1, 0, 0);
 
-	/* wait for bit change */
+	/* wait for PLL opertation to be over */
 	if (hdmi_wait_for_bit_change(hdmi_pll_base(ip_data), PLLCTRL_PLL_GO,
-							0, 0, 1) != 1) {
+							0, 0, 0)) {
 		pr_err("PLL GO bit not set\n");
 		return -ETIMEDOUT;
 	}
