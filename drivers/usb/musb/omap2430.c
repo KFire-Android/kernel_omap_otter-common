4ac8a180853599310f5c9d67094efc69c8c24c57
Moiz Sonasath <m-sonasath@ti.com>
2012-02-08 12:20:23 -0600

    usb: musb: Turn off the PHY while doing interface selection
    The OMAP4 TRM says that for USB OTG, interface selection
    has to be done before the PHY is activated and is not allowed
    to change when the PHY clock is already running.
    
    For charger detection, the interface selection was done with
    the PHY kept on, this was resulting in random hotplug failures
    at times. This patch corrects the issue.
    
    Change-Id: I2385c4e5d5a3d669452af4507557c158e7b48dfc
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- drivers/usb/musb/omap2430.c
+++ drivers/usb/musb/omap2430.c
@@ -265,6 +265,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 			if (musb->gadget_driver) {
 				pm_runtime_get_sync(musb->controller);
 				val = musb_readl(musb->mregs, OTG_INTERFSEL);
+				otg_set_suspend(musb->xceiv, 1);
 				if (data->interface_type ==
 						MUSB_INTERFACE_UTMI) {
 					val &= ~ULPI_12PIN;
@@ -273,6 +274,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 					val |= ULPI_12PIN;
 				}
 				musb_writel(musb->mregs, OTG_INTERFSEL, val);
+				otg_set_suspend(musb->xceiv, 0);
 
 				otg_init(musb->xceiv);
 				omap2430_musb_set_vbus(musb, 1);
@@ -281,6 +283,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 		} else {
 			pm_runtime_get_sync(musb->controller);
 			val = musb_readl(musb->mregs, OTG_INTERFSEL);
+			otg_set_suspend(musb->xceiv, 1);
 			if (data->interface_type == MUSB_INTERFACE_UTMI) {
 				val &= ~ULPI_12PIN;
 				val |= UTMI_8BIT;
@@ -288,6 +291,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 				val |= ULPI_12PIN;
 			}
 			musb_writel(musb->mregs, OTG_INTERFSEL, val);
+			otg_set_suspend(musb->xceiv, 0);
 
 			otg_init(musb->xceiv);
 			omap2430_musb_set_vbus(musb, 1);
@@ -306,6 +310,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 		if (musb->gadget_driver) {
 			pm_runtime_get_sync(musb->controller);
 			val = musb_readl(musb->mregs, OTG_INTERFSEL);
+			otg_set_suspend(musb->xceiv, 1);
 			if (data->interface_type ==
 					MUSB_INTERFACE_UTMI) {
 				val &= ~ULPI_12PIN;
@@ -314,6 +319,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 				val |= ULPI_12PIN;
 			}
 			musb_writel(musb->mregs, OTG_INTERFSEL, val);
+			otg_set_suspend(musb->xceiv, 0);
 		}
 
 #endif
@@ -345,6 +351,7 @@ static void musb_otg_notifier_work(struct work_struct *data_notifier_work)
 				otg_set_vbus(musb->xceiv, 0);
 		}
 		otg_shutdown(musb->xceiv);
+		otg_set_suspend(musb->xceiv, 1);
 		val = musb_readl(musb->mregs, OTG_INTERFSEL);
 		val |= ULPI_12PIN;
 		musb_writel(musb->mregs, OTG_INTERFSEL, val);
@@ -428,6 +435,7 @@ static void omap2430_musb_enable(struct musb *musb)
 
 	case USB_EVENT_ID:
 		val = musb_readl(musb->mregs, OTG_INTERFSEL);
+		otg_set_suspend(musb->xceiv, 1);
 		if (data->interface_type == MUSB_INTERFACE_UTMI) {
 			val &= ~ULPI_12PIN;
 			val |= UTMI_8BIT;
@@ -436,12 +444,14 @@ static void omap2430_musb_enable(struct musb *musb)
 		}
 
 		musb_writel(musb->mregs, OTG_INTERFSEL, val);
+		otg_set_suspend(musb->xceiv, 0);
 		otg_init(musb->xceiv);
 		omap2430_musb_set_vbus(musb, 1);
 		break;
 
 	case USB_EVENT_VBUS:
 		val = musb_readl(musb->mregs, OTG_INTERFSEL);
+		otg_set_suspend(musb->xceiv, 1);
 		if (data->interface_type ==
 			MUSB_INTERFACE_UTMI) {
 			val &= ~ULPI_12PIN;
@@ -450,6 +460,7 @@ static void omap2430_musb_enable(struct musb *musb)
 			val |= ULPI_12PIN;
 		}
 		musb_writel(musb->mregs, OTG_INTERFSEL, val);
+		otg_set_suspend(musb->xceiv, 0);
 		otg_init(musb->xceiv);
 		break;
 
