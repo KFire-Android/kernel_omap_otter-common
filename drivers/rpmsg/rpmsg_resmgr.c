f01a38507e91cec35c9cee904d603bb7e66c2020
Suman Anna <s-anna@ti.com>
2012-02-08 13:30:03 -0600

    rpmsg: resmgr: use absolute inputs for auxclk rates
    Aux clock rates are currently passed as an integer MHz value.
    The rpmsg resource manager multiplies it by the MHz value to
    get the absolute clock rate. This does not allow other clock
    rates like 19.2MHz for aux clock. This is resolved by removing
    the integer MHz restrictions on inputs. Ducati-side is expected
    to pass absolute clock rates.
    
    Change-Id: I051fb59914d2c5fd3ef03c4bbc8c7ff09bb47a06
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- drivers/rpmsg/rpmsg_resmgr.c
+++ drivers/rpmsg/rpmsg_resmgr.c
@@ -43,7 +43,6 @@
 #define AUX_CLK_MIN	0
 #define AUX_CLK_MAX	5
 #define GPTIMERS_MAX	11
-#define MHZ		1000000
 #define MAX_MSG		(sizeof(struct rprm_ack) + sizeof(struct rprm_sdma))
 
 static struct dentry *rprm_dbg;
@@ -209,7 +208,7 @@ static int rprm_auxclk_request(struct rprm_elem *e, struct rprm_auxclk *obj)
 		goto error_aux_src;
 	}
 
-	ret = clk_set_rate(src_parent, (obj->parent_src_clk_rate * MHZ));
+	ret = clk_set_rate(src_parent, obj->parent_src_clk_rate);
 	if (ret) {
 		pr_err("%s: rate not supported by %s\n", __func__,
 					clk_src_name[obj->parent_src_clk]);
@@ -231,7 +230,7 @@ static int rprm_auxclk_request(struct rprm_elem *e, struct rprm_auxclk *obj)
 		goto error_aux_src_parent;
 	}
 
-	ret = clk_set_rate(acd->aux_clk, (obj->clk_rate * MHZ));
+	ret = clk_set_rate(acd->aux_clk, obj->clk_rate);
 	if (ret) {
 		pr_err("%s: rate not supported by %s\n", __func__, clk_name);
 		goto error_aux_enable;
