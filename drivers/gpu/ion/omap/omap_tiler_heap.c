a1e6d31b4fe2b4ed92391ab88bca073bc0cfadaa
Atanas (Tony) Zlatinski <zlatinski@gmail.com>
2012-02-08 17:29:20 -0600

    OMAP4: ION: Use the correct virtual TILER size
    Fixes the ION TILER HEAP allocation function
    to correctly obtain the tiler virtual pages
    and size when dealing with non-page aligned
    tiler allocations.
    
    Change-Id: I59e05aa26b572ed52b7f7d8c653bc6184f6c274d
    Signed-off-by: Atanas (Tony) Zlatinski <zlatinski@gmail.com>

--- drivers/gpu/ion/omap/omap_tiler_heap.c
+++ drivers/gpu/ion/omap/omap_tiler_heap.c
@@ -64,11 +64,14 @@ int omap_tiler_alloc(struct ion_heap *heap,
 {
 	struct ion_handle *handle;
 	struct ion_buffer *buffer;
-	struct omap_tiler_info *info;
+	struct omap_tiler_info *info = NULL;
 	u32 n_phys_pages;
 	u32 n_tiler_pages;
-	ion_phys_addr_t addr;
-	int i, ret;
+	u32 tiler_start = 0;
+	u32 v_size;
+	tiler_blk_handle tiler_handle;
+	ion_phys_addr_t addr = 0;
+	int i = 0, ret;
 
 	if (data->fmt == TILER_PIXEL_FMT_PAGE && data->h != 1) {
 		pr_err("%s: Page mode (1D) allocations must have a height "
@@ -88,37 +91,46 @@ int omap_tiler_alloc(struct ion_heap *heap,
 
 	BUG_ON(!n_phys_pages || !n_tiler_pages);
 
-	info = kzalloc(sizeof(struct omap_tiler_info) +
-		       sizeof(u32) * n_phys_pages +
-		       sizeof(u32) * n_tiler_pages, GFP_KERNEL);
-	if (!info)
-		return -ENOMEM;
-
-	info->n_phys_pages = n_phys_pages;
-	info->n_tiler_pages = n_tiler_pages;
-	info->phys_addrs = (u32 *)(info + 1);
-	info->tiler_addrs = info->phys_addrs + n_phys_pages;
-
 	if( (TILER_ENABLE_NON_PAGE_ALIGNED_ALLOCATIONS)
 			&& (data->token != 0) ) {
-		info->tiler_handle = tiler_alloc_block_area_aligned(data->fmt, data->w, data->h,
-									    &info->tiler_start,
-									    info->tiler_addrs,
+		tiler_handle = tiler_alloc_block_area_aligned(data->fmt, data->w, data->h,
+									    &tiler_start,
+									    NULL,
 									    data->out_align,
 									    data->offset,
 									    data->token);
 	} else {
-		info->tiler_handle = tiler_alloc_block_area(data->fmt, data->w, data->h,
-							    &info->tiler_start,
-							    info->tiler_addrs);
+		tiler_handle = tiler_alloc_block_area(data->fmt, data->w, data->h,
+							    &tiler_start,
+							    NULL);
 	}
 
-	if (IS_ERR_OR_NULL(info->tiler_handle)) {
-		ret = PTR_ERR(info->tiler_handle);
+	if (IS_ERR_OR_NULL(tiler_handle)) {
+		ret = PTR_ERR(tiler_handle);
 		pr_err("%s: failure to allocate address space from tiler\n",
 		       __func__);
 		goto err_nomem;
 	}
+
+	v_size = tiler_block_vsize(tiler_handle);
+
+	if(!v_size)
+		goto err_alloc;
+
+	n_tiler_pages = (PAGE_ALIGN(v_size) / PAGE_SIZE);
+
+	info = kzalloc(sizeof(struct omap_tiler_info) +
+		       sizeof(u32) * n_phys_pages +
+		       sizeof(u32) * n_tiler_pages, GFP_KERNEL);
+	if (!info)
+		goto err_alloc;
+
+	info->tiler_handle = tiler_handle;
+	info->tiler_start = tiler_start;
+	info->n_phys_pages = n_phys_pages;
+	info->n_tiler_pages = n_tiler_pages;
+	info->phys_addrs = (u32 *)(info + 1);
+	info->tiler_addrs = info->phys_addrs + n_phys_pages;
 
 	addr = ion_carveout_allocate(heap, n_phys_pages*PAGE_SIZE, 0);
 	if (addr == ION_CARVEOUT_ALLOCATE_FAIL) {
@@ -158,10 +170,16 @@ int omap_tiler_alloc(struct ion_heap *heap,
 	}
 
 	buffer = ion_handle_buffer(handle);
-	buffer->size = info->n_tiler_pages * PAGE_SIZE;
+	buffer->size = v_size;
 	buffer->priv_virt = info;
 	data->handle = handle;
 	data->offset = (size_t)(info->tiler_start & ~PAGE_MASK);
+
+	if(tiler_fill_virt_array(tiler_handle, info->tiler_addrs,
+			&n_tiler_pages) < 0) {
+		pr_err("%s: failure filling tiler's virtual array %d\n",
+				__func__, n_tiler_pages);
+	}
 
 	return 0;
 
@@ -169,11 +187,14 @@ err:
 	tiler_unpin_block(info->tiler_handle);
 err_alloc:
 	tiler_free_block_area(info->tiler_handle);
-	if (info->lump)
-		ion_carveout_free(heap, addr, n_phys_pages * PAGE_SIZE);
-	else
-		for (i -= 1; i >= 0; i--)
-			ion_carveout_free(heap, info->phys_addrs[i], PAGE_SIZE);
+	if(info)
+	{
+		if (info->lump)
+			ion_carveout_free(heap, addr, n_phys_pages * PAGE_SIZE);
+		else
+			for (i -= 1; i >= 0; i--)
+				ion_carveout_free(heap, info->phys_addrs[i], PAGE_SIZE);
+	}
 err_nomem:
 	kfree(info);
 	return ret;
