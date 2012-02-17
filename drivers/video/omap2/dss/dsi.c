85c3eeb085a2a5d0dc39e79ede1894085d666d98
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-02-17 17:58:04 +0200

    OMAPDSS: remove uses of dss_runtime_get/put
    Now that the omapdss_core device is the parent for all other dss
    devices, we don't need to use the dss_runtime_get/put anymore. Instead,
    enabling omapdss_core will happen automatically when a child device is
    enabled.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dsi.c
+++ drivers/video/omap2/dss/dsi.c
@@ -4802,7 +4802,6 @@ static int omap_dsihw_remove(struct platform_device *dsidev)
 static int dsi_runtime_suspend(struct device *dev)
 {
 	dispc_runtime_put();
-	dss_runtime_put();
 
 	return 0;
 }
@@ -4811,20 +4810,11 @@ static int dsi_runtime_resume(struct device *dev)
 {
 	int r;
 
-	r = dss_runtime_get();
-	if (r)
-		goto err_get_dss;
-
 	r = dispc_runtime_get();
 	if (r)
-		goto err_get_dispc;
+		return r;
 
 	return 0;
-
-err_get_dispc:
-	dss_runtime_put();
-err_get_dss:
-	return r;
 }
 
 static const struct dev_pm_ops dsi_pm_ops = {
