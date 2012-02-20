479a1593f6f8a66e6a77763ec00559c79d671dfd
Sebastien Sabatier <s-sabatier1@ti.com>
2012-02-20 16:10:18 +0100

    ARM:OMAP4:Temp sensor: Fixed context restoration of OMAP temp sensor
    Spurious thermal alert interrupt is generated when restoring the context
    of OMAP temperature sensor driver upon a wake-up from CORE OSWRet/OFF modes.
    This spurious interrupt is due to the fact that current SW implementation
    restores all HW registers without any specific sequence.
    Finally, the thermal alert interrupt handler prints error message because
    the temperature reported by the sensor is invalid.
    
    This patch applies a SW sequence taking into the HW constraints to avoid
    the generation of cold thermal alert.
    HW constraints:
    - The HW logic decrements the counter and then starts the temperature
    measurement.
    - An immediate temperature measurement is possible only if counter is set to
    low value.
    - The HW logic compares the BGAP_TEMP_SENSOR_DTEMP bitfield with threshold as
    soon as the clock is enabled.
    - To avoid spurious interrupt, it is mandatory to unmask interrupts (cold
    and/or hot) when BGAP_TEMP_SENSOR_DTEMP bitfield is different than 0.
    - if BGAP_TEMPSOFF bit sit set, then the sensor is stopped and returns 0.
    
    New SW implementation:
    1/ If all registers have been reset:
    - Force immediate temperature measurement and wait until
    BGAP_TEMP_SENSOR_DTEMP bitfield is updated before completing the full context
    restoration.
    - Ensure that the BGAP_TEMPSOFF bit is not set to 1 during context restoration.
    - Complete the context restoration (mask bits and counter).
    2/ If registers have not been reset:
    - clears TEMPSOFF bit
    - Force immediate temperature measurement and wait until
    BGAP_TEMP_SENSOR_DTEMP bitfield is different than 0.
    
    Change-Id: I6e18e2882107b93cccee8fc20f7052994565a40d
    Signed-off-by: Sebastien Sabatier <s-sabatier1@ti.com>

--- drivers/staging/thermal_framework/sensor/omap_temp_sensor.c
+++ drivers/staging/thermal_framework/sensor/omap_temp_sensor.c
@@ -108,7 +108,7 @@ struct omap_temp_sensor_regs {
 	u32 bg_ctrl;
 	u32 bg_counter;
 	u32 bg_threshold;
-	u32 temp_sensor_tshut_threshold;
+	u32 tshut_threshold;
 };
 
 static struct omap_temp_sensor_regs temp_sensor_context;
@@ -824,6 +824,8 @@ static int omap_temp_sensor_disable(struct omap_temp_sensor *temp_sensor)
 	while ((temp & OMAP4_CLEAN_STOP_MASK) && --counter)
 		temp = omap_temp_sensor_readl(temp_sensor,
 						BGAP_STATUS_OFFSET);
+	if (counter == 0)
+		pr_err("%s:timeout counter for clean stop expired\n", __func__);
 	/* Gate the clock */
 	ret = pm_runtime_put_sync_suspend(&temp_sensor->pdev->dev);
 	if (ret) {
@@ -1131,27 +1133,98 @@ static void omap_temp_sensor_save_ctxt(struct omap_temp_sensor *temp_sensor)
 	    omap_temp_sensor_readl(temp_sensor, BGAP_COUNTER_OFFSET);
 	temp_sensor_context.bg_threshold =
 	    omap_temp_sensor_readl(temp_sensor, BGAP_THRESHOLD_OFFSET);
-	temp_sensor_context.temp_sensor_tshut_threshold =
+	temp_sensor_context.tshut_threshold =
 	    omap_temp_sensor_readl(temp_sensor, BGAP_TSHUT_OFFSET);
+}
+
+static void omap_temp_sensor_force_single_read(
+				struct omap_temp_sensor *temp_sensor)
+{
+	int temp = 0, counter = 1000;
+
+	/* Select single conversion mode */
+	temp = omap_temp_sensor_readl(temp_sensor, BGAP_CTRL_OFFSET);
+	temp &= ~(OMAP4_SINGLE_MODE_MASK);
+	omap_temp_sensor_writel(temp_sensor, temp, BGAP_CTRL_OFFSET);
+	/* Start of Conversion = 1 */
+	temp = omap_temp_sensor_readl(temp_sensor, TEMP_SENSOR_CTRL_OFFSET);
+	temp |= OMAP4_BGAP_TEMP_SENSOR_SOC_MASK;
+	omap_temp_sensor_writel(temp_sensor, temp, TEMP_SENSOR_CTRL_OFFSET);
+	/* Wait until DTEMP is updated */
+	temp = omap_temp_sensor_readl(temp_sensor, TEMP_SENSOR_CTRL_OFFSET);
+	temp &= (OMAP4_BGAP_TEMP_SENSOR_DTEMP_MASK);
+	while ((temp == 0) && --counter) {
+		temp = omap_temp_sensor_readl(temp_sensor,
+			TEMP_SENSOR_CTRL_OFFSET);
+		temp &= (OMAP4_BGAP_TEMP_SENSOR_DTEMP_MASK);
+	}
+	/* Start of Conversion = 0 */
+	temp = omap_temp_sensor_readl(temp_sensor, TEMP_SENSOR_CTRL_OFFSET);
+	temp &= ~(OMAP4_BGAP_TEMP_SENSOR_SOC_MASK);
+	omap_temp_sensor_writel(temp_sensor, temp, TEMP_SENSOR_CTRL_OFFSET);
 }
 
 static void omap_temp_sensor_restore_ctxt(struct omap_temp_sensor *temp_sensor)
 {
-	omap_temp_sensor_writel(temp_sensor,
-				temp_sensor_context.temp_sensor_ctrl,
+	int temp = 0;
+
+	/* if all registers have been lost */
+	if ((omap_temp_sensor_readl(temp_sensor, BGAP_THRESHOLD_OFFSET) == 0) &&
+	    (omap_temp_sensor_readl(temp_sensor, BGAP_COUNTER_OFFSET) == 0)) {
+		omap_temp_sensor_writel(temp_sensor,
+					temp_sensor_context.bg_threshold,
+					BGAP_THRESHOLD_OFFSET);
+		omap_temp_sensor_writel(temp_sensor,
+					temp_sensor_context.tshut_threshold,
+					BGAP_TSHUT_OFFSET);
+		/*
+		 * Force immediate temperature measurement and update of the
+		 * BGAP_TEMP_SENSOR_DTEMP bitfield before completing the full
+		 * context restoration.
+		 * This ensures that HW does not generate spurious thermal alert
+		 * when restoring the mask bits: if the BGAP_TEMP_SENSOR_DTEMP
+		 * bitfield in CONTROL_TEMP_SENSOR register is not yet
+		 * initialized, the comparison done by the HW logic (against the
+		 * temperature thresholds) will generate an unexpected thermal
+		 * alert.
+		 */
+		omap_temp_sensor_force_single_read(temp_sensor);
+
+		/* Complete context restoration */
+		temp_sensor_context.temp_sensor_ctrl &=
+			~(OMAP4_BGAP_TEMPSOFF_MASK);
+		omap_temp_sensor_writel(temp_sensor,
+					temp_sensor_context.temp_sensor_ctrl,
+					TEMP_SENSOR_CTRL_OFFSET);
+		omap_temp_sensor_writel(temp_sensor,
+					temp_sensor_context.bg_counter,
+					BGAP_COUNTER_OFFSET);
+		omap_temp_sensor_writel(temp_sensor,
+					temp_sensor_context.bg_ctrl,
+					BGAP_CTRL_OFFSET);
+	} else { /* registers have not been reset but DTEMP is not yet valid */
+		temp = omap_temp_sensor_readl(temp_sensor,
+			TEMP_SENSOR_CTRL_OFFSET);
+		temp &= (OMAP4_BGAP_TEMP_SENSOR_DTEMP_MASK);
+		if (temp == 0) {
+			/* BGAP_TEMPSOFF should be reset to 0 */
+			temp = omap_temp_sensor_readl(temp_sensor,
+					TEMP_SENSOR_CTRL_OFFSET);
+			temp &= ~(OMAP4_BGAP_TEMPSOFF_MASK);
+			omap_temp_sensor_writel(temp_sensor, temp,
 				TEMP_SENSOR_CTRL_OFFSET);
-	omap_temp_sensor_writel(temp_sensor,
-				temp_sensor_context.bg_ctrl,
-				BGAP_CTRL_OFFSET);
-	omap_temp_sensor_writel(temp_sensor,
-				temp_sensor_context.bg_counter,
-				BGAP_COUNTER_OFFSET);
-	omap_temp_sensor_writel(temp_sensor,
-				temp_sensor_context.bg_threshold,
-				BGAP_THRESHOLD_OFFSET);
-	omap_temp_sensor_writel(temp_sensor,
-				temp_sensor_context.temp_sensor_tshut_threshold,
-				BGAP_TSHUT_OFFSET);
+			udelay(5);	/* wait for 5 us */
+
+			omap_temp_sensor_force_single_read(temp_sensor);
+
+			/* Select continous conversion mode */
+			temp = omap_temp_sensor_readl(temp_sensor,
+						BGAP_CTRL_OFFSET);
+			temp |= OMAP4_SINGLE_MODE_MASK;
+			omap_temp_sensor_writel(temp_sensor, temp,
+						BGAP_CTRL_OFFSET);
+		}
+	}
 }
 
 static int omap_temp_sensor_suspend(struct platform_device *pdev,
