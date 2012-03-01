8b823d74a2d7073abf86341aee91c299d7c40e6f
Juan Gutierrez <jgutierrez@ti.com>
2012-03-01 12:09:32 -0600

    omap: remoteproc: add watchdog timer support for dsp
    Use GPTimer 6 as the watchdog timer for dsp. This is used
    specifically instead of the WDT3, as the latter is not
    capable of generating an interrupt to the DSP core. The
    former is used to generate an interrupt and thereby
    gather some watchdog context data.
    
    Change-Id: I7f8d6e7070cc14f4339e79e42a6affe169270bb2
    Signed-off-by: Juan Gutierrez <jgutierrez@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/mach-omap2/remoteproc.c
+++ arch/arm/mach-omap2/remoteproc.c
@@ -109,6 +109,9 @@ static struct omap_rproc_timers_info ipu_timers[] = {
 
 static struct omap_rproc_timers_info dsp_timers[] = {
 	{ .id = 5 },
+#ifdef CONFIG_REMOTEPROC_WATCHDOG
+	{ .id = 6 },
+#endif
 };
 
 static struct omap_rproc_pdata omap4_rproc_data[] = {
