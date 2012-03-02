1bdd9e017b188d0b8defff87605f37d09971c6ef
Vishwanath BS <vishwanath.bs@ti.com>
2012-03-02 17:17:55 +0200

    ARM: OMAP3: PM: Remove IO Daisychain control from cpuidle
    As IO Daisy chain sequence is triggered via hwmod mux, there is no need to
    control it from cpuidle path for OMAP3.
    
    Also as omap3_disable_io_chain is no longer being used, just remove the
    function.
    
    Change-Id: I541aad5f3b1c4ecaef371b80736bf631ae544628
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Reviewed-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    [girishsg@ti.com: Fix merge conflicts]
    Signed-off-by: Girish S G <girishsg@ti.com>

--- arch/arm/mach-omap2/pm34xx.c
+++ arch/arm/mach-omap2/pm34xx.c
@@ -272,10 +272,6 @@ void omap_sram_idle(void)
 	/* Enable IO-PAD and IO-CHAIN wakeups */
 	per_next_state = pwrdm_read_next_pwrst(per_pwrdm);
 	core_next_state = pwrdm_read_next_pwrst(core_pwrdm);
-	if (omap3_has_io_wakeup() &&
-	    (per_next_state < PWRDM_POWER_ON ||
-	     core_next_state < PWRDM_POWER_ON))
-		omap2_prm_set_mod_reg_bits(OMAP3430_EN_IO_MASK, WKUP_MOD, PM_WKEN);
 
 	pwrdm_pre_transition(NULL);
 
@@ -349,13 +345,6 @@ void omap_sram_idle(void)
 		per_prev_state = pwrdm_read_prev_pwrst(per_pwrdm);
 		omap2_gpio_resume_after_idle();
 	}
-
-	/* Disable IO-PAD and IO-CHAIN wakeup */
-	if (omap3_has_io_wakeup() &&
-	    (per_next_state < PWRDM_POWER_ON ||
-	     core_next_state < PWRDM_POWER_ON))
-		omap2_prm_clear_mod_reg_bits(OMAP3430_EN_IO_MASK, WKUP_MOD,
-					     PM_WKEN);
 
 	clkdm_allow_idle(mpu_pwrdm->pwrdm_clkdms[0]);
 }
