4039ba6b918d64e096c1b22590b583ef2f4550fa
Misael Lopez Cruz <misael.lopez@ti.com>
2012-03-04 01:38:08 -0600

    ASoC: twl6040: Split HF DAC event handler
    Use a dedicated event handler for HS DAC widgets whose only task is
    wait for the settling time of at least 80us.
    
    Change-Id: I2f13051fdc02466c184cf2ee9ed5653b75644023
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/soc/codecs/twl6040.c
+++ sound/soc/codecs/twl6040.c
@@ -895,7 +895,14 @@ static int twl6040_hs_dac_right_event(struct snd_soc_dapm_widget *w,
 	return 0;
 }
 
+static int twl6040_hf_dac_event(struct snd_soc_dapm_widget *w,
+			struct snd_kcontrol *kcontrol, int event)
+{
+	/* HFDAC settling time */
+	usleep_range(80, 200);
 
+	return 0;
+}
 
 static int twl6040_power_mode_event(struct snd_soc_dapm_widget *w,
 			struct snd_kcontrol *kcontrol, int event)
@@ -1334,11 +1341,11 @@ static const struct snd_soc_dapm_widget twl6040_dapm_widgets[] = {
 			SND_SOC_DAPM_PRE_PMD | SND_SOC_DAPM_POST_PMD),
 	SND_SOC_DAPM_DAC_E("HFDAC Left", "Handsfree Playback",
 			TWL6040_REG_HFLCTL, 0, 0,
-			twl6040_power_mode_event,
+			twl6040_hf_dac_event,
 			SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
 	SND_SOC_DAPM_DAC_E("HFDAC Right", "Handsfree Playback",
 			TWL6040_REG_HFRCTL, 0, 0,
-			twl6040_power_mode_event,
+			twl6040_hf_dac_event,
 			SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
 
 	SND_SOC_DAPM_MUX("Handsfree Left Playback",
