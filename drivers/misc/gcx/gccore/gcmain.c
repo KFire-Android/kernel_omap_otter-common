6d4c5c2566376049758b1e29359091e8132c9870
David Sin <davidsin@ti.com>
2012-03-23 10:49:41 -0500

    gccore: set GCHOLD bit before disabling clock.
    Change-Id: I970a2fd10f00bfd3afff4096d7815c2d0a1053d6
    Signed-off-by: David Sin <davidsin@ti.com>

--- drivers/misc/gcx/gccore/gcmain.c
+++ drivers/misc/gcx/gccore/gcmain.c
@@ -637,6 +637,7 @@ static struct gctransition g_gctransition[4][4] = {
 
 #include <plat/omap_hwmod.h>
 #include <plat/omap-pm.h>
+#define GCGPOUT0 0x64
 
 enum gcpower g_gcpower;
 
@@ -669,6 +670,7 @@ enum gcerror gc_set_power(enum gcpower gcpower)
 			}
 			pr_info("gcx: clock enabled.\n");
 		} else {
+			gc_write_reg(GCGPOUT0, 0x1);
 			clk_disable(g_bb2d_clk);
 			pr_info("gcx: clock disabled.\n");
 		}
