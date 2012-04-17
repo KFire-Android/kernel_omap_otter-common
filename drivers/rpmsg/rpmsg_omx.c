76c425a313cf3b846c2724a31cbb391f5660eea4
Hashcode <hashcode0f@gmail.com>
2012-04-16 23:35:27 -0700

    rpmsg fix for dce: /dev/rpmsg_omx#

--- drivers/rpmsg/rpmsg_omx.c
+++ drivers/rpmsg/rpmsg_omx.c
@@ -758,7 +758,7 @@ static int rpmsg_omx_probe(struct rpmsg_channel *rpdev)
 
 	omxserv->dev = device_create(rpmsg_omx_class, &rpdev->dev,
 			MKDEV(major, minor), NULL,
-			rpdev->id.name);
+			"rpmsg-omx%d", minor);
 	if (IS_ERR(omxserv->dev)) {
 		ret = PTR_ERR(omxserv->dev);
 		dev_err(&rpdev->dev, "device_create failed: %d\n", ret);
@@ -833,10 +833,16 @@ static void rpmsg_omx_driver_cb(struct rpmsg_channel *rpdev, void *data,
 		       data, len,  true);
 }
 
+#if 0
 static struct rpmsg_device_id rpmsg_omx_id_table[] = {
 	{ .name	= "rpmsg-omx0" }, /* ipu_c0 */
 	{ .name	= "rpmsg-omx1" }, /* ipu_c1 */
 	{ .name	= "rpmsg-omx2" }, /* dsp */
+	{ },
+};
+#endif
+static struct rpmsg_device_id rpmsg_omx_id_table[] = {
+	{ .name	= "rpmsg-omx" },
 	{ },
 };
 MODULE_DEVICE_TABLE(platform, rpmsg_omx_id_table);
