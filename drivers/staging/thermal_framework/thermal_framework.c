f9dbf50210c34eb9e82bde09eeb28ba9e62ab624
Eduardo Valentin <eduardo.valentin@ti.com>
2012-04-20 14:40:29 -0500

    TI thermal framework: remove unnecessary checks on thermal domain list
    The domain can be fetch from each thermal device. Because of that,
    there is no need to check for the emptiness of thermal domain list.
    
    Change-Id: I736858ec915092baf3171fcded94de7438472653
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Radhesh Fadnis <radhesh.fadnis@ti.com>

--- drivers/staging/thermal_framework/thermal_framework.c
+++ drivers/staging/thermal_framework/thermal_framework.c
@@ -108,11 +108,6 @@ int thermal_sensor_set_temp(struct thermal_dev *tdev)
 	struct thermal_domain *thermal_domain;
 	int ret = -ENODEV;
 
-	if (list_empty(&thermal_domain_list)) {
-		pr_debug("%s: No governors registered\n", __func__);
-		goto out;
-	}
-
 	thermal_domain = tdev->domain;
 	if (!thermal_domain) {
 		pr_err("%s: device not part of a domain\n", __func__);
@@ -145,9 +140,6 @@ int thermal_get_slope(struct thermal_dev *tdev)
 	struct thermal_domain *thermal_domain;
 	int ret = -ENODEV;
 
-	if (list_empty(&thermal_domain_list))
-		pr_info("%s: No sensors registered\n", __func__);
-
 	thermal_domain = tdev->domain;
 	if (!thermal_domain) {
 		pr_err("%s: device not part of a domain\n", __func__);
@@ -178,9 +170,6 @@ int thermal_get_offset(struct thermal_dev *tdev)
 {
 	struct thermal_domain *thermal_domain;
 	int ret = -ENODEV;
-
-	if (list_empty(&thermal_domain_list))
-		pr_debug("%s: No sensors registered\n", __func__);
 
 	thermal_domain = tdev->domain;
 	if (!thermal_domain) {
@@ -217,11 +206,6 @@ int thermal_request_temp(struct thermal_dev *tdev)
 {
 	struct thermal_domain *thermal_domain;
 	int ret = -ENODEV;
-
-	if (list_empty(&thermal_domain_list)) {
-		pr_debug("%s: No thermal sensors registered\n", __func__);
-		return ret;
-	}
 
 	thermal_domain = tdev->domain;
 	if (!thermal_domain) {
@@ -283,11 +267,6 @@ EXPORT_SYMBOL_GPL(thermal_update_temp_rate);
 static int thermal_init_thermal_state(struct thermal_dev *tdev)
 {
 	struct thermal_domain *domain;
-
-	if (list_empty(&thermal_domain_list)) {
-		pr_err("%s: No cooling devices registered\n", __func__);
-		return -ENODEV;
-	}
 
 	domain = tdev->domain;
 	if (!domain) {
