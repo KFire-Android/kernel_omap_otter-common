798d69f818abaccb8c4e470e9a1747a47c93a750
Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>
2012-04-20 16:43:06 +0300

    MFD: TWL6032_GPADC: Correct calibration parametrs.
    This patch corrects GPADC calibration for the TWL6032 due to the
    TWL6032 data manual.
    
    Change-Id: I15fb4148bf94a5123467e8b7a316e7edcdb55ebc
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@ti.com>

--- drivers/mfd/twl6030-gpadc.c
+++ drivers/mfd/twl6030-gpadc.c
@@ -1208,11 +1208,11 @@ static int twl6032_calibration(struct twl6030_gpadc_data *gpadc)
 			/* D1 */
 			d1 = (trim_regs[11] & 0x0F) << 3;
 			d1 |= (trim_regs[9] & 0x0E) >> 1;
-			if (trim_regs[1] & 0x01)
+			if (trim_regs[9] & 0x01)
 				d1 = -d1;
 
 			/* D2 */
-			d2 = (trim_regs[15] & 0x0F) << 2;
+			d2 = (trim_regs[15] & 0x0F) << 3;
 			d2 |= (trim_regs[13] & 0x0E) >> 1;
 			if (trim_regs[13] & 0x01)
 				d2 = -d2;
@@ -1225,8 +1225,8 @@ static int twl6032_calibration(struct twl6030_gpadc_data *gpadc)
 			if (trim_regs[1] & 0x01)
 				temp = -temp;
 
-			d1 = (trim_regs[1] & 0x7E) >> 1;
-			if (trim_regs[12] & 0x01)
+			d1 = (trim_regs[5] & 0x7E) >> 1;
+			if (trim_regs[5] & 0x01)
 				d1 = -d1;
 
 			d1 += temp;
@@ -1237,8 +1237,8 @@ static int twl6032_calibration(struct twl6030_gpadc_data *gpadc)
 			if (trim_regs[2] & 0x01)
 				temp = -temp;
 
-			d2 = (trim_regs[6] & 0x7F) >> 1;
-			if (trim_regs[14] & 0x01)
+			d2 = (trim_regs[6] & 0xFE) >> 1;
+			if (trim_regs[6] & 0x01)
 				d2 = -d2;
 
 			d2 += temp;
