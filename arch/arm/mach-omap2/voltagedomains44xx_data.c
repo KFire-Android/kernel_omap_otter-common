0f2c8a7c9c04e388f585a79a7e609d8427e68e08
Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>
2012-04-23 09:39:21 +0300

    OMAP4: VP: recommeded VP registers settings
    Settings for vddmax were different from recommended.
    The recommendations are different for 4430 and 4460. There are
    no recommended settings for 4470 yet. Set it as for 4460.
    
    Recommended settings(in millivolts):
    -------+--------------+------------
           |     4430     |    4460
    -------+--------------+------------
     MPU   |  1360(0x35)  |  1380(0x58)
     IVA   |  1240(0x2b)  |  1375(0x36)
     CORE  |  1170(0x26)  |  1250(0x2c)
    -----------------------------------
    
    Set recommended register settings in voltage processor.
    
    Change-Id: Ibf9f46eef76da7858245b028558a69e0de4a645b
    Signed-off-by: Oleksandr Kozaruk <oleksandr.kozaruk@ti.com>

--- arch/arm/mach-omap2/voltagedomains44xx_data.c
+++ arch/arm/mach-omap2/voltagedomains44xx_data.c
@@ -132,6 +132,10 @@ void __init omap44xx_voltagedomains_init(void)
 	struct voltagedomain *voltdm;
 	int i;
 
+	omap4_voltdm_mpu.vp->vlimits->vddmax = OMAP4460_VP_MPU_VLIMITTO_VDDMAX;
+	omap4_voltdm_iva.vp->vlimits->vddmax = OMAP4460_VP_IVA_VLIMITTO_VDDMAX;
+	omap4_voltdm_core.vp->vlimits->vddmax = OMAP4460_VP_CORE_VLIMITTO_VDDMAX;
+
 	/*
 	 * XXX Will depend on the process, validation, and binning
 	 * for the currently-running IC
@@ -149,6 +153,12 @@ void __init omap44xx_voltagedomains_init(void)
 		omap4_vdd_core_info.volt_data = omap443x_vdd_core_volt_data;
 		omap4_vdd_mpu_info.dep_vdd_info = omap443x_vddmpu_dep_info;
 		omap4_vdd_iva_info.dep_vdd_info = omap443x_vddiva_dep_info;
+		omap4_voltdm_mpu.vp->vlimits->vddmax =
+						OMAP4430_VP_MPU_VLIMITTO_VDDMAX;
+		omap4_voltdm_iva.vp->vlimits->vddmax =
+						OMAP4430_VP_IVA_VLIMITTO_VDDMAX;
+		omap4_voltdm_core.vp->vlimits->vddmax =
+						OMAP4430_VP_CORE_VLIMITTO_VDDMAX;
 	} else if (cpu_is_omap446x()) {
 		omap4_vdd_mpu_info.volt_data = omap446x_vdd_mpu_volt_data;
 		omap4_vdd_iva_info.volt_data = omap446x_vdd_iva_volt_data;
