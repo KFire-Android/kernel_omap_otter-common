7328527cf0ac904011c97ecd759014dbe81b291b
Muralidhar Dixit <murali.dixit@ti.com>
2012-04-24 12:26:15 +0530

    OMAPDSS: CEC: Physical address discovery form edid
    HDMI sink device will set the physical address for
    CEC devices
    
    Change-Id: I6419834816978dfc8bcb8ef9a7b6c36276e5792c
    Signed-off-by: Muralidhar Dixit <murali.dixit@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -59,6 +59,7 @@ static struct pm_qos_request_list pm_qos_handle;
 #define EDID_TIMING_DESCRIPTOR_SIZE		0x12
 #define EDID_DESCRIPTOR_BLOCK0_ADDRESS		0x36
 #define EDID_DESCRIPTOR_BLOCK1_ADDRESS		0x80
+#define EDID_HDMI_VENDOR_SPECIFIC_DATA_BLOCK	128
 #define EDID_SIZE_BLOCK0_TIMING_DESCRIPTOR	4
 #define EDID_SIZE_BLOCK1_TIMING_DESCRIPTOR	4
 
@@ -92,7 +93,7 @@ static struct {
 
 	u8 s3d_mode;
 	bool s3d_enable;
-
+	int source_physical_address;
 	void (*hdmi_start_frame_cb)(void);
 	void (*hdmi_irq_cb)(int);
 	bool (*hdmi_power_on_cb)(void);
@@ -262,6 +263,23 @@ void hdmi_get_monspecs(struct fb_monspecs *specs)
 		specs->modedb[j++] = specs->modedb[i];
 	}
 	specs->modedb_len = j;
+
+	/* Find out the Source Physical address for the CEC
+	CEC physical address will be part of VSD block from
+	TV Physical address is 2 bytes after 24 bit IEEE
+	registration identifier (0x000C03)
+	*/
+	i = EDID_HDMI_VENDOR_SPECIFIC_DATA_BLOCK;
+	while (i < (HDMI_EDID_MAX_LENGTH - 5)) {
+		if ((edid[i] == 0x03) && (edid[i+1] == 0x0c) &&
+			(edid[i+2] == 0x00)) {
+			hdmi.source_physical_address = (edid[i+3] << 8) |
+				edid[i+4];
+			break;
+		}
+		i++;
+
+	}
 }
 
 u8 *hdmi_read_edid(struct omap_video_timings *dp)
