c9a9fe7ee342f13d72bb88962e316ab7d2855ecc
Shubhrajyoti D <shubhrajyoti@ti.com>
2012-05-02 16:56:12 +0530

    ARM: OMAP5: hwmod data: I2C: Update the flags for I2C5
    OMAP5 has an extra instance of i2c.
    Set the flags for clockactivity and to indicate 16 bit flag.
    
    Signed-off-by: Shubhrajyoti D <shubhrajyoti@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_54xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_54xx_data.c
@@ -2708,6 +2708,7 @@ static struct omap_hwmod omap54xx_i2c5_hwmod = {
 	.name		= "i2c5",
 	.class		= &omap54xx_i2c_hwmod_class,
 	.clkdm_name	= "l4per_clkdm",
+	.flags		= HWMOD_16BIT_REG | HWMOD_SET_DEFAULT_CLOCKACT,
 	.mpu_irqs	= omap54xx_i2c5_irqs,
 	.main_clk	= "func_96m_fclk",
 	.prcm = {
