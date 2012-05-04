c5f6398f7862931d48f7e783d730704c1f2b9fc5
Tony Lofthouse <a0741364@ti.com>
2012-05-04 12:56:59 -0500

    omap: dss: use sw sleep/wkup rather than hw_auto mode
    With hw_auto we are getting unpredictable lock ups with BB2D
    enabled.
    
    Change-Id: Id9c050894a656d8036ffe59d1cc1c29eaec65dfc
    Signed-off-by: Tony Lofthouse <a0741364@ti.com>

--- arch/arm/mach-omap2/clockdomains44xx_data.c
+++ arch/arm/mach-omap2/clockdomains44xx_data.c
@@ -618,7 +618,7 @@ static struct clockdomain l3_dss_44xx_clkdm = {
 	.dep_bit	  = OMAP4430_DSS_STATDEP_SHIFT,
 	.wkdep_srcs	  = l3_dss_wkup_sleep_deps,
 	.sleepdep_srcs	  = l3_dss_wkup_sleep_deps,
-	.flags		  = CLKDM_CAN_HWSUP_SWSUP,
+	.flags		  = CLKDM_CAN_SWSUP,
 	.omap_chip	  = OMAP_CHIP_INIT(CHIP_IS_OMAP44XX),
 };
 
