8767a245accc11a6eeb35377ac311e2ab8edd804
Eugen Mandrenko <ievgen.mandrenko@ti.com>
2012-05-04 13:03:32 +0300

    OMAP2PLUS:fix for TF doesn't work when CPU1 on/off
    TI thermal framework: rework initialization sequence
    
    The initialization of max_thermal was placed in function
    which is executed for each CPU1 start. The old value of
    max_thermal is lost in this case and is set to max_freq.
    As result, the omap_thermal_step_freq_down() can't
    reduce the frequency if CPU1 is  switched on/off often.
    
    Signed-off-by: Eugen Mandrenko <ievgen.mandrenko@ti.com>
    Change-Id: Idcbc6a56407595b8f71743b91d994990a279593b
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>

--- drivers/cpufreq/omap-cpufreq.c
+++ drivers/cpufreq/omap-cpufreq.c
@@ -356,8 +356,6 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
 		max_freq = max(freq_table[i].frequency, max_freq);
-	max_thermal = max_freq;
-	current_cooling_level = 0;
 
 	/*
 	 * On OMAP SMP configuartion, both processors share the voltage
@@ -432,6 +430,9 @@ static int __init omap_cpufreq_init(void)
 
 	ret = cpufreq_register_driver(&omap_driver);
 	omap_cpufreq_ready = !ret;
+
+	max_thermal = max_freq;
+	current_cooling_level = 0;
 
 	if (!ret)
 		ret = omap_cpufreq_cooling_init();
