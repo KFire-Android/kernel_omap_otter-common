e7c17d3244b1476016d08e6a7d2327178fef06f0
Liam Girdwood <lrg@ti.com>
2012-05-14 16:43:18 +0100

    ASoC: omap dmic: Add stream names for each ABE DAI
    Add stream names for ABE DAIs stream DAI matching.
    
    Signed-off-by: Liam Girdwood <lrg@ti.com>

--- sound/soc/omap/omap-dmic.c
+++ sound/soc/omap/omap-dmic.c
@@ -451,6 +451,7 @@ static struct snd_soc_dai_driver omap_dmic_dai[] = {
 {
 	.name = "omap-dmic-abe-dai-0",
 	.capture = {
+		.stream_name = "omap-dmic-abe.0 Capture",
 		.channels_min = 2,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_96000 | SNDRV_PCM_RATE_192000,
@@ -461,6 +462,7 @@ static struct snd_soc_dai_driver omap_dmic_dai[] = {
 {
 	.name = "omap-dmic-abe-dai-1",
 	.capture = {
+		.stream_name = "omap-dmic-abe.1 Capture",
 		.channels_min = 2,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_96000 | SNDRV_PCM_RATE_192000,
@@ -471,6 +473,7 @@ static struct snd_soc_dai_driver omap_dmic_dai[] = {
 {
 	.name = "omap-dmic-abe-dai-2",
 	.capture = {
+		.stream_name = "omap-dmic-abe.2 Capture",
 		.channels_min = 2,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_96000 | SNDRV_PCM_RATE_192000,
