bbae0c598f01e30cfbe1be4731d5f16c143566e6
Jean Pihet <j-pihet@ti.com>
2012-05-15 12:47:46 +0530

    PM QoS: add a PM_QOS_MEMORY_THROUGHPUT class
    Provide a memory or SDMA throughput constraints class,
    which can be applied to the low level platform code using the
    callback notification mechanism and
    also a MISC /dev entry for the constraints from user space.
    
    Signed-off-by: Jean Pihet <j-pihet@ti.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -86,6 +86,18 @@ static struct pm_qos_object network_lat_pm_qos = {
 	.name = "network_latency",
 };
 
+static BLOCKING_NOTIFIER_HEAD(memory_throughput_notifier);
+static struct pm_qos_constraints memory_tput_constraints = {
+	.list = PLIST_HEAD_INIT(memory_tput_constraints.list),
+	.target_value = PM_QOS_MEMORY_THROUGHPUT_DEFAULT_VALUE,
+	.default_value = PM_QOS_MEMORY_THROUGHPUT_DEFAULT_VALUE,
+	.type = PM_QOS_MAX,
+	.notifiers = &memory_throughput_notifier,
+};
+static struct pm_qos_object memory_throughput_pm_qos = {
+	.constraints = &memory_tput_constraints,
+	.name = "memory_throughput",
+};
 
 static BLOCKING_NOTIFIER_HEAD(network_throughput_notifier);
 static struct pm_qos_constraints network_tput_constraints = {
@@ -105,6 +117,7 @@ static struct pm_qos_object *pm_qos_array[] = {
 	&null_pm_qos,
 	&cpu_dma_pm_qos,
 	&network_lat_pm_qos,
+	&memory_throughput_pm_qos,
 	&network_throughput_pm_qos
 };
 
