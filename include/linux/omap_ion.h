6a663dee24bcb47fce76968e8bb5542897a24ea1
Tyler Luu <tluu@ti.com>
2012-05-15 22:29:11 -0500

    ion: omap: change behaviour of getting buffers from pvr fd
    previous implemention shares the pvr ion client and the ion
    handles associated with that client for a particular pvr fd.
    now, we just call ion_share on the handles and return the
    shared buffers. caller is in charge of calling ion_import using
    its own ion client to get the ion handles.
    
    Change-Id: I2abdee1daed7b5c57c66b2e5fddc4eb0d0cc1305
    Signed-off-by: Tyler Luu <tluu@ti.com>

--- include/linux/omap_ion.h
+++ include/linux/omap_ion.h
@@ -51,9 +51,8 @@ int omap_ion_nonsecure_tiler_alloc(struct ion_client *client,
 /* given a handle in the tiler, return a list of tiler pages that back it */
 int omap_tiler_pages(struct ion_client *client, struct ion_handle *handle,
 		     int *n, u32 ** tiler_pages);
-int omap_ion_fd_to_handles(int fd, struct ion_client **client,
-		struct ion_handle **handles,
-		int *num_handles);
+int omap_ion_share_fd_to_buffers(int fd, struct ion_buffer **buffers,
+				 int *num_handles);
 int omap_tiler_vinfo(struct ion_client *client,
 			struct ion_handle *handle, unsigned int *vstride,
 			unsigned int *vsize);
