b71b2b2b8cc894ea7057d0954560a90c71fc8073
Fernando Guzman Lugo <fernando.lugo@ti.com>
2012-05-16 20:31:10 -0500

    omap: iommu: deny clkdm idle when iommu fault is not handle until shutdown
    When and iommu fault happen and it could not be handled (that means a
    valid physical address could not be found) we cannot ack the status bit
    of the interrupt. Therefore, we need to deny clkdm idle for the clock of
    the iommu until the ack can be done. Otherwise, if there is no
    clkdm activity (that can happen if the processor associated with the
    processor is put under reset) the clkdm will try to go to idle and it will
    fail and get stuck in transition state because of the pending interrupt.
    In order to avoid that scenario we deny idle in the mmufault ISR and then
    in the shutdown of the iommu we ack the interrupt and then allow idle
    again.
    
    Also, for omap4 and omap5 the IPU and DSP processors have a shared MMU
    which try to use the iommu again once it is acked even if the processor
    is already under reset. For that reason the shared MMU must be reset.
    The shared MMU only has hardreset and it is shared with the iommu, for
    that reason we need to do a hardreset en the iommuu device. In order
    to accomplish that we use hwmod functions.
    
    Signed-off-by: Fernando Guzman Lugo <fernando.lugo@ti.com>
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>

--- arch/arm/mach-omap2/iommu2.c
+++ arch/arm/mach-omap2/iommu2.c
@@ -19,6 +19,8 @@
 #include <linux/stringify.h>
 
 #include <plat/iommu.h>
+#include <plat/omap_hwmod.h>
+#include "clockdomain.h"
 
 /*
  * omap2 architecture specific register bit definitions
@@ -96,11 +98,37 @@ static int omap2_iommu_enable(struct omap_iommu *obj)
 
 static void omap2_iommu_disable(struct omap_iommu *obj)
 {
-	u32 l = iommu_read_reg(obj, MMU_CNTL);
+	struct omap_hwmod *oh;
+	u32 l;
 
+	oh = omap_hwmod_lookup(obj->name);
+	if (!oh)
+		return;
+	/*
+	 * IPU and DSP iommus are not directly connected to the processor
+	 * instead they are  behind a shared MMU. Therefore in the case of
+	 * a mmu fault and the mmu fault was not handled, even if the processor
+	 * is under reset, the shared MMU will try to translation the address
+	 * again causing that the status flag cannot be clear and therefore
+	 * as soon as the clkdm wants to go to idle the clkdm will be stuck
+	 * in transition state. The only way to reset the shared MMU is doing
+	 * a hardreset of the L2 iommu which shared the reset line with the
+	 * shared MMU. That way we can clean the status bit and turn off
+	 * the iommu without any issue.
+	 */
+	if (!strcmp(obj->name, "ipu") || !strcmp(obj->name, "dsp")) {
+		omap_hwmod_assert_hardreset(oh, oh->rst_lines->name);
+		omap_hwmod_deassert_hardreset(oh, oh->rst_lines->name);
+		goto out;
+	}
+
+	l = iommu_read_reg(obj, MMU_IRQSTATUS);
+	iommu_write_reg(obj, l, MMU_IRQSTATUS);
+	l = iommu_read_reg(obj, MMU_CNTL);
 	l &= ~MMU_CNTL_MASK;
 	iommu_write_reg(obj, l, MMU_CNTL);
-
+out:
+	clkdm_allow_idle(oh->clkdm);
 	dev_dbg(obj->dev, "%s is shutting down\n", obj->name);
 }
 
@@ -111,8 +139,13 @@ static void omap2_iommu_set_twl(struct omap_iommu *obj, bool on)
 
 static u32 omap2_iommu_fault_isr(struct omap_iommu *obj, u32 *ra)
 {
+	struct omap_hwmod *oh;
 	u32 stat, da;
 	u32 errs = 0;
+
+	oh = omap_hwmod_lookup(obj->name);
+	if (!oh)
+		return 0;
 
 	stat = iommu_read_reg(obj, MMU_IRQSTATUS);
 	stat &= MMU_IRQ_MASK;
@@ -135,6 +168,8 @@ static u32 omap2_iommu_fault_isr(struct omap_iommu *obj, u32 *ra)
 	if (stat & MMU_IRQ_MULTIHITFAULT)
 		errs |= OMAP_IOMMU_ERR_MULTIHIT_FAULT;
 	iommu_write_reg(obj, stat, MMU_IRQSTATUS);
+
+	clkdm_deny_idle(oh->clkdm);
 
 	return errs;
 }
