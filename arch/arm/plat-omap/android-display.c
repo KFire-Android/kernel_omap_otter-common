cc6e53a908c3ae87a253dafdb1af5cbd7e7adc14
Dima Svetlov <svetlov@ti.com>
2012-05-21 18:19:25 -0700

    plat-omap: android-display: add 2 extra VRAM buffers for gc320
    2 extra VRAM buffers are needed to gc320 composition
    
    Change-Id: I5e02392f32a93c2eb12314151cd3a72b44fd4ae9
    Signed-off-by: Dima Svetlov <svetlov@ti.com>

--- arch/arm/plat-omap/android-display.c
+++ arch/arm/plat-omap/android-display.c
@@ -162,6 +162,14 @@ static void set_vram_sizes(struct sgx_omaplfb_config *sgx_config,
 	}
 
 	if (sgx_config) {
+#if defined(CONFIG_GCBV)
+		/* Add 2 extra VRAM buffers for gc320 composition - 4470 only*/
+		/* TODO: cpu_is_omap447x() is not returning the proper value
+			at this stage. Need to fix it */
+		if (1/*cpu_is_omap447x()*/)
+			sgx_config->vram_buffers += 2;
+#endif
+
 		vram += sgx_config->vram_reserve;
 		num_vram_buffers = max(sgx_config->vram_buffers,
 				       num_vram_buffers);
