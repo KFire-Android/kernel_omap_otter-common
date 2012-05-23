7766e54c80b62f0b0eedf2f20d318af67b55e761
Dan Murphy <dmurphy@ti.com>
2012-05-23 07:52:14 -0500

    ARM:OMAP: Add empty omap54xx_sevm header
    Add the empty product common header
    
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

---
+++ arch/arm/mach-omap2/board-54xx-sevm.h
@@ -0,0 +1,21 @@
+/*
+ * arch/arm/mach-omap2/board-54xx-sevm.h
+ *
+ * Copyright (C) 2012 Texas Instruments
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _MACH_OMAP_BOARD_54XX_SEVM_H
+#define _MACH_OMAP_BOARD_54XX_SEVM_H
+
+
+#endif
