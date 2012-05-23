dd44422cb336ef5ac84dd223a551818eb5af9337
Andy Gross <andy.gross@ti.com>
2012-05-23 12:54:53 -0500

    omap5: panda: Create omap dmm device
    Create omap dmm device in the correct order.
    
    Change-Id: Ib68cd396ea7255dfbd206758f8a63a25f9622c05
    Signed-off-by: Andy Gross <andy.gross@ti.com>

--- arch/arm/mach-omap2/board-omap5panda.c
+++ arch/arm/mach-omap2/board-omap5panda.c
@@ -38,6 +38,7 @@
 #include <asm/hardware/gic.h>
 #include <plat/common.h>
 #include <plat/mmc.h>
+#include <plat/drm.h>
 #include "hsmmc.h"
 #include "common-board-devices.h"
 #include "mux.h"
@@ -729,6 +730,7 @@ static void __init omap_5_panda_init(void)
 	omap_hsmmc_init(mmc);
 	platform_add_devices(omap5evm_devices, ARRAY_SIZE(omap5evm_devices));
 
+	omap_init_dmm_tiler();
 	omap5panda_display_init();
 }
 
