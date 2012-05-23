fee85f36cf612e7783a257cb43bb34c7c92f9e4a
Leed Aguilar <leed.aguilar@ti.com>
2012-05-23 17:27:46 -0400

    ARM: OMAP5: sEVM: Register the TSL2771 sensor in board file
    The TAOS TSL2771 ALS and proximity sensor is connected on
    the I2C bus 2 addr 0x39 and it uses the GPIO 149 as an
    interrupt line
    
    Change-Id: I28797f354bbdf48b7b67aca13ca1db1b29cfcd7a
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- arch/arm/mach-omap2/board-54xx-sevm-sensors.c
+++ arch/arm/mach-omap2/board-54xx-sevm-sensors.c
@@ -21,12 +21,43 @@
 #include <linux/i2c.h>
 #include <linux/io.h>
 #include <linux/init.h>
+#include <linux/i2c/tsl2771.h>
 
 #include <plat/i2c.h>
+
+#define OMAP5_TSL2771_INT_GPIO		149
+
+struct tsl2771_platform_data tsl2771_data = {
+	.irq_flags      = (IRQF_TRIGGER_LOW | IRQF_ONESHOT),
+	.flags          = (TSL2771_USE_ALS | TSL2771_USE_PROX),
+	.def_enable                     = 0x0,
+	.als_adc_time                   = 0xdb,
+	.prox_adc_time                  = 0xff,
+	.wait_time                      = 0x00,
+	.als_low_thresh_low_byte        = 0x0,
+	.als_low_thresh_high_byte       = 0x0,
+	.als_high_thresh_low_byte       = 0x0,
+	.als_high_thresh_high_byte      = 0x0,
+	.prox_low_thresh_low_byte       = 0x0,
+	.prox_low_thresh_high_byte      = 0x0,
+	.prox_high_thresh_low_byte      = 0x0,
+	.prox_high_thresh_high_byte     = 0x0,
+	.interrupt_persistence          = 0xf6,
+	.config                         = 0x00,
+	.prox_pulse_count               = 0x03,
+	.gain_control                   = 0xE0,
+	.glass_attn                     = 0x01,
+	.device_factor                  = 0x34,
+};
 
 static struct i2c_board_info __initdata omap5evm_sensor_i2c2_boardinfo[] = {
 	{
 		I2C_BOARD_INFO("bmp085", 0x77),
+	},
+	{
+		I2C_BOARD_INFO("tsl2771", 0x39),
+		.platform_data = &tsl2771_data,
+		.irq = OMAP5_TSL2771_INT_GPIO,
 	},
 };
 
