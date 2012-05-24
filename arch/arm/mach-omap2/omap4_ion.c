312867cc8abb2b2877d424d87e4e91d0cb59e03a
Dan Murphy <dmurphy@ti.com>
2012-05-24 18:37:19 +0530

    OMAP: Add ION Memmanager support
    Add a common ION file to initialize the ION memory
    spaces for DUCATI, Tiler and large surfaces
    
    Change-Id: I17e608d90c5568bd4be1285cee1976e05192950d
    Signed-off-by: Dan Murphy <dmurphy@ti.com>
    Signed-off-by: Varadarajan, Charulatha <charu@ti.com>

---
+++ arch/arm/mach-omap2/omap4_ion.c
@@ -0,0 +1,76 @@
+/*
+ * ION Initialization for OMAP4.
+ *
+ * Copyright (C) 2011 Texas Instruments
+ *
+ * Author: Dan Murphy <dmurphy@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/ion.h>
+#include <linux/memblock.h>
+#include <linux/omap_ion.h>
+#include <linux/platform_device.h>
+
+#include "omap4_ion.h"
+
+static struct ion_platform_data omap4_ion_data = {
+	.nr = 3,
+	.heaps = {
+		{
+			.type = ION_HEAP_TYPE_CARVEOUT,
+			.id = OMAP_ION_HEAP_SECURE_INPUT,
+			.name = "secure_input",
+			.base = PHYS_ADDR_SMC_MEM -
+					OMAP4_ION_HEAP_SECURE_INPUT_SIZE,
+			.size = OMAP4_ION_HEAP_SECURE_INPUT_SIZE,
+		},
+		{	.type = OMAP_ION_HEAP_TYPE_TILER,
+			.id = OMAP_ION_HEAP_TILER,
+			.name = "tiler",
+			.base = PHYS_ADDR_DUCATI_MEM -
+					OMAP4_ION_HEAP_TILER_SIZE,
+			.size = OMAP4_ION_HEAP_TILER_SIZE,
+		},
+		{
+			.type = OMAP_ION_HEAP_TYPE_TILER,
+			.id = OMAP_ION_HEAP_NONSECURE_TILER,
+			.name = "nonsecure_tiler",
+			.base = 0x80000000 + SZ_512M + SZ_2M,
+			.size = OMAP4_ION_HEAP_NONSECURE_TILER_SIZE,
+		},
+	},
+};
+
+static struct platform_device omap4_ion_device = {
+	.name = "ion-omap",
+	.id = -1,
+	.dev = {
+		.platform_data = &omap4_ion_data,
+	},
+};
+
+void __init omap4_register_ion(void)
+{
+	platform_device_register(&omap4_ion_device);
+}
+
+void __init omap4_ion_init(void)
+{
+	int i;
+	int ret;
+
+	for (i = 0; i < omap4_ion_data.nr; i++)
+		if (omap4_ion_data.heaps[i].type == ION_HEAP_TYPE_CARVEOUT ||
+		    omap4_ion_data.heaps[i].type == OMAP_ION_HEAP_TYPE_TILER) {
+			ret = memblock_remove(omap4_ion_data.heaps[i].base,
+					      omap4_ion_data.heaps[i].size);
+			if (ret)
+				pr_err("memblock remove of %x@%lx failed\n",
+				       omap4_ion_data.heaps[i].size,
+				       omap4_ion_data.heaps[i].base);
+		}
+}
---
+++ arch/arm/mach-omap2/omap5_ion.h
@@ -0,0 +1,43 @@
+/*
+ * arch/arm/mach-omap2/omap5_ion.h
+ *
+ * Copyright (C) 2011 Texas Instruments
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _OMAP5_ION_H
+#define _OMAP5_ION_H
+
+#define PHYS_ADDR_SMC_SIZE		(SZ_1M * 3)
+#define PHYS_ADDR_SMC_MEM		(0x80000000 + SZ_1G -		\
+						PHYS_ADDR_SMC_SIZE)
+#define OMAP_ION_HEAP_SECURE_INPUT_SIZE	(SZ_1M * 90)
+#define OMAP_ION_HEAP_TILER_SIZE		(SZ_128M - SZ_32M)
+#define PHYS_ADDR_DUCATI_SIZE			(SZ_1M * 165)
+#define PHYS_ADDR_DUCATI_MEM		(PHYS_ADDR_SMC_MEM -		\
+						PHYS_ADDR_DUCATI_SIZE -\
+						OMAP_ION_HEAP_SECURE_INPUT_SIZE)
+
+/* TODO: Clean this up and align with OMAP4 as well */
+#define OMAP5_ION_HEAP_NONSECURE_TILER_SIZE	(SZ_1M * 15)
+#define OMAP5_ION_HEAP_TILER_SIZE	(SZ_128M - SZ_32M - \
+					OMAP5_ION_HEAP_NONSECURE_TILER_SIZE)
+
+#ifdef CONFIG_ION_OMAP
+void omap5_ion_init(void);
+void omap5_register_ion(void);
+#else
+static inline void omap5_ion_init(void) { return; }
+static inline void omap5_register_ion(void) { return; }
+#endif
+
+#endif
