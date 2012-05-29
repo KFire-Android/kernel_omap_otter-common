7ad1b1eb5287a7d7be2205b1e26cd61e69fe076e
Kishon Vijay Abraham I <kishon@ti.com>
2012-05-29 15:14:18 -0500

    drivers: usb: add a new driver for usb3 phy
    Added a driver for usb3 phy that handles the interaction between usb phy
    device and dwc3 controller. Writing to control module is taken care of by
    API's provided by scm driver.
    
    Change-Id: Idae259e512988f62bd63687c76eaf53ea5d8f685
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- include/linux/usb/omap_usb.h
+++ include/linux/usb/omap_usb.h
@@ -23,14 +23,55 @@
 #ifndef __DRIVERS_OMAP_USB2_H
 #define __DRIVERS_OMAP_USB2_H
 
+#include <linux/io.h>
 #include <linux/usb/otg.h>
+
+struct usb_dpll_params {
+	u16	m;
+	u8	n;
+	u8	freq:3;
+	u8	sd;
+	u32	mf;
+};
+
+enum sys_clk_rate {
+	CLK_RATE_UNDEFINED = -1,
+	CLK_RATE_12MHZ,
+	CLK_RATE_16MHZ,
+	CLK_RATE_19MHZ,
+	CLK_RATE_26MHZ,
+	CLK_RATE_38MHZ
+};
+
+#define	NUM_SYS_CLKS		5
+#define	PLL_STATUS		0x00000004
+#define	PLL_GO			0x00000008
+#define	PLL_CONFIGURATION1	0x0000000C
+#define	PLL_CONFIGURATION2	0x00000010
+#define	PLL_CONFIGURATION3	0x00000014
+#define	PLL_CONFIGURATION4	0x00000020
+
+#define	PLL_REGM_MASK		0x001FFE00
+#define	PLL_REGM_SHIFT		0x9
+#define	PLL_REGM_F_MASK		0x0003FFFF
+#define	PLL_REGM_F_SHIFT	0x0
+#define	PLL_REGN_MASK		0x000001FE
+#define	PLL_REGN_SHIFT		0x1
+#define	PLL_SELFREQDCO_MASK	0x0000000E
+#define	PLL_SELFREQDCO_SHIFT	0x1
+#define	PLL_SD_MASK		0x0003FC00
+#define	PLL_SD_SHIFT		0x9
+#define	SET_PLL_GO		0x1
+#define	PLL_LOCK		0x2
 
 struct omap_usb {
 	struct usb_phy		phy;
 	struct phy_companion	*comparator;
+	void __iomem		*pll_ctrl_base;
 	struct device		*dev;
 	struct device		*control_dev;
 	struct clk		*wkupclk;
+	struct clk		*optclk;
 	u8			is_suspended:1;
 };
 
@@ -43,5 +84,16 @@ static inline void omap_usb2_set_comparator(struct phy_companion *comparator)
 {
 }
 #endif
+
+static inline u32 omap_usb_readl(const void __iomem *addr, unsigned offset)
+{
+	return __raw_readl(addr + offset);
+}
+
+static inline void omap_usb_writel(const void __iomem *addr, unsigned offset,
+								u32 data)
+{
+	__raw_writel(data, addr + offset);
+}
 
 #endif /* __DRIVERS_OMAP_USB_H */
