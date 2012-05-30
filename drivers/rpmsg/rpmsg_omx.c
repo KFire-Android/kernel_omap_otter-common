fd60740888b8d8c179013831a73569e30f31dfaa
Hemant Hariyani <hemanthariyani@ti.com>
2012-05-30 10:29:06 -0500

    rpmsg-omx: Use ion abstraction to get handle from PVR fd
    PVRExportFDToIONHandles was being called directly by rpmsg-omx.
    This is now abstracted by ion and drivers should use
    omap_ion_fd_to_handles instead.
    
    Change-Id: Id9886d490e9730cfeac83a3602a7b4e91811e9b8
    Signed-off-by: Hemant Hariyani <hemanthariyani@ti.com>
    Signed-off-by: Rodrigo Obregon <robregon@ti.com>

--- drivers/rpmsg/rpmsg_omx.c
+++ drivers/rpmsg/rpmsg_omx.c
@@ -145,17 +145,18 @@ static int _rpmsg_omx_buffer_lookup(struct rpmsg_omx_instance *omx,
 			goto exit;
 		}
 
-#ifdef CONFIG_PVR_SGX
 		/* how about an sgx buffer wrapping an ion handle? */
 		{
 			int fd;
 			struct ion_handle *handles[2] = { NULL, NULL };
 			struct ion_client *pvr_ion_client;
 			ion_phys_addr_t paddr2;
+			int num_handles = 2;
 
 			fd = buffer;
-			PVRSRVExportFDToIONHandles(fd, &pvr_ion_client,
-					handles);
+			if (omap_ion_fd_to_handles(fd, &pvr_ion_client,
+					handles, &num_handles) < 0)
+				goto nopvr;
 
 			/* Get the 1st buffer's da */
 			if ((handles[0]) && !ion_phys(pvr_ion_client,
@@ -176,9 +177,8 @@ static int _rpmsg_omx_buffer_lookup(struct rpmsg_omx_instance *omx,
 					goto exit;
 			}
 		}
-
-#endif
 	}
+nopvr:
 exit:
 #endif
 
