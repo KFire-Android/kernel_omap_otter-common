c3a609b35acf491c295206f562802fc820f928bf
Misael Lopez Cruz <misael.lopez@ti.com>
2012-06-04 20:49:58 -0500

    ASoC: omap-abe-twl6040: Enable headset detection for omap5sevm
    OMAP5 sEVM supports headset detection based on twl6040 PLUGINT interrupt.
    
    Change-Id: Id65f364c6944f8cffc09a7531af5e89dffca1086
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/soc/omap/omap-abe-twl6040.c
+++ sound/soc/omap/omap-abe-twl6040.c
@@ -471,7 +471,7 @@ static int omap_abe_twl6040_init(struct snd_soc_pcm_runtime *rtd)
 
 		ret = snd_soc_jack_add_pins(&hs_jack, ARRAY_SIZE(hs_jack_pins),
 					hs_jack_pins);
-		if (machine_is_omap_4430sdp())
+		if (machine_is_omap_4430sdp() || machine_is_omap5_sevm())
 			twl6040_hs_jack_detect(codec, &hs_jack, SND_JACK_HEADSET);
 		else
 			snd_soc_jack_report(&hs_jack, SND_JACK_HEADSET, SND_JACK_HEADSET);
