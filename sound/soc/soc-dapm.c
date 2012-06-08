c10856b0f6aab80d4dc13addbc87f355eb58d7cd
Misael Lopez Cruz <misael.lopez@ti.com>
2012-06-07 20:11:09 -0500

    ASoC: dapm: Use card's power_mutex during codec shutdown
    Codec shutdown performs a DAPM power sequence that might cause conflicts
    and/or race conditions if another stream power event is running simultaneously.
    Use card's power_mutex to protect any potential race condition between them.
    
    ASoC: dapm: Fix race condition for widgets power list creation
    
    Multiple calls to dapm_power_widgets() can create a race condition
    causing widget power list to be corrupted. Those scenarios can
    occur in multistream usecases, stream start/stop along with update
    power calls (mixer, mux, dapm_sync, stream_event) simultaneously.
    
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>
    
    porting from p-android-omap-3.0(commit cfb8769c and e224f7e)
    to p-linux-omap-3.4 as part of k3.0 to k3.4 migration effort
    for OMAP4 Tablet2 support
    
    Change-Id: I3de96b93eea697d255e8f40d198eea12fe8ea4cc
    Signed-off-by: Ranganath Krishnan <ranganath@ti.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -1571,6 +1571,7 @@ static int dapm_power_widgets(struct snd_soc_dapm_context *dapm, int event)
 		dapm_power_one_widget(w, &up_list, &down_list);
 	}
 
+	mutex_lock(&card->power_mutex);
 	list_for_each_entry(w, &card->widgets, list) {
 		switch (w->id) {
 		case snd_soc_dapm_pre:
@@ -1653,6 +1654,8 @@ static int dapm_power_widgets(struct snd_soc_dapm_context *dapm, int event)
 	pop_wait(card->pop_time);
 
 	trace_snd_soc_dapm_done(card);
+
+	mutex_unlock(&card->power_mutex);
 
 	return 0;
 }
@@ -3510,9 +3513,12 @@ EXPORT_SYMBOL_GPL(snd_soc_dapm_free);
 
 static void soc_dapm_shutdown_codec(struct snd_soc_dapm_context *dapm)
 {
+	struct snd_soc_card *card = dapm->card;
 	struct snd_soc_dapm_widget *w;
 	LIST_HEAD(down_list);
 	int powerdown = 0;
+
+	mutex_lock(&card->power_mutex);
 
 	list_for_each_entry(w, &dapm->card->widgets, list) {
 		if (w->dapm != dapm)
@@ -3536,6 +3542,8 @@ static void soc_dapm_shutdown_codec(struct snd_soc_dapm_context *dapm)
 			snd_soc_dapm_set_bias_level(dapm,
 						    SND_SOC_BIAS_STANDBY);
 	}
+
+	mutex_unlock(&card->power_mutex);
 }
 
 /*
