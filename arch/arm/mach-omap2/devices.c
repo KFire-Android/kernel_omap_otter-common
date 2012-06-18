4eed287dd57a5927d85158fd24f3e71f32811ddd
Sebastien Guiriec <s-guiriec@ti.com>
2012-06-18 16:58:09 +0200

    OMAP4: devices: Do not create aess device if the dtb has been provided
    If dtb is provided the needed device will be created dynamically.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -767,7 +767,6 @@ static int __init omap2_init_devices(void)
 	 * please keep these calls, and their implementations above,
 	 * in alphabetical order so they're easier to sort through.
 	 */
-	omap_init_aess();
 	omap_init_audio();
 	omap_init_camera();
 	omap_init_hdmi_audio();
@@ -775,6 +774,7 @@ static int __init omap2_init_devices(void)
 	omap_init_mbox();
 	/* If dtb is there, the devices will be created dynamically */
 	if (!of_have_populated_dt()) {
+		omap_init_aess();
 		omap_init_dmic();
 		omap_init_mcpdm();
 		omap_init_mcspi();
