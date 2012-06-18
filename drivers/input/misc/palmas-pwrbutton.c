511b11121f7aee085aa16004257c749bc8282e6c
Dan Murphy <dmurphy@ti.com>
2012-06-18 13:50:50 -0500

    Palmas: Fix power button to read the line status
    The PWRON interrupt from palmas is only a falling edge condition so the driver will only
    be called once when the button is pressed. And the current implementation reports a quick
    press and release of the key to the input device driver.
    
    To fix this there is a delayed work item that is scheduled when the
    key is pressed to read the PWRON line status.
    
    The work item will be scheduled @ 20ms intervals until the button is released.
    
    Change-Id: I89ebcdec71372650e9478b32532da7a43a6c1c95
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- drivers/input/misc/palmas-pwrbutton.c
+++ drivers/input/misc/palmas-pwrbutton.c
@@ -20,6 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -30,19 +31,77 @@
 #include <linux/platform_device.h>
 #include <linux/mfd/palmas.h>
 
+#define PALMAS_PWR_KEY_PRESS	0x01
+#define PALMAS_PWR_KEY_Q_TIME	20	/* Time in ms */
+
 struct palmas_pwron {
 	struct palmas *palmas;
 	struct input_dev *input_dev;
+	struct delayed_work input_work;
+	int current_state;
 };
+
+static int palmas_get_pwr_state(struct palmas_pwron *pwron)
+{
+	unsigned int addr;
+	unsigned int reg;
+	int ret, slave;
+
+	slave = PALMAS_BASE_TO_SLAVE(PALMAS_INTERRUPT_BASE);
+	addr = PALMAS_BASE_TO_REG(PALMAS_INTERRUPT_BASE,
+			PALMAS_INT1_LINE_STATE);
+
+	ret = regmap_read(pwron->palmas->regmap[slave], addr, &reg);
+	if (ret) {
+		pr_err("%s:Cannot read palmas PWRON status.\n", __func__);
+		return -1;
+	}
+	/* PWRON line state is bit two of the register.
+	 * Shift down the bit and invert the data for the key to be
+	 * recognized.
+	 */
+	return !(reg >> 1 & PALMAS_PWR_KEY_PRESS);
+}
+
+static void palmas_power_work(struct work_struct *work)
+{
+	struct palmas_pwron *pwron = container_of((struct delayed_work *)work,
+					  struct palmas_pwron,
+					  input_work);
+	int next_state;
+
+	next_state = palmas_get_pwr_state(pwron);
+	if (next_state < 0)
+		return;
+
+	/* If the state did not change then schedule a work item to check the
+	 * status of the power button line */
+	if (next_state == pwron->current_state)
+		schedule_delayed_work(&pwron->input_work,
+			msecs_to_jiffies(PALMAS_PWR_KEY_Q_TIME));
+	else {
+		pwron->current_state = next_state;
+		input_report_key(pwron->input_dev, KEY_POWER,
+				pwron->current_state);
+		input_sync(pwron->input_dev);
+	}
+}
 
 static irqreturn_t pwron_irq(int irq, void *palmas_pwron)
 {
 	struct palmas_pwron *pwron = palmas_pwron;
 	struct input_dev *input_dev = pwron->input_dev;
 
-	input_report_key(input_dev, KEY_POWER, 1);
-	input_report_key(input_dev, KEY_POWER, 0);
+	cancel_delayed_work_sync(&pwron->input_work);
+	pwron->current_state = palmas_get_pwr_state(pwron);
+	if (pwron->current_state < 0)
+		return IRQ_HANDLED;
+
+	input_report_key(input_dev, KEY_POWER, pwron->current_state);
 	input_sync(input_dev);
+
+	if (pwron->current_state == PALMAS_PWR_KEY_PRESS)
+		schedule_delayed_work(&pwron->input_work, 0);
 
 	return IRQ_HANDLED;
 }
@@ -88,6 +147,8 @@ static int __devinit palmas_pwron_probe(struct platform_device *pdev)
 	regmap_update_bits(palmas->regmap[slave], addr,
 			LONG_PRESS_KEY_LPK_TIME_MASK, 0);
 
+	INIT_DELAYED_WORK(&pwron->input_work, palmas_power_work);
+
 	err = request_threaded_irq(irq, NULL, pwron_irq,
 			IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
 			"palmas_pwron", pwron);
@@ -102,7 +163,7 @@ static int __devinit palmas_pwron_probe(struct platform_device *pdev)
 		goto free_irq;
 	}
 
-	platform_set_drvdata(pdev, input_dev);
+	platform_set_drvdata(pdev, pwron);
 
 	return 0;
 
@@ -117,7 +178,8 @@ free_pwron:
 
 static int __devexit palmas_pwron_remove(struct platform_device *pdev)
 {
-	struct input_dev *input_dev = platform_get_drvdata(pdev);
+	struct palmas_pwron *pwron = platform_get_drvdata(pdev);
+	struct input_dev *input_dev = pwron->input_dev;
 	int irq = platform_get_irq(pdev, 0);
 
 	free_irq(irq, input_dev);
@@ -126,11 +188,48 @@ static int __devexit palmas_pwron_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_PM
+/**
+ *	palmas_pwron_suspend - called on device suspend
+ *	@dev: device being suspended
+ *
+ *	Cancel the pending work item for reading the power button
+ */
+static int palmas_pwron_suspend(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct palmas_pwron *pwron = platform_get_drvdata(pdev);
+
+	cancel_delayed_work_sync(&pwron->input_work);
+
+	return 0;
+}
+
+/**
+ *	palmas_pwron_resume - called on device resume
+ *	@dev: device being resumed
+ *
+ *	Do nothing for now as there is nothing to do but leave the placeholder
+ */
+static int palmas_pwron_resume(struct device *dev)
+{
+	/* Do nothing for now */
+	return 0;
+}
+
+static UNIVERSAL_DEV_PM_OPS(palmas_pwron_pm, palmas_pwron_suspend,
+			palmas_pwron_resume, NULL);
+
+#else
+static UNIVERSAL_DEV_PM_OPS(palmas_pwron_pm, NULL, NULL, NULL);
+#endif
+
 static struct platform_driver palmas_pwron_driver = {
 	.remove		= __devexit_p(palmas_pwron_remove),
 	.driver		= {
 		.name	= "palmas-pwrbutton",
 		.owner	= THIS_MODULE,
+		.pm	= &palmas_pwron_pm,
 	},
 };
 
