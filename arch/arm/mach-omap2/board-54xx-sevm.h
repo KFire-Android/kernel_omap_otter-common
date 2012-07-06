e97fd5618259b4316cc1a2a2d310bbcbd1074ae0
Huzefa Kankroliwala <huzefank@ti.com>
2012-07-06 13:07:49 -0500

    OMAP5: seVM board file: fix build break for missing header
    The current OMAP5 sEVM board file board-omap5evm.c includes
    only board-54xx-sevm.h file and NOT board-omap5evm.h file.
    This causes build break since UART function is declared there.
    Fix this by including board-omap5evm.h in board-54xx-sevm.h
    
    Change-Id: I72733f4b94848df7a6f68858dfd9f2414ace342b
    Signed-off-by: Huzefa Kankroliwala <huzefank@ti.com>

--- arch/arm/mach-omap2/board-54xx-sevm.h
+++ arch/arm/mach-omap2/board-54xx-sevm.h
@@ -17,16 +17,12 @@
 #ifndef _MACH_OMAP_BOARD_54XX_SEVM_H
 #define _MACH_OMAP_BOARD_54XX_SEVM_H
 
+#include "board-omap5evm.h"
+
 int __init sevm_dock_init(void);
 int __init sevm_touch_init(void);
 int __init sevm_sensor_init(void);
 int __init omap5sevm_connectivity_init(void);
 int __init sevm_panel_init(void);
-
-#ifdef CONFIG_OMAP_HSI
-extern void __init omap5evm_modem_init(bool force_mux);
-#else
-static inline void __init omap5evm_modem_init(bool force_mux) { }
-#endif
 
 #endif
