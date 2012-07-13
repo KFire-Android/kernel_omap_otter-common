5d52d6cd1138892d7059c5d91278b8a611fcd828
Craig Stout <craig.stout@ti.com>
2012-07-13 16:21:11 -0700

    gcx: bump version to 2.1.2
    Change-Id: I97c3c17e6b1cb74f110bd15c4c61bcf031c91b57
    Signed-off-by: Craig Stout <craig.stout@ti.com>

--- drivers/misc/gcx/gcioctl/version.h
+++ drivers/misc/gcx/gcioctl/version.h
@@ -1,4 +1,4 @@
-#define VER_FILEVERSION             {2, 1, 1, 0}
-#define VER_FILEVERSION_STR         "2.1.1.0\0"
-#define VER_PRODUCTVERSION          {2, 1, 1, 0}
+#define VER_FILEVERSION             {2, 1, 2, 0}
+#define VER_FILEVERSION_STR         "2.1.2.0\0"
+#define VER_PRODUCTVERSION          {2, 1, 2, 0}
 #define VER_PRODUCTVERSION_STR      "2.1\0"
