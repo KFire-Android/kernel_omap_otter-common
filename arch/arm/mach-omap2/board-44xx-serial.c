60b4582e3015506702b9ca1e2b938c7e9b612f28
Semen Protsenko <semen.protsenko@ti.com>
2012-07-25 15:12:13 +0300

    OMAP: UART: Specify DMA RX buffer size
    At the moment DMA mode is disabled for UART. When enable it -- there is
    continuously printing "DMA misaligned error" even though data is aligned
    properly (tested with IS_ALIGNED() macro). Actually the problem is in zero
    length size for RX DMA buffer.
    
    This patch set RX DMA buffer size equals to default size to prevent futher
    problems if DMA will be enabled for UART. Also it set other DMA RX fields to
    their default values.
    
    This patch inspired by OMAP5 serial board file (board-omap5evm-serial.c)
    in k3.4.
    
    Change-Id: Id73c30892b2104b4283783e740f137514be64a1f
    Signed-off-by: Semen Protsenko <semen.protsenko@ti.com>

--- arch/arm/mach-omap2/board-44xx-serial.c
+++ arch/arm/mach-omap2/board-44xx-serial.c
@@ -99,18 +99,27 @@ static struct omap_device_pad tablet_uart4_pads[] __initdata = {
 
 static struct omap_uart_port_info tablet_uart_info_uncon __initdata = {
 	.use_dma	= 0,
+	.dma_rx_timeout = DEFAULT_RXDMA_TIMEOUT,
+	.dma_rx_poll_rate = DEFAULT_RXDMA_POLLRATE,
+	.dma_rx_buf_size = DEFAULT_RXDMA_BUFSIZE,
 	.auto_sus_timeout = DEFAULT_AUTOSUSPEND_DELAY,
 	.wer = 0,
 };
 
 static struct omap_uart_port_info tablet_uart_info __initdata = {
 	.use_dma	= 0,
+	.dma_rx_timeout = DEFAULT_RXDMA_TIMEOUT,
+	.dma_rx_poll_rate = DEFAULT_RXDMA_POLLRATE,
+	.dma_rx_buf_size = DEFAULT_RXDMA_BUFSIZE,
 	.auto_sus_timeout = DEFAULT_AUTOSUSPEND_DELAY,
 	.wer = (OMAP_UART_WER_TX | OMAP_UART_WER_RX | OMAP_UART_WER_CTS),
 };
 
 static struct omap_uart_port_info tablet_wilink_uart_info __initdata = {
 	.use_dma	= 0,
+	.dma_rx_timeout = DEFAULT_RXDMA_TIMEOUT,
+	.dma_rx_poll_rate = DEFAULT_RXDMA_POLLRATE,
+	.dma_rx_buf_size = DEFAULT_RXDMA_BUFSIZE,
 	.auto_sus_timeout = DEFAULT_AUTOSUSPEND_DELAY,
 	.wer = (OMAP_UART_WER_TX | OMAP_UART_WER_RX | OMAP_UART_WER_CTS),
 	.rts_mux_driver_control = 1,
