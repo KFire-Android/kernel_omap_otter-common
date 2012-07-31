ffa9f706fe59fcd9835ee8de64661bad8a36b567
Andrii Beregovenko <a.beregovenko@ti.com>
2012-07-31 17:19:16 -0500

    blaze_tablet: panel: disable LED only when 0 is set
    During animated disabling of LED, brighness decrease step by step up
    to zero. In this way panel will be disabled twice that leads to
    disabling of LCD pannel too, that is incorrect bahavior.
    
    Such problem present on Tablet2 boards and caused by schematic
    connection between reset pin GPIO 102 of LVDS chip (TC358765XBG-BGA64)
    and the LCD backlight control signal "LED_BKLT_EN".
    
    Signed-off-by: Andrii Beregovenko <a.beregovenko@ti.com>
    Change-Id: I46fcb4cc18439a0f70b2e649f1eba677630fe3e7

--- arch/arm/mach-omap2/board-44xx-tablet-panel.c
+++ arch/arm/mach-omap2/board-44xx-tablet-panel.c
@@ -87,7 +87,8 @@ static void omap4_tablet_set_primary_brightness(u8 brightness)
 	} else if (brightness <= 1) {
 		twl_i2c_write_u8(TWL6030_MODULE_ID1, 0x08, TWL6030_TOGGLE3);
 		twl_i2c_write_u8(TWL6030_MODULE_ID1, 0x38, TWL6030_TOGGLE3);
-		gpio_set_value(LED_DISP_EN, 0);
+		if (brightness == 0)
+			gpio_set_value(LED_DISP_EN, 0);
 	}
 }
 
