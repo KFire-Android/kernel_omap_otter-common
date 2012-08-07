083a3c670a6eda13a17e18f3dd9597ada93c4086
Eugen Mandrenko <ievgen.mandrenko@ti.com>
2012-08-07 13:06:50 +0300

    OMAP4: DPLL cascading: remove unused CM_SHADOW_FREQ_CONFIG2
    The value from CM_SHADOW_FREQ_CONFIG2 can't be applied because
    GPMC is always active and can't be entered in idle state.
    
    The GPMC is always active because on OMAP4, CPU accesses on
    unmapped addresses are redirected to GPMC by L3 interconnect.
    CPU is speculative nature and such accesses are possible which
    can lead to indirect access to GPMC and if it's clock is
    not running, it can result in hang/abort on the platform.
    See path d60565948468b for more information.
    
    Change-Id: Ia62d8bf8c59ac3a06445b6f5f9e08ad0c9369c5b
    Signed-off-by: Eugen Mandrenko <ievgen.mandrenko@ti.com>

--- arch/arm/mach-omap2/dpll44xx.c
+++ arch/arm/mach-omap2/dpll44xx.c
@@ -219,7 +219,7 @@ int omap4_core_dpll_m2_set_rate(struct clk *clk, unsigned long rate)
  */
 int omap4_core_dpll_set_rate(struct clk *clk, unsigned long rate)
 {
-	int i = 0, m2_div, m5_div;
+	int i = 0, m2_div;
 	u32 mask, reg;
 	u32 shadow_freq_cfg1 = 0, shadow_freq_cfg2 = 0;
 	struct clk *new_parent;
@@ -299,27 +299,6 @@ int omap4_core_dpll_set_rate(struct clk *clk, unsigned long rate)
 		omap_emif_setup_registers(rate / 2, LPDDR2_VOLTAGE_STABLE);
 
 		/*
-		 * program CM_DIV_M5_DPLL_CORE.DPLL_CLKOUT_DIV into shadow
-		 * register as well as L3_CLK freq and update GPMC frequency
-		 *
-		 * HACK: hardcode L3_CLK = CORE_CLK / 2 for DPLL cascading
-		 * HACK: hardcode CORE_CLK = CORE_X2_CLK / 2 for DPLL
-		 * cascading
-		 */
-		m5_div = omap4_prm_read_bits_shift(
-				dpll_core_m5x2_ck->clksel_reg,
-				dpll_core_m5x2_ck->clksel_mask);
-
-		shadow_freq_cfg2 =
-			(m5_div << OMAP4430_DPLL_CORE_M5_DIV_SHIFT) |
-			(1 << OMAP4430_CLKSEL_L3_SHADOW_SHIFT) |
-			(0 << OMAP4430_CLKSEL_CORE_1_1_SHIFT) |
-			(1 << OMAP4430_GPMC_FREQ_UPDATE_SHIFT);
-
-		__raw_writel(shadow_freq_cfg2, OMAP4430_CM_SHADOW_FREQ_CONFIG2);
-
-
-		/*
 		 * program CM_DIV_M2_DPLL_CORE.DPLL_CLKOUT_DIV
 		 * for divide by two, ensure DLL_OVERRIDE = '1'
 		 * and put DPLL_CORE into LP Bypass
@@ -376,26 +355,6 @@ int omap4_core_dpll_set_rate(struct clk *clk, unsigned long rate)
 
 		/* program mn divider values */
 		omap4_prm_rmw_reg_bits(mask, reg, dd->mult_div1_reg);
-
-		/*
-		 * program CM_DIV_M5_DPLL_CORE.DPLL_CLKOUT_DIV into shadow
-		 * register as well as L3_CLK freq and update GPMC frequency
-		 *
-		 * HACK: hardcode L3_CLK = CORE_CLK / 2 for DPLL cascading
-		 * HACK: hardcode CORE_CLK = CORE_X2_CLK / 1 for DPLL
-		 * cascading
-		 */
-		m5_div = omap4_prm_read_bits_shift(
-				dpll_core_m5x2_ck->clksel_reg,
-				dpll_core_m5x2_ck->clksel_mask);
-
-		shadow_freq_cfg2 =
-			(m5_div << OMAP4430_DPLL_CORE_M5_DIV_SHIFT) |
-			(1 << OMAP4430_CLKSEL_L3_SHADOW_SHIFT) |
-			(0 << OMAP4430_CLKSEL_CORE_1_1_SHIFT) |
-			(1 << OMAP4430_GPMC_FREQ_UPDATE_SHIFT);
-
-		__raw_writel(shadow_freq_cfg2, OMAP4430_CM_SHADOW_FREQ_CONFIG2);
 
 		/*
 		 * program DPLL_CORE_M2_DIV with same value
