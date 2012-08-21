c9b32f2de152d52341b3a36d91b6fca05e8bfa9d
Grygorii Strashko <grygorii.strashko@ti.com>
2012-08-21 18:24:13 -0500

    ARM: OMAP: hwmod: Don't wakeup SW Conrolled clkdm when disabling module
    All HW Controllable blocks/modules need the module to be in FUNCT
    (functional) state before it can be DISABLED.
    The way to transition a module from IDLE to FUNCT state is to force
    the clockdomain, which the module belongs to, to wakeup (SW_WKUP)
    before disabling the module.
    But, if clockdomain is configured to support only SW_SLEEP/SW_WKUP
    modes, then:
    - it will be always in FUNC state before calling omap_hwmod_idle() or
    omap_hwmod_shutdown() APIs.
    - following sequence of code will cause wrong clockdomain usecounting:
    	omap_hwmod_enable(a:MODULEMODE_HWCTRL)
    		clkdm_hwmod_enable()	-> usecount=1; CLKTRCTRL=SW_WKUP
    		clk_enable()            -> usecount=2; CLKTRCTRL=SW_WKUP
    	omap_hwmod_idle(a:MODULEMODE_HWCTRL)
    		clkdm_hwmod_enable()	-> usecount=3; CLKTRCTRL=SW_WKUP
    << clkdm_usecount wrongly incremented here
    		clk_disable()           -> usecount=2; CLKTRCTRL=not changed
    		clkdm_hwmod_disable()	-> usecount=1; CLKTRCTRL=not chan
    
    Hence, don't force the clockdomain to SW_WKUP state if clockdomain
    doesn't support HW_AUTO mode when disabling HW Controllable module.
    
    Change-Id: I7db4a7a7916febbd2a9713b6b3b7be680322ae08
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -2182,9 +2182,12 @@ static int _idle(struct omap_hwmod *oh)
 	if (oh->class->sysc)
 		_idle_sysc(oh);
 	_del_initiator_dep(oh, mpu_oh);
-	if (oh->clkdm && (oh->prcm.omap4.modulemode == MODULEMODE_HWCTRL)) {
+	if (oh->clkdm && oh->clkdm->flags &&
+	    (oh->clkdm->flags & CLKDM_CAN_ENABLE_AUTO) &&
+	    (oh->prcm.omap4.modulemode == MODULEMODE_HWCTRL)) {
 		/*
-		 * For modules with modulemode configured as "auto"
+		 * For modules with modulemode configured as "auto" and
+		 * if this clockdomain supports HW_AUTO mode
 		 * the clockdomain must be in SW_WKUP before disabling
 		 * a module completely.
 		 */
@@ -2271,10 +2274,12 @@ static int _shutdown(struct omap_hwmod *oh)
 	/* clocks and deps are already disabled in idle */
 	if (oh->_state == _HWMOD_STATE_ENABLED) {
 		_del_initiator_dep(oh, mpu_oh);
-		if (oh->clkdm &&
-		    oh->prcm.omap4.modulemode == MODULEMODE_HWCTRL) {
+		if (oh->clkdm && oh->clkdm->flags &&
+		    (oh->clkdm->flags & CLKDM_CAN_ENABLE_AUTO) &&
+		    (oh->prcm.omap4.modulemode == MODULEMODE_HWCTRL)) {
 			/*
-			 * For modules with modulemode configured as "auto"
+			 * For modules with modulemode configured as "auto" and
+			 * if this clockdomain supports HW_AUTO mode
 			 * the clockdomain must be in SW_WKUP before disabling
 			 * a module completely.
 			 */
