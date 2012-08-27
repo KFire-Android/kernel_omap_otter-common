aeae586833f495632ddf83169eb3afb9a474afef
Fernando Guzman Lugo <fernando.lugo@ti.com>
2012-08-27 13:51:50 -0500

    rpmsg: omx: remove omx services linked list
    The omx services linked list is only used to perform a lookup in
    the rpmsg_omx_probe function for an already created omxserv. This
    lookup is now performed using the idr_for_each api, eliminating the
    need for the linked list. The code using this linked list has
    therefore been cleaned up.
    
    Change-Id: I64125914cad033aabcb969b9a2a4e5cffccd5799
    Signed-off-by: Fernando Guzman Lugo <fernando.lugo@ti.com>

--- drivers/rpmsg/rpmsg_omx.c
+++ drivers/rpmsg/rpmsg_omx.c
@@ -63,7 +63,6 @@ enum rpc_omx_map_info_type {
 };
 
 struct rpmsg_omx_service {
-	struct list_head next;
 	struct cdev cdev;
 	struct device *dev;
 	struct rpmsg_channel *rpdev;
@@ -98,7 +97,6 @@ static dev_t rpmsg_omx_dev;
 /* store all remote omx connection services (usually one per remoteproc) */
 static DEFINE_IDR(rpmsg_omx_services);
 static DEFINE_SPINLOCK(rpmsg_omx_services_lock);
-static LIST_HEAD(rpmsg_omx_services_list);
 
 #ifdef CONFIG_ION_OMAP
 static int _rpmsg_pa_to_da(struct rpmsg_omx_instance *omx, u32 pa, u32 *da)
@@ -678,19 +676,22 @@ static const struct file_operations rpmsg_omx_fops = {
 	.owner		= THIS_MODULE,
 };
 
+static int _match_omx_service(int id, void *p, void *data)
+{
+	struct rpmsg_omx_service *omxserv = p;
+
+	return strcmp(dev_name(omxserv->dev), data) ? 0 : (int)p;
+}
+
 static int rpmsg_omx_probe(struct rpmsg_channel *rpdev)
 {
 	int ret, major, minor;
-	struct rpmsg_omx_service *omxserv = NULL, *tmp;
+	struct rpmsg_omx_service *omxserv;
 
 	/* look for an already created omx service */
 	spin_lock(&rpmsg_omx_services_lock);
-	list_for_each_entry(tmp, &rpmsg_omx_services_list, next) {
-		if (!strcmp(dev_name(tmp->dev), rpdev->id.name)) {
-			omxserv = tmp;
-			break;
-		}
-	}
+	omxserv = (struct rpmsg_omx_service *)idr_for_each(&rpmsg_omx_services,
+					_match_omx_service, rpdev->id.name);
 	spin_unlock(&rpmsg_omx_services_lock);
 	if (omxserv)
 		goto serv_up;
@@ -719,8 +720,6 @@ static int rpmsg_omx_probe(struct rpmsg_channel *rpdev)
 	INIT_LIST_HEAD(&omxserv->list);
 	mutex_init(&omxserv->lock);
 	init_completion(&omxserv->comp);
-
-	list_add(&omxserv->next, &rpmsg_omx_services_list);
 
 	major = MAJOR(rpmsg_omx_dev);
 
@@ -775,7 +774,6 @@ static void __devexit rpmsg_omx_remove(struct rpmsg_channel *rpdev)
 	if (rproc->state != RPROC_CRASHED) {
 		device_destroy(rpmsg_omx_class, MKDEV(major, omxserv->minor));
 		cdev_del(&omxserv->cdev);
-		list_del(&omxserv->next);
 		mutex_unlock(&omxserv->lock);
 		spin_lock(&rpmsg_omx_services_lock);
 		idr_remove(&rpmsg_omx_services, omxserv->minor);
@@ -854,16 +852,7 @@ module_init(init);
 
 static void __exit fini(void)
 {
-	struct rpmsg_omx_service *omxserv, *tmp;
-	int major = MAJOR(rpmsg_omx_dev);
-
 	unregister_rpmsg_driver(&rpmsg_omx_driver);
-	list_for_each_entry_safe(omxserv, tmp, &rpmsg_omx_services_list, next) {
-		device_destroy(rpmsg_omx_class, MKDEV(major, omxserv->minor));
-		cdev_del(&omxserv->cdev);
-		list_del(&omxserv->next);
-		kfree(omxserv);
-	}
 	class_destroy(rpmsg_omx_class);
 	unregister_chrdev_region(rpmsg_omx_dev, MAX_OMX_DEVICES);
 }
