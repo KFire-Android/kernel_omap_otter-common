0bf7154eeaee2c5fc073aa5f4a456cca20025f08
Artem Rudenko <artemrudenko@ti.com>
2012-09-12 16:16:48 +0300

    OMAPDSS: DSSCOMP: support of MEM2MEM mode with blanked pannel
    This change implements MEM2MEM mode for pannels blanked case.
    
    Change-Id: Ia1e0034c73f196d877283e5ecc974f277988265e
    Signed-off-by: Artem Rudenko <artemrudenko@ti.com>

--- drivers/video/omap2/dsscomp/queue.c
+++ drivers/video/omap2/dsscomp/queue.c
@@ -723,6 +723,14 @@ skip_ovl_set:
 	if (!d->win.h && !d->win.y)
 		d->win.h = dssdev->panel.timings.y_res - d->win.y;
 
+	/* Report m2m_only mode to a manager. A manager will apply a new
+	 * composition and will free a previous composition immediately. */
+	mgr->m2m_only = comp->m2m_only;
+
+	/* turn on clock if m2m_only mode is enabled (LCD is OFF) */
+	/* turn off clock if LCD goes ON and prev comp was m2m_only */
+	dss_m2m_clock_handling(mgr);
+
 	if (wb_apply) {
 		struct omap_writeback_info wb_info;
 		struct omap_writeback *wb;
