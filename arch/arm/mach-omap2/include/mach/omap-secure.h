8fd48e2df163425c19ddb3b2f7d0d2b223d4421c
Srinivas Pulukuru <srinivas.pulukuru@ti.com>
2012-09-20 15:22:54 -0500

    HACK: ARM: OMAP2+: register secure device for use by security driver
    Register SMC driver as platform device and
    export PM functions needed by SMC driver as part of platform
    data - This is known to work with OMAP5 ES1.0, but the strategy
    can be made to work with multiple SoCs as well.
    Tested against:
    * SMC version AH01.01p2.36793.
    
    This is still a hack considering the APIs being exported out,
    but it allows us to scope the fixes that need to be
    eventually done.
    
    OMAP5 introduces an expected base and offset.
    Board files need to initiate the defaults using
    omap5_secure_workspace_addr_default() call in their .reserve
    prior to calling omap_reserve.
    This could have been handled using a cpu_is check, however
    IO maps are not ready at the point in time of reserve, nor
    is cpu_is check ready yet, so an explicit call is mandated
    per platform supported.
    
    If boards do not meet the assumptions of the TI default
    release of PPA, the board files may use the following API
    instead of omap5_secure_workspace_addr_default to set the
    the platform specific address and size as needed prior to
    omap_reserve is invoked as part of .reserve handler.
    void omap_secure_set_secure_workspace_addr(phys_addr_t secure_workspace_addr,
                                              phys_addr_t secure_workspace_size);
    
    Change-Id: Icdd82ff5446b5f45a52316038c72684f3da5f47e
    Signed-off-by: Srinivas Pulukuru <srinivas.pulukuru@ti.com>

--- arch/arm/mach-omap2/include/mach/omap-secure.h
+++ arch/arm/mach-omap2/include/mach/omap-secure.h
@@ -62,5 +62,69 @@ extern u32 omap_secure_dispatcher(u32 idx, u32 flag, u32 nargs,
 extern u32 omap_smc2(u32 id, u32 falg, u32 pargs);
 extern phys_addr_t omap_secure_ram_mempool_base(void);
 
+typedef u32 (*omap_secure_dispatcher_t)(u32 idx, u32 flag,
+			u32 nargs, u32 arg1, u32 arg2, u32 arg3, u32 arg4);
+struct clockdomain;
+/**
+ * struct omap_secure_platform_data -  For Secure driver to use
+ * @disable_nonboot_cpus:	standard linux API
+ * @enable_nonboot_cpus:	standard linux API
+ * @sched_setaffinity:		standard linux API
+ * @sched_getaffinity:		standard linux API
+ * @clkdm_allow_idle:		standard omap SoC API
+ * @clkdm_lookup:		standard omap SoC API
+ * @clkdm_wakeup:		standard omap SoC API
+ * @secure_dispatcher_replace:	API to replace standard dispatcher by alternate
+ * @omap_dispatcher_replace:	standard omap secure dispatcher
+ * @mm:				standard linux init_mm struct
+ * @secure_workspace_addr:	smc physical workspace address within the 1st GB
+ * @secure_workspace_size:	size of workspace reserved frm being
+ *				mapped to kernel memory space
+ *
+ * An standin structure to expose Linux kernel internal private
+ * structure to Security Driver.
+ * XXX: MOST OF THESE APIS should eventually disappear, but a first
+ * step towards that.
+ */
+struct omap_secure_platform_data {
+	int (*disable_nonboot_cpus)(void);
+	void (*enable_nonboot_cpus)(void);
+	long (*sched_setaffinity)(pid_t pid, const struct cpumask *new_mask);
+	long (*sched_getaffinity)(pid_t pid, struct cpumask *mask);
+	void (*clkdm_allow_idle)(struct clockdomain *clkdm);
+	struct clockdomain* (*clkdm_lookup)(const char *name);
+	int (*clkdm_wakeup)(struct clockdomain *clkdm);
+	int (*secure_dispatcher_replace) (omap_secure_dispatcher_t replacement);
+	u32 (*omap_secure_dispatcher)(u32 idx, u32 flag, u32 nargs,
+				      u32 arg1, u32 arg2, u32 arg3, u32 arg4);
+	struct mm_struct *mm;
+	phys_addr_t secure_workspace_addr;
+	u32 secure_workspace_size;
+};
+
+void omap_secure_set_secure_workspace_addr(phys_addr_t secure_workspace_addr,
+					   phys_addr_t secure_workspace_size);
+void omap_reserve_secure_workspace_addr(void);
+
+/*
+ * OMAP5 defaults assume atleast 1GB availability
+ * For platforms that do not meet this need or use modified PPA
+ * different from TI defaults, override from board file
+ */
+#define OMAP5_SECURE_WORKSPACE_BASE	0xBFA00000
+#define OMAP5_SECURE_WORKSPACE_SIZE	(6 * SZ_1M)
+/**
+ * omap5_secure_workspace_addr_default() - set default OMAP5 SMC address and size
+ *
+ * cpu_is_omapXYZ() macros dont work in reserve due to bad init sequence in OMAP
+ * framework (works only after io and early inits are called). So SoC specific
+ * APIs to help board files... :(
+ */
+static inline void omap5_secure_workspace_addr_default(void)
+{
+	omap_secure_set_secure_workspace_addr(OMAP5_SECURE_WORKSPACE_BASE,
+					      OMAP5_SECURE_WORKSPACE_SIZE);
+}
+
 #endif /* __ASSEMBLER__ */
 #endif /* OMAP_ARCH_OMAP_SECURE_H */
