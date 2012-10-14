cdab323b65a5fcaaee63b32eaf3b981b93388e8e
Sebastien Guiriec <s-guiriec@ti.com>
2012-10-14 22:36:50 +0200

    gpio:pca953x: Update to module_init in order to probe after pin ctrl.
    Update i2c driver to module_init in order to probe after pin control
    driver. This is needed due to the fact than i2c is not pinmux inside
    the bootloader.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -808,10 +808,7 @@ static int __init pca953x_init(void)
 {
 	return i2c_add_driver(&pca953x_driver);
 }
-/* register after i2c postcore initcall and before
- * subsys initcalls that may rely on these GPIOs
- */
-subsys_initcall(pca953x_init);
+module_init(pca953x_init);
 
 static void __exit pca953x_exit(void)
 {
