0bdd4147236355e6ed49a2f276b1dcbb32d58f04
Eduardo Valentin <eduardo.valentin@ti.com>
2012-11-05 17:27:58 -0600

    thermal: omap: update copyright entry
    simple copyright update
    
    Change-Id: Ic9490f531bb6f6bab9f065f587b08de20dc1f023
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Radhesh Fadnis <radhesh.fadnis@ti.com>

--- drivers/thermal/omap-bandgap.c
+++ drivers/thermal/omap-bandgap.c
@@ -1,7 +1,7 @@
 /*
  * OMAP4 Bandgap temperature sensor driver
  *
- * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2011-2012 Texas Instruments Incorporated - http://www.ti.com/
  * Author: J Keerthy <j-keerthy@ti.com>
  * Author: Moiz Sonasath <m-sonasath@ti.com>
  * Couple of fixes, DT and MFD adaptation:
