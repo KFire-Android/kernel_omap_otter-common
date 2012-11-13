d90705067902f8dd6d08749d3d80b801c13c70ef
Andrii Tseglytskyi <andrii.tseglytskyi@ti.com>
2012-11-13 17:01:11 +0200

    ARM: OMAP54XX: powerdomains: update PD_GPU data
    Patch adds wakeup latencies and updates powerstates for PD_GPU.
    
    Change-Id: I36b59812a59a4c85ce7e0d178714aecabadad617
    Signed-off-by: Andrii Tseglytskyi <andrii.tseglytskyi@ti.com>

--- arch/arm/mach-omap2/powerdomains54xx_data.c
+++ arch/arm/mach-omap2/powerdomains54xx_data.c
@@ -342,7 +342,7 @@ static struct powerdomain gpu_54xx_pwrdm = {
 	.voltdm		  = { .name = "mm" },
 	.prcm_offs	  = OMAP54XX_PRM_GPU_INST,
 	.prcm_partition	  = OMAP54XX_PRM_PARTITION,
-	.pwrsts		  = PWRSTS_OFF_ON,
+	.pwrsts		  = PWRSTS_OFF_INA_ON,
 	.banks		  = 1,
 	.pwrsts_mem_ret	= {
 		[0] = PWRSTS_OFF_RET,	/* gpu_mem */
@@ -351,6 +351,14 @@ static struct powerdomain gpu_54xx_pwrdm = {
 		[0] = PWRSTS_OFF_RET,	/* gpu_mem */
 	},
 	.flags		  = PWRDM_HAS_LOWPOWERSTATECHANGE,
+	.wakeup_lat = {
+		[PWRDM_POWER_OFF] = 1000,
+		[PWRDM_POWER_OSWR] = UNSUP_STATE,
+		[PWRDM_POWER_CSWR] = UNSUP_STATE,
+		[PWRDM_POWER_RET] = UNSUP_STATE,
+		[PWRDM_POWER_INACTIVE] = 20,
+		[PWRDM_POWER_ON] = 0,
+	},
 };
 
 /* wkupaon_54xx_pwrdm: Wake-up power domain */
