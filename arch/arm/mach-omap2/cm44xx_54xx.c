1eb38f1c5c54d5bba239fc84067ff833e3747876
Hashcode <hashcode0f@gmail.com>
2012-11-24 12:14:13 -0800

    fix OMAP5 define issue

--- arch/arm/mach-omap2/cm44xx_54xx.c
+++ arch/arm/mach-omap2/cm44xx_54xx.c
@@ -28,10 +28,14 @@
 #include "prcm44xx.h"
 #include "cm-regbits-44xx.h"
 
+#ifdef CONFIG_ARCH_OMAP4
+#include "cm2_54xx_es1.h"
+#else
 #ifdef CONFIG_ARCH_OMAP5_ES1
 #include "cm2_54xx_es1.h"
 #else
 #include "cm2_54xx.h"
+#endif
 #endif
 
 /* CM1 hardware module low-level functions */
