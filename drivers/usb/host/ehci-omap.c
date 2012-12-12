66fc489f196c09b7dc2c913ab89c28bf1a7cb849
Ruslan Bilovol <ruslan.bilovol@ti.com>
2012-12-12 18:03:10 +0200

    usb: ehci-omap: implement workaround for errata i640
    The OMAP4 EHCI controller has the following defect (errata ID i640):
    
    Title:
    	USB HOST EHCI In TLL Mode Will See The Port Being Disabled Upon Resume
    	Or Remote Wakeup.
    
    Description:
    	This bug impacts the TLL module when used in conjunction with the EHCI
    	part of the USB host module. Upon resume or remote wakeup from a bus
    	suspended state, the device will switch from full speed to high speed
    	before the host does.This will incorrectly generate a disconnect IRQ on
    	the host side (USBSTS[2]:PCD=1), and the hardware will disable the port
    	(PORTSC[2]:PED=0). The peripheral will then go back to suspend because
    	there is no more activity on the bus.
    
    	PHY mode (non-TLL) is not impacted. OHCI is not impacted.
    
    WORKAROUND:
    	The workaround consists of stalling the device for some time so that
    	the host can switch to HS before the device.
    
    	The programming guide to follow at the end of the resume sequence is:
    	1. The host ends the resume sequence by clearing PORTSC.FPR bit.
    	2. The software waits for the TLL to report the LineState change from
    	   K to SE0 on ULPI. PORTSC.LS will switch from 0x1(K) to 0x0(SE0).
    	3. Gate the TLL ULPI clock and thereby the device controller by PRCM
    	   control: the whole TLL is frozen, including the ULPI clock
    		- Clear the appropriate OPTFCLKEN_USB_CHx_CLK bit in the
    		  CM_L3INIT_HSUSBTLL_CLKCTRL register
    	4. Wait 3 us for the host to go to HS.
    	5. Restart the TLL ULPI clock and the device controller by PRCM
    	   control, to unfreeze the entire TLL, including the ULPI clock
    		- Set back the appropriate OPTFCLKEN_USB_CHx_CLK bit in the
    		  CM_L3INIT_HSUSBTLL_CLKCTRL register
    	6. The USB device switches to HS.
    
    This workaround is based on next patches from k3.0:
    - 4ca67e5 OMAP4: USB Host: Re-implement EHCI Hub Control to workaround errata
    	(by Anand Gadiyar <gadiyar@ti.com>)
    - 7ce60b1 OMAP4: USB: Update EHCI TLL errata i640
    	(by Jon Hunter <jon-hunter@ti.com>)
    
    Change-Id: I7fa89428950f3a5b39db7db76fdbee28f5cfec52
    Signed-off-by: Ruslan Bilovol <ruslan.bilovol@ti.com>

--- drivers/usb/host/ehci-omap.c
+++ drivers/usb/host/ehci-omap.c
@@ -57,6 +57,7 @@
 #define	EHCI_INSNREG05_ULPI_EXTREGADD_SHIFT		8
 #define	EHCI_INSNREG05_ULPI_WRDATA_SHIFT		0
 #define L3INIT_HSUSBHOST_CLKCTRL			(0x4A009358)
+#define L3INIT_HSUSBTLL_CLKCTRL				(0x4A009368)
 
 /* EHCI-HSIC module requires L3 clocked @ 250MHz+ */
 #define PM_QOS_MEMORY_THROUGHPUT_USBHOST         (250 * 4 * 1000)
@@ -204,6 +205,10 @@ static int ehci_omap_hub_control(
 	unsigned long	flags;
 	int		retval = 0;
 
+	u32		runstop = 0, temp_reg, tll_reg;
+
+	tll_reg = (u32)OMAP2_L4_IO_ADDRESS(L3INIT_HSUSBTLL_CLKCTRL);
+
 	if (((wIndex & 0xff) > 0) && ((wIndex & 0xff) < OMAP3_HS_USB_PORTS) &&
 			(pdata->port_mode[wIndex-1] ==
 					OMAP_EHCI_PORT_MODE_PHY)) {
@@ -286,11 +291,56 @@ static int ehci_omap_hub_control(
 				set_bit(wIndex - 1, &ehci->port_c_suspend);
 				ehci->reset_done[wIndex - 1] = 0;
 
-				/* stop resume signaling */
-				temp = ehci_readl(ehci, status_reg);
-				ehci_writel(ehci,
-					temp & ~(PORT_RWC_BITS | PORT_RESUME),
-					status_reg);
+				/*
+				 * i640 errata WA:
+				 * To Stop Resume Signalling, it is required
+				 * to Stop the Host Controller and disable the
+				 * TLL Functional Clock.
+				 * This errata is very specific. The timing
+				 * window between clearing FPR and cutting the
+				 * clock is very critical (nanoseconds).
+				 * Clock framework cannot guarantee such timing
+				 * so direct writing to L3INIT_HSUSBTLL_CLKCTRL
+				 * register have been used instead of functions
+				 * provided by clock framework.
+				 */
+				if (cpu_is_omap44xx()
+						&& (omap_rev() < OMAP4430_REV_ES2_3)
+						&& (pdata->port_mode[wIndex - 1]
+							== OMAP_EHCI_PORT_MODE_TLL)) {
+
+					/* Stop the Host Controller */
+					runstop = ehci_readl(ehci,
+							&ehci->regs->command);
+					ehci_writel(ehci, (runstop & ~CMD_RUN),
+							&ehci->regs->command);
+					(void) ehci_readl(ehci,
+							&ehci->regs->command);
+					handshake(ehci, &ehci->regs->status,
+							STS_HALT,
+							STS_HALT,
+							2000);
+					temp_reg = __raw_readl(tll_reg);
+					temp_reg &= ~(1 << (wIndex + 7));
+
+					/* stop resume signaling */
+					temp = __raw_readl(status_reg)
+							& ~(PORT_RWC_BITS
+							| PORT_RESUME);
+					__raw_writel(temp, status_reg);
+
+					/* Disable the Channel Optional Fclk */
+					__raw_writel(temp_reg, tll_reg);
+					dmb();
+				} else {
+					/* stop resume signaling */
+					temp = ehci_readl(ehci, status_reg);
+					ehci_writel(ehci,
+						temp & ~(PORT_RWC_BITS
+								| PORT_RESUME),
+						status_reg);
+				}
+
 				clear_bit(wIndex - 1, &ehci->resuming_ports);
 
 				/*
@@ -313,6 +363,28 @@ static int ehci_omap_hub_control(
 
 				retval = handshake(ehci, status_reg,
 					   PORT_RESUME, 0, 2000 /* 2msec */);
+
+				/*
+				 * i640 errata WA (continued):
+				 * Enable the Host Controller and start the
+				 * Channel Optional Fclk since resume has
+				 * finished.
+				 */
+				if (cpu_is_omap44xx()
+						&& (omap_rev() < OMAP4430_REV_ES2_3)
+						&& (pdata->port_mode[wIndex - 1]
+							== OMAP_EHCI_PORT_MODE_TLL)) {
+					udelay(3);
+					temp_reg = omap_readl(L3INIT_HSUSBTLL_CLKCTRL);
+					omap_writel((temp_reg
+							| (1 << (wIndex + 7))),
+							L3INIT_HSUSBTLL_CLKCTRL);
+					ehci_writel(ehci, runstop,
+							&ehci->regs->command);
+					(void) ehci_readl(ehci,
+							&ehci->regs->command);
+				}
+
 				if (retval != 0) {
 					ehci_err(ehci,
 						"port %d resume error %d\n",
