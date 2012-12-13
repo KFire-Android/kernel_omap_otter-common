f7e0f6509c0132068fbc234f813dca68952c9882
Nishanth Menon <nm@ti.com>
2012-12-12 21:10:14 -0600

    ARM: OMAP54XX: cm1_54xx: consolidate so that just cm1_54xx.h needs inclusion
    including the wrong cm1_54xx.h Vs cm1_54xx_es1.h in usage will wreak havoc
    in code by the usage of ES2.0 macros in ES1.0 build and viceverca.
    
    So ensure that cm1_54xx.h is the generic header that needs to be included
    which will then include ES1.0 or ES2.0 macros as needed.
    
    ES1.0 C data files have not been modified as they are meant to be built
    solely for ES1.0. un-modified files are:
    arch/arm/mach-omap2/clock54xx_es1_data.c
    arch/arm/mach-omap2/clockdomains54xx_es1_data.c
    arch/arm/mach-omap2/omap_hwmod_54xx_es1_data.c
    
    Change-Id: I5394fc4d8e5adec42163e4ef6d35440ef49deb36
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/dpll44xx_54xx.c
+++ arch/arm/mach-omap2/dpll44xx_54xx.c
@@ -37,13 +37,12 @@
 
 #ifdef CONFIG_ARCH_OMAP5_ES1
 #include "cm-regbits-54xx_es1.h"
-#include "cm1_54xx_es1.h"
 #include "cm2_54xx_es1.h"
 #else
 #include "cm-regbits-54xx.h"
-#include "cm1_54xx.h"
 #include "cm2_54xx.h"
 #endif
+#include "cm1_54xx.h"
 
 #define MAX_DPLL_WAIT_TRIES	1000000
 #define MAX_FREQ_UPDATE_TIMEOUT  100000
