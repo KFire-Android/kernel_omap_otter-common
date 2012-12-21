01134962f8ba8b14e9221aa9c5def0d45fd1ba08
Alexander Savchenko <oleksandr.savchenko@ti.com>
2012-12-21 15:26:56 +0200

    Blaze: Board: fix: restore board-props init call
    Restore board properties initialization call for create
    appropriate sysfs entry for OMAP44XX 4430SDP platform.
    
    Change-Id: I6f0c292139669173117a0b610877c1314d56746b
    Signed-off-by: Alexander Savchenko <oleksandr.savchenko@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -1298,7 +1298,7 @@ static void __init omap_4430sdp_init(void)
 
 	omap4_mux_init(board_mux, NULL, package);
 	omap_init_board_version(0);
-
+	omap_create_board_props();
 	omap4_i2c_init();
 	blaze_sensor_init();
 	blaze_touch_init();
