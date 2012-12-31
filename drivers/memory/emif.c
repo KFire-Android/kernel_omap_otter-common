bd732d5d5ad328e62ea3a12a78d42dafd9824ec7
Vaibhav Bedia <vaibhav.bedia@ti.com>
2012-12-31 10:35:21 +0530

    memory: emif: Move EMIF related header file to include/linux/
    OMAP4 and AM33XX share the same EMIF controller IP. Although there
    are significant differences in the IP integration due to which
    AM33XX can't reuse the EMIF driver DVFS similar to OMAP4,
    it can definitely benefit by reusing the EMIF related macros
    defined in drivers/memory/emif.h.
    
    In the current OMAP PM framework the PM code resides under
    arch/arm/mach-omap2/. To enable reuse of the register defines move
    the emif header file to include/linux so that both the EMIF driver
    and the AM33XX PM code can benefit.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Aneesh V <aneesh@ti.com>

--- drivers/memory/emif.c
+++ drivers/memory/emif.c
@@ -24,8 +24,8 @@
 #include <linux/module.h>
 #include <linux/list.h>
 #include <linux/spinlock.h>
+#include <linux/ti_emif.h>
 #include <memory/jedec_ddr.h>
-#include "emif.h"
 #include "of_memory.h"
 
 /**
