8b77cdf59074a5d5bb8b7ba60aeb7ba233740617
Lajos Molnar <lajos@ti.com>
2013-01-22 14:58:01 -0800

    OMAPDSS: Added forced TILER1D support for 5432 ES2.0
    This was already added for 5430 ES2.0, but is also applicable
    for 5432.
    
    Change-Id: Ic8e156cc0daefd6d5ee69b32190908a58fda211d
    Signed-off-by: Lajos Molnar <lajos@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -2560,7 +2560,8 @@ int dispc_setup_wb(struct writeback_cache_data *wb)
 				color_mode, wb->rotation_type);
 
 	/* Force 1D tiled mode */
-	if (cpu_is_omap54xx() && (omap_rev() == OMAP5430_REV_ES2_0))
+	if (cpu_is_omap54xx() && (omap_rev() == OMAP5430_REV_ES2_0 ||
+				  omap_rev() == OMAP5432_REV_ES2_0))
 		dispc_ovl_set_1d_tiled_mode(plane, wb->force_1d);
 
 	dispc_ovl_set_row_inc(plane, row_inc);
