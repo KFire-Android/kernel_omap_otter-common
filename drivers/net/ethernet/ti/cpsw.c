7b606efc83ee85747898f97cd297fd17f02e5615
Mugunthan V N <mugunthanvnm@ti.com>
2013-04-08 15:57:10 +0530

    drivers: net: cpsw: build warning as cpts is pointer
      CC      drivers/net/ethernet/ti/cpsw.o
    drivers/net/ethernet/ti/cpsw.c: In function 'cpsw_tx_handler':
    drivers/net/ethernet/ti/cpsw.c:462:2: warning: passing argument 1 of 'cpts_tx_timestamp' from incompatible pointer type
    drivers/net/ethernet/ti/cpts.h:136:20: note: expected 'struct cpts *' but argument is of type 'struct cpts **'
    
    This warning is generated after 3.8.6 merge comflict. cpts is change from
    struct cpts to struct cpts * for Dual EMAC implementation with the following
    commit.
    
    commit f94298a2dc0cc18e7bc8f2abc6a610e8b43a5dc2
    Author: Mugunthan V N <mugunthanvnm@ti.com>
    Date:   Mon Feb 11 09:52:19 2013 +0000
    
        driver: net: ethernet: cpsw: make cpts as pointer
    
        As CPTS is common module for both EMAC in Dual EMAC mode so making cpts as
        pointer.
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- drivers/net/ethernet/ti/cpsw.c
+++ drivers/net/ethernet/ti/cpsw.c
@@ -459,7 +459,7 @@ void cpsw_tx_handler(void *token, int len, int status)
 	 */
 	if (unlikely(netif_queue_stopped(ndev)))
 		netif_wake_queue(ndev);
-	cpts_tx_timestamp(&priv->cpts, skb);
+	cpts_tx_timestamp(priv->cpts, skb);
 	priv->stats.tx_packets++;
 	priv->stats.tx_bytes += len;
 	dev_kfree_skb_any(skb);
