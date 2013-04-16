99c82e1f53f616705965b3eee4b9c92a56c8a495
Nishanth Menon <nm@ti.com>
2013-04-16 12:15:27 -0500

    ARM: ABB: OMAP5+: Add ABB override data for OMAP5
    Add ABB support for OMAP5 - the LDOVBB override information
    
    Change-Id: Ic314edc6342b440c5364f6243a888bf72ac98de8
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/abb54xx_data.c
+++ arch/arm/mach-omap2/abb54xx_data.c
@@ -9,11 +9,15 @@
  * published by the Free Software Foundation.
  */
 
+#include <linux/io.h>
+#include <mach/ctrl_module_wkup_54xx.h>
+
 #include "abb.h"
 #include "vp.h"
 #include "prm54xx.h"
 #include "prm44xx.h"
 #include "prm-regbits-54xx.h"
+#include "iomap.h"
 
 static struct omap_abb_ops omap5_abb_ops = {
 	.check_tranxdone  = omap4_prm_abb_check_txdone,
@@ -36,6 +40,9 @@ struct omap_abb_instance omap5_abb_mpu = {
 	.ctrl_offs		= OMAP54XX_PRM_ABBLDO_MPU_CTRL_OFFSET,
 	.prm_irq_id		= OMAP5_VP_VDD_MPU_ID,
 	.common			= &omap5_abb_common,
+	.mux_control_reg	= OMAP54XX_CTRL_MODULE_WKUP_REGADDR(OMAP5_CTRL_MODULE_WKUP_LDOVBB_MPU_VOLTAGE_CTRL),
+	.mux_control_mask	= OMAP5_LDOVBBMPU_FBB_MUX_CTRL_MASK,
+	.mux_control_vset_out_mask = OMAP5_LDOVBBMPU_FBB_VSET_OUT_MASK,
 };
 
 struct omap_abb_instance omap5_abb_mm = {
@@ -43,4 +50,7 @@ struct omap_abb_instance omap5_abb_mm = {
 	.ctrl_offs		= OMAP54XX_PRM_ABBLDO_MM_CTRL_OFFSET,
 	.prm_irq_id		= OMAP5_VP_VDD_MM_ID,
 	.common			= &omap5_abb_common,
+	.mux_control_reg	= OMAP54XX_CTRL_MODULE_WKUP_REGADDR(OMAP5_CTRL_MODULE_WKUP_LDOVBB_MM_VOLTAGE_CTRL),
+	.mux_control_mask	= OMAP5_LDOVBBMM_FBB_MUX_CTRL_MASK,
+	.mux_control_vset_out_mask = OMAP5_LDOVBBMM_FBB_VSET_OUT_MASK,
 };
