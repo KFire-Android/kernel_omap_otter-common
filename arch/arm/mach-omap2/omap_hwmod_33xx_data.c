91e9ffd414cd71be4cbed13ebfd6aab3c6e4dbb7
Dan Murphy <dmurphy@ti.com>
2013-04-23 13:47:10 -0500

    TI-Integration:Fix auto merge issue on 33xx hwmod for ocmc ram
    The auto merge of the omap_hwmod_33xx_data.c seemed to have removed
    entries for the ocmram initilization.
    
    The below commit seems to have been over written in a merge.
    
    commit e70216ee5b50ac2d0015c08701e193bb2da983d2
    ARM: OMAP2+: AM33XX: hwmod: Register OCMC RAM hwmod
    
    Did not seem to be complete in the merge.
    So adding back the code to register the OCMC RAM hwmod
    
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_33xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_33xx_data.c
@@ -3567,6 +3567,13 @@ static struct omap_hwmod_ocp_if am33xx_l3_main__aes0 = {
 	.user		= OCP_USER_MPU | OCP_USER_SDMA,
 };
 
+/* l3 main -> ocmc */
+static struct omap_hwmod_ocp_if am33xx_l3_main__ocmc = {
+	.master         = &am33xx_l3_main_hwmod,
+	.slave          = &am33xx_ocmcram_hwmod,
+	.user           = OCP_USER_MPU | OCP_USER_SDMA,
+};
+
 static struct omap_hwmod_ocp_if *am33xx_hwmod_ocp_ifs[] __initdata = {
 	&am33xx_l4_fw__emif_fw,
 	&am33xx_l3_main__emif,
@@ -3645,6 +3652,7 @@ static struct omap_hwmod_ocp_if *am33xx_hwmod_ocp_ifs[] __initdata = {
 	&am33xx_l3_main__tptc1,
 	&am33xx_l3_main__tptc2,
 	&am33xx_l3_s__usbss,
+	&am33xx_l3_main__ocmc,
 	&am33xx_l4_hs__cpgmac0,
 	&am33xx_cpgmac0__mdio,
 	&am33xx_l3_main__sha0,
