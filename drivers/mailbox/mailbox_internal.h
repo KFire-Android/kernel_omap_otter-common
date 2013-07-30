sumananna/mailbox

mailbox: add support for multiple instances
The current mailbox driver core is designed to have only one mailbox
h/w block in the system, but this is very unrealistic. Support has
been added for multiple instances of same or different mailbox h/w
blocks by adding a containing mailbox_device structure. The mailboxes
contained within a h/w block are associated with this mailbox device
structure. The mailbox drivers have to create this mailbox device object
and register their mailboxes with this device.

The design includes changing the mailbox_register/unregister functions
to allow the registration and deregistration of mailboxes using a
containing mailbox device object. Function ops are added per mailbox
device as well, to have flexibility in each individual mailbox device
implementations. The mailbox startup code is reconfigured to use both
the mailbox_device and mbox startup/shutdown ops accordingly.

While at it, modify the OMAP & DBX500 mailbox drivers to adapt to
these new API changes.

Signed-off-by: Suman Anna <s-anna@ti.com>

drivers/mailbox/mailbox_internal.h
@@ -123,6 +123,7 @@ struct mailbox_queue {
  *	  not be touched by the mailbox core
  * @use_count: number of current references to the mailbox, useful in
  *	  controlling the mailbox state
+ * @parent: back reference to the containing parent mailbox device object
  * @notifier: notifier chain of clients, to which a received message is
  *	  communicated
  */
@@ -136,6 +137,7 @@ struct mailbox {
 	struct device		*dev;
 	void			*priv;
 	int			use_count;
+	struct mailbox_device	*parent;
 	struct blocking_notifier_head	notifier;
 };
 
@@ -146,7 +148,7 @@ void mailbox_init_seq(struct mailbox *);
  * mailbox objects registration and de-registration functions with the
  * mailbox core.
  */
-int mailbox_register(struct device *parent, struct mailbox **);
-int mailbox_unregister(void);
+int mailbox_register(struct mailbox_device *device, struct mailbox **);
+int mailbox_unregister(struct mailbox_device *device);
 
 #endif /* MAILBOX_INTERNAL_H */

KFire-Android/kernel_omap_otter-common

drivers/mailbox/mailbox_internal.h
@@ -126,6 +126,7 @@ struct mailbox_queue {
  *	  not be touched by the mailbox core
  * @use_count: number of current references to the mailbox, useful in
  *	  controlling the mailbox state
+ * @parent: back reference to the containing parent mailbox device object
  * @notifier: notifier chain of clients, to which a received message is
  *	  communicated
  */
@@ -139,6 +140,7 @@ struct mailbox {
 	struct device		*dev;
 	void			*priv;
 	int			use_count;
+	struct mailbox_device	*parent;
 	struct blocking_notifier_head	notifier;
 };
 
@@ -146,7 +148,7 @@ struct mailbox {
  * mailbox objects registration and de-registration functions with the
  * mailbox core.
  */
-int mailbox_register(struct device *parent, struct mailbox **);
-int mailbox_unregister(void);
+int mailbox_register(struct mailbox_device *mdev, struct mailbox **);
+int mailbox_unregister(struct mailbox_device *mdev);
 
 #endif /* MAILBOX_INTERNAL_H */