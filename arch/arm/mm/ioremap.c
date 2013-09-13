f7be2b191319c669001509ffd5d64ecdb053a871
Sunita Nadampalli <sunitan@ti.com>
2013-09-13 15:56:30 -0500

    HACK: mm: ioremap: Allow ioremap for RAM buffers
    The buffers allocated from ion carveout
    need to be shared across multiple processes for
    mm usecases. During the buffer import, it is
    hitting WARN condition on ioremap logic.
    Till the issue is root caused, the WARN is ignored.
    
    Change-Id: I0fedd26698d9a70254de42124511d43a65af5c0d
    Signed-off-by: Sunita Nadampalli <sunitan@ti.com>

--- arch/arm/mm/ioremap.c
+++ arch/arm/mm/ioremap.c
@@ -241,8 +241,10 @@ void __iomem * __arm_ioremap_pfn_caller(unsigned long pfn,
 	/*
 	 * Don't allow RAM to be mapped - this causes problems with ARMv6+
 	 */
+#if 0
 	if (WARN_ON(pfn_valid(pfn)))
 		return NULL;
+#endif
 
 	area = get_vm_area_caller(size, VM_IOREMAP, caller);
  	if (!area)
