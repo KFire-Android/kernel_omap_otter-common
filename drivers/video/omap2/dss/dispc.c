9c7820605db0ff957d98b337b38739977b230423
srinivas pulukuru <srinivas.pulukuru@ti.com>
2013-09-16 11:59:34 -0500

    OMAPDSS: DISPC: set preload to high threshold to avoid FIFO underflow
    set preload to high threshold to avoid FIFO underflow in HDMI
    
    Change-Id: Ia21692755cc5fa1f7d829ee6faf6ce93ac1388ab
    Signed-off-by: srinivas pulukuru <srinivas.pulukuru@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -1238,6 +1238,10 @@ void dispc_ovl_set_fifo_threshold(enum omap_plane plane, u32 low, u32 high)
 				hi_start, hi_end) * unit,
 			low * unit, high * unit);
 
+	/* preload to high threshold to avoid FIFO underflow , NA for WB */
+	if (plane != OMAP_DSS_WB)
+		dispc_write_reg(DISPC_OVL_PRELOAD(plane), min(high, 0xfffu));
+
 	dispc_write_reg(DISPC_OVL_FIFO_THRESHOLD(plane),
 			FLD_VAL(high, hi_start, hi_end) |
 			FLD_VAL(low, lo_start, lo_end));
