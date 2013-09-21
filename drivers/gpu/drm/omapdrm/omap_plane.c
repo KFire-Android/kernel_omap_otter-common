a7a565365dceeec503ca677c8b61a2f1890bd1d9
alaganraj <alaganraj.s@ti.com>
2013-09-21 19:19:58 +0530

    omapdrm: Expose the OMAP pre-multiplied alpha property through DRM
    Added support for pre-multiplied alpha changes through DRM plane properties
    
    Change-Id: I37e5ca378593390463cca0c7a44f49f79d538729
    Signed-off-by: alaganraj <alaganraj.s@ti.com>

--- drivers/gpu/drm/omapdrm/omap_plane.c
+++ drivers/gpu/drm/omapdrm/omap_plane.c
@@ -343,6 +343,16 @@ void omap_plane_install_properties(struct drm_plane *plane,
 		priv->global_alpha_prop = prop;
 	}
 	drm_object_attach_property(obj, prop, 0);
+
+	prop = priv->pre_mult_alpha_prop;
+	if (!prop) {
+		prop = drm_property_create_range(dev, 0, "pre_mult_alpha",
+						 0, 1);
+		if (prop == NULL)
+			return;
+		priv->pre_mult_alpha_prop = prop;
+	}
+	drm_object_attach_property(obj, prop, 0);
 }
 
 int omap_plane_set_property(struct drm_plane *plane,
@@ -363,6 +373,11 @@ int omap_plane_set_property(struct drm_plane *plane,
 	} else if (property == priv->global_alpha_prop) {
 		DBG("%s: global_alpha: %02x", omap_plane->name, (uint32_t)val);
 		omap_plane->info.global_alpha = val;
+		ret = apply(plane);
+	} else if (property == priv->pre_mult_alpha_prop) {
+		DBG("%s: pre_mult_alpha: %02x", omap_plane->name,
+						(uint32_t)val);
+		omap_plane->info.pre_mult_alpha = val;
 		ret = apply(plane);
 	}
 
