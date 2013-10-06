bbf3b2a4bea3fe91d8b7c8c02315ac35a1e2e483
Dandawate Saket <dsaket@ti.com>
2013-10-06 14:52:13 -0700

    OMAPDSS: PANEL: Add driver to support LG101 display
    Add driver to support LG101 chalk display using dts
    
    Change-Id: I1b6d3431a8d65bc03ddf3bfd24793e04003d5d78
    Signed-off-by: Dandawate Saket <dsaket@ti.com>

---
+++ drivers/video/omap2/displays/panel-lg101.c
@@ -0,0 +1,298 @@
+/*
+ * Copyright (C) 2013 Texas Instruments Inc
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
+#include <linux/of_i2c.h>
+#include <linux/of.h>
+#include <linux/regmap.h>
+
+#include <video/omapdss.h>
+#include <video/omap-panel-lg101.h>
+
+static const struct omap_video_timings lg101_default_timings = {
+	/* 1280 x 800 @ 60 Hz Reduced blanking VESA CVT 0.31M3-R */
+	.x_res		= 1280,
+	.y_res		= 800,
+
+	.pixel_clock	= 85333,
+
+	.hfp		= 32,
+	.hsw		= 48,
+	.hbp		= 80,
+
+	.vfp		= 4,
+	.vsw		= 3,
+	.vbp		= 7,
+
+	.vsync_level	= OMAPDSS_SIG_ACTIVE_LOW,
+	.hsync_level	= OMAPDSS_SIG_ACTIVE_LOW,
+	.data_pclk_edge = OMAPDSS_DRIVE_SIG_RISING_EDGE,
+	.de_level		= OMAPDSS_SIG_ACTIVE_HIGH,
+	.sync_pclk_edge = OMAPDSS_DRIVE_SIG_OPPOSITE_EDGES,
+};
+
+struct panel_drv_data {
+	struct omap_dss_device *dssdev;
+	struct mutex lock;
+	int p_gpio;
+	int dith;
+};
+
+static int lg101_power_on(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+	int r;
+
+	if (dssdev->state == OMAP_DSS_DISPLAY_ACTIVE)
+		return 0;
+
+	dev_dbg(&dssdev->dev, "lg101_power_on");
+
+	gpio_set_value_cansleep(ddata->p_gpio, 0); /* S0-0 S1-1 S2-0 A1 -> B2*/
+
+	omapdss_dpi_set_timings(dssdev, &dssdev->panel.timings);
+	omapdss_dpi_set_data_lines(dssdev, dssdev->phy.dpi.data_lines);
+
+	r = omapdss_dpi_display_enable(dssdev);
+	if (r)
+		goto err0;
+
+	return 0;
+err0:
+	return r;
+}
+
+static void lg101_power_off(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+
+	if (dssdev->state != OMAP_DSS_DISPLAY_ACTIVE)
+		return;
+	dev_dbg(&dssdev->dev, "lg101_power_off");
+
+	gpio_set_value_cansleep(ddata->p_gpio, 1); /* S0-1 S1-0 S2-0 A1 -> B1*/
+
+	omapdss_dpi_display_disable(dssdev);
+}
+
+static int lg101_probe_of(struct omap_dss_device *dssdev,
+		struct panel_drv_data *ddata)
+{
+	struct device_node *node = dssdev->dev.of_node;
+	int r, datalines, gpio, gpio_count;
+
+	gpio_count = of_gpio_count(node);
+	/* DRA-7 board has 1 selection gpio */
+	if (gpio_count != 1) {
+		dev_err(&dssdev->dev, "wrong number of GPIOs %d\n", gpio_count);
+		return -EINVAL;
+	}
+	dev_dbg(&dssdev->dev, "gpio count %d\n", gpio_count);
+
+	/* Get the GPIO for GPMC vs VID3 selection */
+	gpio = of_get_gpio(node, 0);
+	if (gpio_is_valid(gpio)) {
+		ddata->p_gpio = gpio;
+	} else {
+		dev_err(&dssdev->dev, "fail to parse vid3 gpio %d\n", gpio);
+		return -EINVAL;
+	}
+	dev_dbg(&dssdev->dev, "gpio number %d\n", gpio);
+
+	r = of_property_read_u32(node, "data-lines", &datalines);
+	if (r) {
+		dev_err(&dssdev->dev, "failed to parse datalines");
+		return r;
+	}
+	dssdev->phy.dpi.data_lines = datalines;
+
+	return 0;
+}
+
+static int lg101_probe(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata;
+	int r;
+
+	dev_err(&dssdev->dev, "lg101_probe probe\n");
+
+	ddata = devm_kzalloc(&dssdev->dev, sizeof(*ddata), GFP_KERNEL);
+	if (!ddata)
+		return -ENOMEM;
+
+	dssdev->panel.timings = lg101_default_timings;
+
+	ddata->dssdev = dssdev;
+	mutex_init(&ddata->lock);
+
+	if (dssdev->dev.of_node) {
+		r = lg101_probe_of(dssdev, ddata);
+		if (r)
+			return r;
+	} else if (dssdev->data) {
+		struct lg101_platform_data *pdata = dssdev->data;
+		dssdev->phy.dpi.data_lines = pdata->datalines;
+		ddata->p_gpio = pdata->p_gpio;
+	} else {
+		return -EINVAL;
+	}
+
+	if (dssdev->phy.dpi.data_lines == 24)
+		ddata->dith = 0;
+	else if (dssdev->phy.dpi.data_lines == 18)
+		ddata->dith = 1;
+	else
+		return -EINVAL;
+
+	r = gpio_request_one(ddata->p_gpio, GPIOF_OUT_INIT_LOW, "vid3_sel");
+	if (r)
+		return r;
+
+	dev_set_drvdata(&dssdev->dev, ddata);
+
+	dev_dbg(&dssdev->dev, "lg101_probe probe sucessful..\n");
+
+	return 0;
+}
+
+static void __exit lg101_remove(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+
+	mutex_lock(&ddata->lock);
+
+	gpio_free(ddata->p_gpio);
+
+	dev_set_drvdata(&dssdev->dev, NULL);
+
+	mutex_unlock(&ddata->lock);
+
+	kfree(ddata);
+}
+
+static int lg101_enable(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+	int r;
+
+	mutex_lock(&ddata->lock);
+
+	r = lg101_power_on(dssdev);
+	if (r == 0)
+		dssdev->state = OMAP_DSS_DISPLAY_ACTIVE;
+
+	mutex_unlock(&ddata->lock);
+
+	return r;
+}
+
+static void lg101_disable(struct omap_dss_device *dssdev)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+
+	mutex_lock(&ddata->lock);
+
+	lg101_power_off(dssdev);
+
+	dssdev->state = OMAP_DSS_DISPLAY_DISABLED;
+
+	mutex_unlock(&ddata->lock);
+}
+
+static void lg101_set_timings(struct omap_dss_device *dssdev,
+		struct omap_video_timings *timings)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+
+	mutex_lock(&ddata->lock);
+	omapdss_dpi_set_timings(dssdev, timings);
+	dssdev->panel.timings = *timings;
+	mutex_unlock(&ddata->lock);
+}
+
+static void lg101_get_timings(struct omap_dss_device *dssdev,
+		struct omap_video_timings *timings)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+
+	mutex_lock(&ddata->lock);
+	*timings = dssdev->panel.timings;
+	mutex_unlock(&ddata->lock);
+}
+
+static int lg101_check_timings(struct omap_dss_device *dssdev,
+		struct omap_video_timings *timings)
+{
+	struct panel_drv_data *ddata = dev_get_drvdata(&dssdev->dev);
+	int r;
+
+	mutex_lock(&ddata->lock);
+	r = dpi_check_timings(dssdev, timings);
+	mutex_unlock(&ddata->lock);
+
+	return r;
+}
+
+#if defined(CONFIG_OF)
+static const struct of_device_id lg101_of_match[] = {
+	{
+		.compatible = "ti,lg101",
+	},
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, lg101_of_match);
+#else
+#define lg101_of_match NULL
+#endif
+
+static struct omap_dss_driver lg101_driver = {
+	.probe		= lg101_probe,
+	.remove		= __exit_p(lg101_remove),
+
+	.enable		= lg101_enable,
+	.disable	= lg101_disable,
+
+	.set_timings	= lg101_set_timings,
+	.get_timings	= lg101_get_timings,
+	.check_timings	= lg101_check_timings,
+
+	.driver         = {
+		.name   = "lg101",
+		.owner  = THIS_MODULE,
+		.of_match_table = lg101_of_match,
+	},
+};
+
+static int __init lg101_init(void)
+{
+	return omap_dss_register_driver(&lg101_driver);
+}
+
+static void __exit lg101_exit(void)
+{
+	omap_dss_unregister_driver(&lg101_driver);
+}
+
+module_init(lg101_init);
+module_exit(lg101_exit);
+MODULE_LICENSE("GPL");
