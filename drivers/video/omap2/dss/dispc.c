06055ddb00ffcaa093142595432f4814bc473bca
alaganraj <alaganraj.s@ti.com>
2013-11-18 19:41:19 +0530

    OMAPDSS: DISPC: update CTRL_CORE_SMA_SW_1 register while updating DISPC_POL_FREQ1
    As per TRM, Pixel clock control on/off (onoff), Rise/Fall control (rf) and
    Invert pixel clock control (ipc) bits of  DISPC_POL_FREQ1 register must match
    corresponding bits in CTRL_CORE_SMA_SW_1 register.
    
    Change-Id: I4b238eb0df3799ae8af1beb6e512a89277bc0b2f
    Signed-off-by: alaganraj <alaganraj.s@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -84,6 +84,11 @@ struct dispc_features {
 
 	/* revert to the OMAP4 mechanism of DISPC Smart Standby operation */
 	bool standby_workaround:1;
+
+	/* match onoff, rf, ipc bits of DISPC_POL_FREQ1 in CTRL_CORE_SMA_SW_1
+	   register on dra7xx
+	*/
+	bool update_sma_sw1_reg:1;
 };
 
 #define DISPC_MAX_NR_FIFOS 5
@@ -93,6 +98,7 @@ static struct clockdomain *l3_1_clkdm, *l3_2_clkdm;
 static struct {
 	struct platform_device *pdev;
 	void __iomem    *base;
+	void __iomem    *ctrl_core_sma_sw1;
 
 	int		ctx_loss_cnt;
 
@@ -2975,6 +2981,7 @@ static void _dispc_mgr_set_lcd_timings(enum omap_channel channel, int hsw,
 {
 	u32 timing_h, timing_v, l;
 	bool onoff, rf, ipc;
+	u32 onoff_shift, rf_shift, ipc_shift;
 
 	timing_h = FLD_VAL(hsw-1, dispc.feat->sw_start, 0) |
 			FLD_VAL(hfp-1, dispc.feat->fp_start, 8) |
@@ -3023,6 +3030,36 @@ static void _dispc_mgr_set_lcd_timings(enum omap_channel channel, int hsw,
 	l |= FLD_VAL(hsync_level, 13, 13);
 	l |= FLD_VAL(vsync_level, 12, 12);
 	dispc_write_reg(DISPC_POL_FREQ(channel), l);
+
+	/* onoff, rf, ipc values must match in CTRL_CORE_SMA_SW_1 regs */
+	if (dispc.feat->update_sma_sw1_reg) {
+		l = ioread32(dispc.ctrl_core_sma_sw1);
+
+		switch (channel) {
+		case OMAP_DSS_CHANNEL_LCD:
+			rf_shift = 16;
+			onoff_shift = 19;
+			ipc_shift = 22;
+			break;
+		case OMAP_DSS_CHANNEL_LCD2:
+			rf_shift = 17;
+			onoff_shift = 20;
+			ipc_shift = 23;
+			break;
+		case OMAP_DSS_CHANNEL_LCD3:
+			rf_shift = 18;
+			onoff_shift = 21;
+			ipc_shift = 24;
+			break;
+		default:
+			BUG();
+		}
+
+		l = FLD_MOD(l, onoff, onoff_shift, onoff_shift);
+		l = FLD_MOD(l, rf, rf_shift, rf_shift);
+		l = FLD_MOD(l, ipc, ipc_shift, ipc_shift);
+		iowrite32(l, dispc.ctrl_core_sma_sw1);
+	}
 }
 
 /* change name to mode? */
@@ -3571,7 +3608,7 @@ void dispc_disable_sidle(void)
 	REG_FLD_MOD(DISPC_SYSCONFIG, 1, 4, 3);	/* SIDLEMODE: no idle */
 }
 
-static void _omap_dispc_initial_config(void)
+static int _omap_dispc_initial_config(void)
 {
 	u32 l;
 
@@ -3608,6 +3645,28 @@ static void _omap_dispc_initial_config(void)
 
 	if (dispc.feat->standby_workaround)
 		REG_FLD_MOD(DISPC_MSTANDBY_CTRL, 1, 0, 0);
+
+	if (dispc.feat->update_sma_sw1_reg) {
+		/* CTRL_MOD_CORE_SMA_SW1 register */
+		struct resource res[] = {
+			{
+				.start	= 0x4A002534,
+				.end	= 0x4A002538,
+				.flags	= IORESOURCE_MEM,
+			},
+		};
+
+		dispc.ctrl_core_sma_sw1 =
+			devm_request_and_ioremap(&dispc.pdev->dev,
+						 (struct resource *) &res);
+
+		if (!dispc.ctrl_core_sma_sw1) {
+			DSSERR("can't ioremap CONTROL_CORE_SMA_SW1\n");
+			return -ENOMEM;
+		}
+	}
+
+	return 0;
 }
 
 static const struct dispc_features omap24xx_dispc_feats __initconst = {
@@ -3625,6 +3684,7 @@ static const struct dispc_features omap24xx_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_24xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.update_sma_sw1_reg	=	false,
 };
 
 static const struct dispc_features omap34xx_rev1_0_dispc_feats __initconst = {
@@ -3642,6 +3702,7 @@ static const struct dispc_features omap34xx_rev1_0_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_34xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.update_sma_sw1_reg	=	false,
 };
 
 static const struct dispc_features omap34xx_rev3_0_dispc_feats __initconst = {
@@ -3659,6 +3720,7 @@ static const struct dispc_features omap34xx_rev3_0_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_34xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.update_sma_sw1_reg	=	false,
 };
 
 static const struct dispc_features omap44xx_dispc_feats __initconst = {
@@ -3676,6 +3738,7 @@ static const struct dispc_features omap44xx_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_44xx,
 	.num_fifos		=	5,
 	.gfx_fifo_workaround	=	true,
+	.update_sma_sw1_reg	=	false,
 };
 
 static const struct dispc_features omap54xx_dispc_feats __initconst = {
@@ -3694,6 +3757,26 @@ static const struct dispc_features omap54xx_dispc_feats __initconst = {
 	.num_fifos		=	5,
 	.gfx_fifo_workaround	=	true,
 	.standby_workaround	=	true,
+	.update_sma_sw1_reg	=	false,
+};
+
+static const struct dispc_features dra7xx_dispc_feats __initconst = {
+	.sw_start		=	7,
+	.fp_start		=	19,
+	.bp_start		=	31,
+	.sw_max			=	256,
+	.vp_max			=	4095,
+	.hp_max			=	4096,
+	.mgr_width_start	=	11,
+	.mgr_height_start	=	27,
+	.mgr_width_max		=	4096,
+	.mgr_height_max		=	4096,
+	.calc_scaling		=	dispc_ovl_calc_scaling_44xx,
+	.calc_core_clk		=	calc_core_clk_44xx,
+	.num_fifos		=	5,
+	.gfx_fifo_workaround	=	true,
+	.standby_workaround	=	true,
+	.update_sma_sw1_reg	=	true,
 };
 
 static int __init dispc_init_features(struct platform_device *pdev)
@@ -3729,8 +3812,11 @@ static int __init dispc_init_features(struct platform_device *pdev)
 		break;
 
 	case OMAPDSS_VER_OMAP5:
-	case OMAPDSS_VER_DRA7xx:
 		src = &omap54xx_dispc_feats;
+		break;
+
+	case OMAPDSS_VER_DRA7xx:
+		src = &dra7xx_dispc_feats;
 		break;
 
 	default:
@@ -3794,7 +3880,9 @@ static int __init omap_dispchw_probe(struct platform_device *pdev)
 	if (r)
 		goto err_runtime_get;
 
-	_omap_dispc_initial_config();
+	r = _omap_dispc_initial_config();
+	if (r)
+		goto err_runtime_get;
 
 	rev = dispc_read_reg(DISPC_REVISION);
 	dev_dbg(&pdev->dev, "OMAP DISPC rev %d.%d\n",
