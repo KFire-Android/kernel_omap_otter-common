89c250e9d0e2bad849c192e5825186cf82856570
Archit Taneja <archit@ti.com>
2013-11-27 12:18:48 +0530

    v4l: ti-vpe: enable basic scaler support
    Add the required scaler register configurations which lets us perform linear
    scaling for the supported range of horizontal and vertical scaling ratios.
    
    The horizontal scaler performs polyphase scaling using it's 8 tap 32 phase
    filter, decimation is performed when downscaling passes beyond 2x or 4x.
    
    The vertical scaler performs polyphase scaling using it's 5 tap 32 phase
    filter, it switches to a simpler form of scaling using the running average
    filter when the downscale ratio is more than 4x.
    
    Many of the scaler features like peaking, trimming and non-linear scaling aren't
    implemented for now. Only those register fields are configured which are
    required for basic scaling operation.
    
    The function to configure scaler registers takes the sc_data handle, the source
    and destination directions, and the scaler address data block offsets for the
    current context so that they can be configured.
    
    Change-Id: Iaaf08f5f3d5c2c9d95d97543ef333c7b7fa7178a
    Signed-off-by: alaganraj <alaganraj.s@ti.com>

--- drivers/media/platform/ti-vps/sc.c
+++ drivers/media/platform/ti-vps/sc.c
@@ -20,15 +20,6 @@
 #include "sc.h"
 #include "sc_coeff.h"
 
-void sc_set_regs_bypass(struct sc_data *sc, u32 *sc_reg0)
-{
-	u32 val = 0;
-
-	val |= CFG_SC_BYPASS;
-
-	*sc_reg0 = val;
-}
-
 void sc_dump_regs(struct sc_data *sc)
 {
 	struct device *dev = &sc->pdev->dev;
@@ -156,6 +147,131 @@ void sc_set_vs_coeffs(struct sc_data *sc, void *addr, unsigned int src_h,
 
 	sc->vs_index = idx;
 	sc->load_coeff_v = true;
+}
+
+void sc_config_scaler(struct sc_data *sc, u32 *sc_reg0, u32 *sc_reg8,
+		u32 *sc_reg17, unsigned int src_w, unsigned int src_h,
+		unsigned int dst_w, unsigned int dst_h)
+{
+	struct device *dev = &sc->pdev->dev;
+	u32 val = 0;
+	int dcm_x, dcm_shift;
+	bool use_rav;
+	unsigned long lltmp;
+	u32 lin_acc_inc, lin_acc_inc_u;
+	u32 col_acc_offset;
+	u16 factor = 0;
+	int row_acc_init_rav = 0, row_acc_init_rav_b = 0;
+	u32 row_acc_inc = 0, row_acc_offset = 0, row_acc_offset_b = 0;
+	u32 *sc_reg1 = sc_reg0 + 1;
+	u32 *sc_reg2 = sc_reg1 + 1;
+	u32 *sc_reg3 = sc_reg2 + 1;
+	u32 *sc_reg4 = sc_reg3 + 1;
+	u32 *sc_reg5 = sc_reg4 + 1;
+	u32 *sc_reg6 = sc_reg5 + 1;
+	u32 *sc_reg9 = sc_reg8 + 1;
+	u32 *sc_reg12 = sc_reg9 + 3;
+	u32 *sc_reg13 = sc_reg12 + 1;
+	u32 *sc_reg24 = sc_reg17 + 7;
+
+	/* clear all the features(they may get enabled elsewhere later) */
+	val &= ~(CFG_SELFGEN_FID | CFG_TRIM | CFG_ENABLE_SIN2_VER_INTP |
+		CFG_INTERLACE_I | CFG_DCM_4X | CFG_DCM_2X | CFG_AUTO_HS |
+		CFG_ENABLE_EV | CFG_USE_RAV | CFG_INVT_FID | CFG_SC_BYPASS |
+		CFG_INTERLACE_O | CFG_Y_PK_EN | CFG_HP_BYPASS);
+
+	if (src_w == dst_w && src_h == dst_h) {
+		val |= CFG_SC_BYPASS;
+		*sc_reg0 = val;
+		return;
+	}
+
+	/* we only support linear scaling for now */
+	val |= CFG_LINEAR;
+
+	/* configure horizontal scaler */
+
+	/* enable 2X or 4X decimation */
+	dcm_x = src_w / dst_w;
+	if (dcm_x > 4) {
+		val |= CFG_DCM_4X;
+		dcm_shift = 2;
+	} else if (dcm_x > 2) {
+		val |= CFG_DCM_2X;
+		dcm_shift = 1;
+	} else {
+		dcm_shift = 0;
+	}
+
+	lltmp = dst_w - 1;
+	lin_acc_inc = div64_u64(((u64)(src_w >> dcm_shift) - 1) << 24, lltmp);
+	lin_acc_inc_u = 0;
+	col_acc_offset = 0;
+
+	dev_dbg(dev, "hs config: src_w = %d, dst_w = %d, decimation = %s, lin_acc_inc = %08x\n",
+		src_w, dst_w,
+		dcm_shift == 2 ? "4x" : (dcm_shift == 1 ? "2x" : "none"),
+		lin_acc_inc);
+
+	/* configure vertical scaler */
+
+	/* use RAV for vertical scaler if vertical downscaling is > 4x */
+	if (dst_h < (src_h >> 2)) {
+		use_rav = true;
+		val |= CFG_USE_RAV;
+	} else {
+		use_rav = false;
+	}
+
+	if (use_rav) {
+		/* use RAV */
+		factor = (u16) ((dst_h << 10) / src_h);
+
+		row_acc_init_rav = factor + ((1 + factor) >> 1);
+		if (row_acc_init_rav >= 1024)
+			row_acc_init_rav -= 1024;
+
+		row_acc_init_rav_b = row_acc_init_rav +
+				(1 + (row_acc_init_rav >> 1)) -
+				(1024 >> 1);
+
+		if (row_acc_init_rav_b < 0) {
+			row_acc_init_rav_b += row_acc_init_rav;
+			row_acc_init_rav *= 2;
+		}
+
+	} else {
+		/* use polyphase */
+		row_acc_inc = ((src_h - 1) << 16) / (dst_h - 1);
+		row_acc_offset = 0;
+		row_acc_offset_b = 0;
+	}
+
+	dev_dbg(dev, "vs config: src_h = %d, dst_h = %d, use_rav = %d factor = %d, row_acc_init = %08x, row_acc_init_b = %08x row_acc_inc %08x\n",
+		src_h, dst_h, use_rav, factor,
+		row_acc_init_rav, row_acc_init_rav_b, row_acc_inc);
+
+	*sc_reg0 = val;
+	*sc_reg1 = row_acc_inc;
+	*sc_reg2 = row_acc_offset;
+	*sc_reg3 = row_acc_offset_b;
+
+	*sc_reg4 = ((lin_acc_inc_u & CFG_LIN_ACC_INC_U_MASK) <<
+			CFG_LIN_ACC_INC_U_SHIFT) | (dst_w << CFG_TAR_W_SHIFT) |
+			(dst_h << CFG_TAR_H_SHIFT);
+
+	*sc_reg5 = (src_w << CFG_SRC_W_SHIFT) | (src_h << CFG_SRC_H_SHIFT);
+
+	*sc_reg6 = (row_acc_init_rav_b << CFG_ROW_ACC_INIT_RAV_B_SHIFT) |
+		(row_acc_init_rav << CFG_ROW_ACC_INIT_RAV_SHIFT);
+
+	*sc_reg9 = lin_acc_inc;
+
+	*sc_reg12 = col_acc_offset << CFG_COL_ACC_OFFSET_SHIFT;
+
+	*sc_reg13 = factor;
+
+	*sc_reg24 = (src_w << CFG_ORG_W_SHIFT) | (src_h << CFG_ORG_H_SHIFT);
 }
 
 struct sc_data *sc_create(struct platform_device *pdev)
