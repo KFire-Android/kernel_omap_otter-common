600e93e90fd84f9c3d5468588b19a8104a6f8260
Hashcode <hashcode0f@gmail.com>
2014-03-20 21:33:51 -0700

    Revert "mmc: Disable MMC_CAP_ERASE for all drivers"
    This reverts commit 13acd9f89d25c7ebd56ab65d59dcf6a5c6f895d2.
    
    Change-Id: Ie939cc6bf7337dad180f8d7e24662782184ec043

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2483,7 +2483,7 @@ int sdhci_add_host(struct sdhci_host *host)
 	} else
 		mmc->f_min = host->max_clk / SDHCI_MAX_DIV_SPEC_200;
 
-	mmc->caps |= MMC_CAP_SDIO_IRQ | MMC_CAP_CMD23;
+	mmc->caps |= MMC_CAP_SDIO_IRQ | MMC_CAP_ERASE | MMC_CAP_CMD23;
 
 	if (host->quirks & SDHCI_QUIRK_MULTIBLOCK_READ_ACMD12)
 		host->flags |= SDHCI_AUTO_CMD12;
